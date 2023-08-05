a0981f5879fd8058891c72d2ea584eb342ac7f5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:41 +0000

    Add package masonite-permission/0.1.7/masonite-permission-0.1.7.tar.gz

---
+++ packages/masonite-permission/masonite-permission-0.1.7.tar.gz/masonite-permission-0.1.7/tests/integrations/app/controllers/WelcomeController.py
@@ -0,0 +1,22 @@
+"""A WelcomeController Module."""
+from masonite.views import View
+from masonite.controllers import Controller
+
+from src.masonite_permission.models.role import Role
+from tests.integrations.app.models.User import User
+
+
+class WelcomeController(Controller):
+    """WelcomeController Controller Class."""
+
+    def show(self, view: View):
+        user = User.first()
+        role = Role.where("slug", "reporter").first()
+        user.sync_roles(["admin", 2, "editor", role])
+
+        return view.render("welcome", {"user": user})
+
+    def test(self):
+        user = User.first()
+
+        return user.permissions()
