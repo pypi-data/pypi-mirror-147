6806f7ad0d293524d1d2d3fc2c96e2c367dc0fcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:18:22 +0000

    Add package gau-bin-probability-33/0.1/gau_bin_probability_33-0.1.tar.gz

---
+++ packages/gau-bin-probability-33/gau_bin_probability_33-0.1.tar.gz/gau_bin_probability_33-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gau_bin_probability_33',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['gau_bin_probability_33'],
+      author = 'Ilhwan Park',
+      author_email = 'ihp111@gmail.com',
+      zip_safe=False)
