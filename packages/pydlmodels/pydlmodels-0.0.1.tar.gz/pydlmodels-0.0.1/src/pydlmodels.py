7a0ff58e1832ac710a8c596a2c3dfd12aa6c8f83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:18:15 +0000

    Add package pydlmodels/0.0.1/pydlmodels-0.0.1.tar.gz

---
+++ packages/pydlmodels/pydlmodels-0.0.1.tar.gz/pydlmodels-0.0.1/src/pydlmodels.py
@@ -0,0 +1,185 @@
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from pybaseanal import*
+from termcolor import colored, cprint
+
+
+
+
+# GPU Utility function - 4
+def get_default_device():
+    """Pick GPU if available, else CPU"""
+    if torch.cuda.is_available():
+        return torch.device('cuda')
+    else:
+        return torch.device('cpu')
+    
+def to_device(data, device):
+    """Move tensor(s) to chosen device"""
+    if isinstance(data, (list,tuple)):
+        return [to_device(x, device) for x in data]
+    return data.to(device, non_blocking=True)
+
+class DeviceDataLoader():
+    """Wrap a dataloader to move data to a device"""
+    def __init__(self, dl, device):
+        self.dl = dl
+        self.device = device
+        
+    def __iter__(self):
+        """Yield a batch of data after moving it to device"""
+        for b in self.dl: 
+            yield to_device(b, self.device)
+
+    def __len__(self):
+        """Number of batches"""
+        return len(self.dl)
+
+
+
+
+
+
+
+class ImageClassificationBase(nn.Module): # loss for trainig
+    def training_step(self, batch):
+        images, labels = batch 
+        out = self(images)                  # Generate predictions
+        loss = F.cross_entropy(out, labels) # Calculate loss
+        return loss
+    
+    def validation_step(self, batch): # loss of val data
+        images, labels = batch 
+        out = self(images)                    # Generate predictions
+        loss = F.cross_entropy(out, labels)   # Calculate loss
+        acc = accuracy(out, labels)           # Calculate accuracy
+        return {'val_loss': loss.detach(), 'val_acc': acc}
+        
+    def validation_epoch_end(self, outputs):
+        batch_losses = [x['val_loss'] for x in outputs]
+        epoch_loss = torch.stack(batch_losses).mean()   # Combine losses
+        batch_accs = [x['val_acc'] for x in outputs]
+        epoch_acc = torch.stack(batch_accs).mean()      # Combine accuracies
+        return {'val_loss': epoch_loss.item(), 'val_acc': epoch_acc.item()}
+    
+    def epoch_end(self, epoch, result):
+        print("Epoch [{}], train_loss: {:.4f}, val_loss: {:.4f}, val_acc: {:.4f}".format(
+            epoch, result['train_loss'], result['val_loss'], result['val_acc']))
+        
+def accuracy(outputs, labels):
+    _, preds = torch.max(outputs, dim=1)
+    return torch.tensor(torch.sum(preds == labels).item() / len(preds))
+
+
+@torch.no_grad()
+def evaluate(model, val_loader):
+    model.eval()
+    outputs = [model.validation_step(batch) for batch in val_loader]
+    return model.validation_epoch_end(outputs)
+
+def fit(epochs, lr, model, train_loader, val_loader, opt_func=torch.optim.SGD):
+    history = []
+    optimizer = opt_func(model.parameters(), lr)
+    for epoch in range(epochs):
+        # Training Phase 
+        model.train()
+        train_losses = []
+        for batch in train_loader:
+            loss = model.training_step(batch)
+            train_losses.append(loss)
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+        # Validation phase
+        result = evaluate(model, val_loader)
+        result['train_loss'] = torch.stack(train_losses).mean().item()
+        model.epoch_end(epoch, result)
+        history.append(result)
+    return history
+
+def conv_block(in_channels, out_channels, pool=False):
+    layers = [nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1), 
+              nn.BatchNorm2d(out_channels), 
+              nn.ReLU(inplace=True)]
+    if pool: layers.append(nn.MaxPool2d(2))
+    return nn.Sequential(*layers)
+
+class ResNet9(ImageClassificationBase):
+    def __init__(self, in_channels, num_classes):
+        super().__init__()
+        
+        self.conv1 = conv_block(in_channels, 64)
+        self.conv2 = conv_block(64, 128, pool=True)
+        self.res1 = nn.Sequential(conv_block(128, 128), conv_block(128, 128))
+        
+        self.conv3 = conv_block(128, 256, pool=True)
+        self.conv4 = conv_block(256, 512, pool=True)
+        self.res2 = nn.Sequential(conv_block(512, 512), conv_block(512, 512))
+        
+        self.classifier = nn.Sequential(nn.AdaptiveMaxPool2d(1),
+                                        nn.Flatten(), 
+                                        nn.Dropout(0.2),
+                                        nn.Linear(512, num_classes))
+       
+    def forward(self, xb):
+        out = self.conv1(xb)
+        out = self.conv2(out)
+        out = self.res1(out) + out
+        out = self.conv3(out)
+        out = self.conv4(out)
+        out = self.res2(out) + out
+        out = self.classifier(out)
+        return out
+
+
+def plot_accuracies(history):
+    accuracies = [x['val_acc'] for x in history]
+    plt.plot(accuracies, '-x')
+    plt.xlabel('epoch')
+    plt.ylabel('accuracy')
+    plt.title('Accuracy vs. No. of epochs')
+    plt.show()
+
+def plot_losses(history):
+    train_losses = [x.get('train_loss') for x in history]
+    val_losses = [x['val_loss'] for x in history]
+    plt.plot(train_losses, '-bx')
+    plt.plot(val_losses, '-rx')
+    plt.xlabel('epoch')
+    plt.ylabel('loss')
+    plt.legend(['Training', 'Validation'])
+    plt.title('Loss vs. No. of epochs')
+    plt.show()
+
+
+
+def train_model(dataset, train_data, Valid_data, epochs, learning_rate, in_channels, num_classes ):
+    print("Available Models are - RestNet9")
+    m = input("Paste model name :")
+    if m == "RestNet9":
+        device = get_default_device()
+        print("On Device ", device)
+        cprint("Loading data on device", 'green', attrs=['blink'])
+        train_dl = DeviceDataLoader(train_data, device)
+        valid_dl = DeviceDataLoader(Valid_data, device)
+        model = to_device(ResNet9(in_channels, num_classes), device)
+        cprint("Evaluating and fitting model", 'green', attrs=['blink'])
+        history = [evaluate(model, valid_dl)]
+        print(history)
+        history += fit(epochs, learning_rate,model, train_dl, valid_dl, torch.optim.Adam)
+        cprint('Accuracy vs. No. of epochs', 'blue', attrs=['blink'])
+        plot_accuracies(history)
+        cprint('Loss vs. No. of epochs', 'red', attrs=['blink'])
+        plot_losses(history)
+    else:
+        train_model(dataset)
+
+
+
+
+
+
+
+
