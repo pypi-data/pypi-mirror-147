1b5c6d63c961b1f7dddc0b1f8ec58082bf064bc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:22 +0000

    Add package pony-stubs/0.3.0/pony-stubs-0.3.0.tar.gz

---
+++ packages/pony-stubs/pony-stubs-0.3.0.tar.gz/pony-stubs-0.3.0/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pony-stubs']
+
+package_data = \
+{'': ['*'],
+ 'pony-stubs': ['flask/*',
+                'orm/*',
+                'orm/dbproviders/*',
+                'orm/integration/*',
+                'thirdparty/*',
+                'utils/*']}
+
+setup_kwargs = {
+    'name': 'pony-stubs',
+    'version': '0.3.0',
+    'description': 'Type stubs for Pony ORM',
+    'long_description': '# Pony stubs\n\nPython type hint stubs for [Pony ORM](https://github.com/ponyorm/pony)\n\nWIP\n',
+    'author': 'Joonas Palosuo',
+    'author_email': 'joonas.palosuo@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
