1ff70adf6e58d32d915b0bd8f7e51a379f5606f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:42 +0000

    Add package model-insight/0.41/model-insight-0.41.tar.gz

---
+++ packages/model-insight/model-insight-0.41.tar.gz/model-insight-0.41/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='model-insight',
+    version='0.41',
+    license='MIT',
+    author="Wang Haihua",
+    author_email='reformship@gmail.com',
+    description='A package for learning and teaching mathematical modeling',
+    long_description='long description blabla',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/reformship/model-insight',
+    keywords='mathematical modeling',
+    install_requires=[
+          'numpy','pandas','matplotlib'
+      ],
+    include_package_data=False,
+    #package_data={'': ['src/model_insight/datasets/*.csv','src/model_insight/datasets/*.xls','src/model_insight/datasets/*.xlsx']},
+
+)
