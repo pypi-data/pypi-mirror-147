7d6222e3b614b31695cfcb3179d3e186f8335306
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:44 +0000

    Add package assertio/1.3.6/assertio-1.3.6-py3-none-any.whl

---
+++ packages/assertio/assertio-1.3.6-py3-none-any.whl/assertio/requests/precondition_request.py
@@ -0,0 +1,39 @@
+"""Preconditions module."""
+from typing import Dict, Union
+
+from ..decorators import given
+from .base_request import BaseRequest
+
+
+class Preconditions(BaseRequest):
+    """Precondition methods."""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    @given
+    def to(self, endpoint, **kwargs):
+        """Set endpoint to request."""
+        self.endpoint = endpoint
+        if kwargs:
+            self.endpoint = self.endpoint.format(**kwargs)
+
+    @given
+    def with_method(self, method):
+        """Set HTTP request method."""
+        self.method = method
+
+    @given
+    def with_body(self, body: Union[Dict, str]):
+        """Set request Content-Type: appliaction/json body."""
+        self.body = body
+
+    @given
+    def with_headers(self, headers):
+        """Set request header or headers."""
+        self.headers = headers
+
+    @given
+    def with_params(self, params):
+        """Set request query parameters."""
+        self.params = params
