31f342a2d46d027ea0f6464c79e3443874120ce8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:33:03 +0000

    Add package peachpayments-partner/0.1.7/peachpayments_partner-0.1.7-py3-none-any.whl

---
+++ packages/peachpayments-partner/peachpayments_partner-0.1.7-py3-none-any.whl/peachpayments_partner/config.py
@@ -0,0 +1,4 @@
+"""Module handling all the project configurations."""
+AUTH0_JWKS_URL = "https://login.peachpayments.com/.well-known/jwks.json"
+PEACH_AUTH_SERVICE_BASE_URL = "https://qa-dashboard.ppay.io/"
+GENERATE_ACCESS_TOKEN_PATH = "api/oauth/token"
