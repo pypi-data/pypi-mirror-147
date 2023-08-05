3c05adf220f4de9433dd9f85d7e6a221c0c942f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:30 +0000

    Add package vollseg-napari/2.2.4/vollseg-napari-2.2.4.tar.gz

---
+++ packages/vollseg-napari/vollseg-napari-2.2.4.tar.gz/vollseg-napari-2.2.4/setup.py
@@ -0,0 +1,68 @@
+from setuptools import setup, find_packages
+from os import path
+
+
+
+_dir = path.dirname(__file__)
+
+with open(path.join(_dir,'vollseg_napari','_version.py'), encoding="utf-8") as f:
+    exec(f.read())
+
+with open(path.join(_dir,'README.md'), encoding="utf-8") as f:
+    long_description = f.read()
+
+
+setup(
+    name='vollseg-napari',
+
+    version= __version__,
+
+    description='Irregular cell shape segmentation using VollSeg',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/kapoorlab/vollseg',
+    project_urls={
+        'Source Code': 'https://github.com/kapoorlab/vollseg-napari',
+        'Documentation': 'https://github.com/kapoorlab/vollseg-napari',
+        'Bug Tracker': 'https://github.com/kapoorlab/vollseg-napari/issues',
+        'User Support': 'https://forum.image.sc/tag/vollseg',
+        'Twitter': 'https://twitter.com/entracod',
+    },
+    author='Varun Kapoor',
+    author_email='varun.kapoor@kapoorlabs.org',
+    license='BSD 3-Clause License',
+    packages=find_packages(),
+    python_requires='>=3.7',
+
+      package_data={'vollseg_napari': [ 'resources/*', 'napari.yaml' ]},
+
+      entry_points={'napari.manifest': ['vollseg-napari = vollseg_napari:napari.yaml']},
+
+     
+      
+      dependeny_links = ['https://github.com/bhoeckendorf/pyklb.git@skbuild'],
+
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Science/Research',
+          'Topic :: Scientific/Engineering',
+          'License :: OSI Approved :: BSD License',
+
+          'Operating System :: OS Independent',
+
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
+
+          'Framework :: napari',
+      ],
+
+    install_requires=[
+        'vollseg',
+        'napari>=0.4.13',
+         'typing-extensions>=3.10.0.0'
+        'magicgui',
+        'tensorflow-gpu>=2.7.0',
+        'keras>=2.8'
+    ],
+)
---
+++ packages/vollseg-napari/vollseg-napari-2.2.4.tar.gz/vollseg-napari-2.2.4/vollseg_napari/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  8 15:54:54 2021
+
+@author: vkapoor
+"""
+
+from ._version import __version__
+
