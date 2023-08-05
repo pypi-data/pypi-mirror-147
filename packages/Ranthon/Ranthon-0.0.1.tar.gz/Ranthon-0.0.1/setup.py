f8c0f75a2dce290d622ddafe5704e0f953c62276
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:39 +0000

    Add package Ranthon/0.0.1/Ranthon-0.0.1.tar.gz

---
+++ packages/Ranthon/Ranthon-0.0.1.tar.gz/Ranthon-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+__project__  = "Ranthon"
+__version__ = "0.0.1"
+__description__ = "a bunch of random python modules"
+__packages__ = ["infseq"]
+
+setup(
+    name = __project__,
+    version = __version__,
+    description = __description__,
+    packages = __packages__
+)
