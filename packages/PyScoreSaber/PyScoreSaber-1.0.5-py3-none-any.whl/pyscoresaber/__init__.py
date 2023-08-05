d3b6f6c0cf6f8bff6371c13c6efa77b00d35d133
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:19:30 +0000

    Add package PyScoreSaber/1.0.5/PyScoreSaber-1.0.5-py3-none-any.whl

---
+++ packages/PyScoreSaber/PyScoreSaber-1.0.5-py3-none-any.whl/pyscoresaber/__init__.py
@@ -0,0 +1,10 @@
+import logging
+
+from .scoresaber import ScoreSaber
+from .scoresaber_api import ScoreSaberAPI
+from .scoresaber_provider import ScoreSaberProvider
+from .errors import *
+from .models import *
+
+handler = logging.StreamHandler()
+logging.getLogger("PyScoreSaber").addHandler(handler)
