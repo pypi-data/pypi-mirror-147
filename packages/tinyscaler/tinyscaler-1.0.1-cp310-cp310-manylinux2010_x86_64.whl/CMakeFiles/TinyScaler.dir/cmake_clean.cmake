4041e75db023e867ed85a3fb16a5cd8c0b81cd42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:45 +0000

    Add package tinyscaler/1.0.1/tinyscaler-1.0.1-cp310-cp310-manylinux2010_x86_64.whl

---
+++ packages/tinyscaler/tinyscaler-1.0.1-cp310-cp310-manylinux2010_x86_64.whl/CMakeFiles/TinyScaler.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TinyScaler.dir/src/scaler.c.o"
+  "CMakeFiles/TinyScaler.dir/src/scaler.c.o.d"
+  "libTinyScaler.a"
+  "libTinyScaler.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TinyScaler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
