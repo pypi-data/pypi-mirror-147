953043def349bf75c0bda026c02e833cd513af9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:25:18 +0000

    Add package vpype-perspective/0.1.0a0/vpype-perspective-0.1.0a0.tar.gz

---
+++ packages/vpype-perspective/vpype-perspective-0.1.0a0.tar.gz/vpype-perspective-0.1.0a0/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+
+with open("README.md") as f:
+    readme = f.read()
+
+with open("LICENSE") as f:
+    license = f.read()
+
+setup(
+    name="vpype-perspective",
+    version="0.1.0-alpha.0",
+    description="Put your art in perspective with vpype.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author="Antoine Beyeler",
+    url="https://github.com/abey79/vpype-perspective",
+    packages=["vpype_perspective"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Multimedia :: Graphics",
+        "Environment :: Plugins",
+    ],
+    setup_requires=["wheel"],
+    install_requires=[
+        "click",
+        "numpy",
+        "vpype[all]>=1.10",
+    ],
+    entry_points="""
+            [vpype.plugins]
+            perspective=vpype_perspective.perspective:perspective
+            pspread=vpype_perspective.commands:pspread
+            protate=vpype_perspective.commands:protate
+            ptranslate=vpype_perspective.commands:ptranslate
+            pscale=vpype_perspective.commands:pscale
+        """,
+)
