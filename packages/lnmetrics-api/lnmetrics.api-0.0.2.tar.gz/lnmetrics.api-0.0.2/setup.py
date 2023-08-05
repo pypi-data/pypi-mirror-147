1b6032d52032799ed49f625205bc1e562ee03986
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:34 +0000

    Add package lnmetrics-api/0.0.2/lnmetrics.api-0.0.2.tar.gz

---
+++ packages/lnmetrics-api/lnmetrics.api-0.0.2.tar.gz/lnmetrics.api-0.0.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['lnmetrics_api', 'lnmetrics_api.queries']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['aiohttp>=3.8.1,<4.0.0', 'gql>=3.1.0,<4.0.0']
+
+setup_kwargs = {
+    'name': 'lnmetrics.api',
+    'version': '0.0.2',
+    'description': 'Python implementation of the lnmetrics API to query the lnmetrics services',
+    'long_description': None,
+    'author': 'Vincenzo Palazzo',
+    'author_email': 'vincenzopalazzodev@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
