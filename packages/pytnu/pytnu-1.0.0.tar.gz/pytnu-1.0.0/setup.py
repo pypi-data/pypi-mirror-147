39c8b41741ac6ebe2d6d97e60a8d0b10855c4f67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:23:46 +0000

    Add package pytnu/1.0.0/pytnu-1.0.0.tar.gz

---
+++ packages/pytnu/pytnu-1.0.0.tar.gz/pytnu-1.0.0/setup.py
@@ -0,0 +1,26 @@
+import pathlib
+from setuptools import setup
+import setuptools
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="pytnu",
+    version="1.0.0",
+    description="An easy text-menu creation interface",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kYpranite/pytnu",
+    author="Ethan Huang",
+    author_email="ethanhuang2078@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=setuptools.find_packages(where="src", exclude=("tests",)),
+    include_package_data=True,
+)
