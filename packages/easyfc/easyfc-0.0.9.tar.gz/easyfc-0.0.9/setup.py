ced80aaf534a55bfbedfb7073e02a3ee0d5c1e30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:01 +0000

    Add package easyfc/0.0.9/easyfc-0.0.9.tar.gz

---
+++ packages/easyfc/easyfc-0.0.9.tar.gz/easyfc-0.0.9/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="easyfc",
+    version="0.0.9",
+    author='terryZ',
+    author_email="zhengcairui@foxmail.com",
+    description="适用于FasS业务的获取参数列表以及返回错误信息库",
+    packages=setuptools.find_packages(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ]
+)
