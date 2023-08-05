2e9045ff1d31189775b4182116f3facc3d80a943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:20 +0000

    Add package oaklib/0.1.8/oaklib-0.1.8-py3-none-any.whl

---
+++ packages/oaklib/oaklib-0.1.8-py3-none-any.whl/oaklib/implementations/ontobee/ontobee_implementation.py
@@ -0,0 +1,35 @@
+from abc import ABC
+from dataclasses import dataclass
+
+from oaklib.implementations.sparql.sparql_implementation import SparqlImplementation
+from oaklib.interfaces.mapping_provider_interface import MappingProviderInterface
+from oaklib.interfaces.search_interface import SearchInterface
+
+
+@dataclass
+class OntobeeImplementation(SparqlImplementation, SearchInterface, MappingProviderInterface):
+    """
+    Wraps the Ontobee sparql endpoint
+
+    An OntobeeImplementation can be initialed by:
+
+        .. code:: python
+
+           >>>  oi = OntobeeImplementation.create()
+
+        The default ontobee endpoint will be assumed
+
+    See: `<https://www.ontobee.org/>`_
+    """
+
+    def _default_url(self) -> str:
+        return "http://sparql.hegroup.org/sparql"
+
+
+
+
+
+
+
+
+
