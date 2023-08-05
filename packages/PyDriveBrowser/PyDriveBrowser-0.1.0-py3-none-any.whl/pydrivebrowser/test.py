9ca53f40a973a8902d622525713396a492ef1bea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:54 +0000

    Add package PyDriveBrowser/0.1.0/PyDriveBrowser-0.1.0-py3-none-any.whl

---
+++ packages/PyDriveBrowser/PyDriveBrowser-0.1.0-py3-none-any.whl/pydrivebrowser/test.py
@@ -0,0 +1,6 @@
+from cli_browser import CliBrowser
+
+u = 'https://drive.google.com/drive/folders/1OW3WLSPoDc01rU8XQL9AVMhPluBnXuBp?usp=sharing'
+file_id = '1EwtHVx8FfQiZmW2sYtg649QrZQJnE9Kh'
+c = CliBrowser()
+f = c.select_file(u)
