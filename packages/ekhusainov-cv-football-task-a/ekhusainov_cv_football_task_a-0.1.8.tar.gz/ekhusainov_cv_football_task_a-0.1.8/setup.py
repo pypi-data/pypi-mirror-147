123ca6800efca3ae3fa426d50b0134bf389fa77d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:38 +0000

    Add package ekhusainov-cv-football-task-a/0.1.8/ekhusainov_cv_football_task_a-0.1.8.tar.gz

---
+++ packages/ekhusainov-cv-football-task-a/ekhusainov_cv_football_task_a-0.1.8.tar.gz/ekhusainov_cv_football_task_a-0.1.8/setup.py
@@ -0,0 +1,24 @@
+
+import os
+from setuptools import find_packages, setup
+
+REQUIREMENTS_PATH = "requirements.txt"
+
+
+with open(REQUIREMENTS_PATH) as our_file:
+    required_libraries = our_file.read().splitlines()
+
+setup(
+    name="ekhusainov_cv_football_task_a",
+    version="0.1.8",
+    description="Test exercise. CV. Classification.",
+    long_description="README.md",
+    packages=["ekhusainov_cv_football_task_a"],
+    author="Khusainov Eldar",
+    install_requires=required_libraries,
+    requirements=["requests<=2.21.0"],
+    license="MIT",
+    license_files="LICENSE",
+    author_email="xluttiy@gmail.com",
+    zip_safe=False,
+)
