8627f3375920187b5e5e2a676ea1b463afbfd52f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:37:15 +0000

    Add package pyacadcom/0.0.8/pyacadcom-0.0.8.tar.gz

---
+++ packages/pyacadcom/pyacadcom-0.0.8.tar.gz/pyacadcom-0.0.8/pyacadcom/__init__.py
@@ -0,0 +1,15 @@
+""""
+    pyacadcom
+    ******************
+
+    AutoCAD automation through COM using pywin32
+
+    :copyright: (c) 2022 by Dmitriy Lobyntsev
+    :licence: BSD
+"""
+
+__version__ = "0.0.8"
+
+from pyacadcom.api import *
+from pyacadcom.datatype import *
+from pyacadcom.userinput import *
