b58eb9e3da8c79304e6348793b1d5623866db4ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:36 +0000

    Add package surfingcrypto/0.2.0/surfingcrypto-0.2.0-py3-none-any.whl

---
+++ packages/surfingcrypto/surfingcrypto-0.2.0-py3-none-any.whl/surfingcrypto/portfolio/portfolio.py
@@ -0,0 +1,148 @@
+"""
+portfolio objects.
+"""
+import pandas as pd
+from surfingcrypto.portfolio import MyCoinbase
+from surfingcrypto.portfolio.tracker import Tracker
+
+
+
+class Portfolio:
+    """
+    User portfolio.
+
+    At the moment the only supported :obj:`portfolio_type` is Coinbase, via
+    the :obj:`surfingcrypto.portfolio.coinbase` submodule.
+
+    Has methods for assessing portfolio values, grouping transactions by type,
+    calculate total fees, etc.
+
+    With `Portfolio.Analysis._standardize()` it is possible to adapt
+    the `surfingcrypto.coinbase.MyCoinbase` object to a standard form
+    where there are only `buy` or `sell` trades
+    and the amounts are always positive.
+
+    This standard portfolio allows to reconstruct easily the timeline
+    of the portfolio value, at each given instant in time.
+
+    Arguments:
+
+    Attributes:
+        portfolio_type (string): type of portfolio
+        df (:obj:`pandas.DataFrame`): coinbase transaction
+            history as from the `surfingcrypto.coinbase.MyCoinbase` output
+        std_df (:obj:`pandas.DataFrame`): standardized transaction history,
+             contains only `buy` or `sell`
+    """
+
+    def __init__(self, portfolio_type, **kwargs):
+        self.portfolio_type = portfolio_type
+        if self.portfolio_type.lower() == "coinbase":
+            self.coinbase = MyCoinbase(active_accounts=False, **kwargs)
+            self.coinbase.get_history()
+            self.errors=[]
+            self._standardize()
+            self._init_log()
+        else:
+            raise NotImplementedError
+
+    def _standardize(self):
+        """
+        creates a dataframe of only `buy` and `sell` orders,
+        for easier portfolio analysis.
+
+        It calculates missing fee information of `trade` transactions.
+
+        Note:
+            At the moment does not include `send`,`deposit`
+            or `withdrawal` transactions
+
+        """
+        self.std_df = self.coinbase.history.df.copy()
+
+        # exclude fiat deposit and withdrawals AND SEND
+        self.std_df = self.std_df[
+            self.std_df["type"].isin(["buy", "sell", "trade", "send"])
+        ]
+
+        # set trades as buy or sell transactions
+        m = (self.std_df["type"] == "trade") & (self.std_df["amount"] < 0)
+        self.std_df.loc[m, "type"] = "sell"
+        m = (self.std_df["type"] == "trade") & (self.std_df["amount"] > 0)
+        self.std_df.loc[m, "type"] = "buy"
+
+        # set sends as buy and sells
+        m = (self.std_df["type"] == "send") & (self.std_df["amount"] < 0)
+        self.std_df.loc[m, "type"] = "sell"
+        m = (self.std_df["type"] == "send") & (self.std_df["amount"] > 0)
+        self.std_df.loc[m, "type"] = "buy"
+
+        # make all amounts positive
+        self.std_df["amount"] = self.std_df["amount"].abs()
+        self.std_df["native_amount"] = self.std_df["native_amount"].abs()
+
+        # exclude double transactions coin-fiat for the tracking purpose.
+        self.std_df = self.std_df[~(self.std_df["symbol"] == "EUR")]
+
+        # split trades fees among the two
+        for trade in self.std_df.trade_id.unique():
+            if trade is not None:
+                t = self.std_df[self.std_df["trade_id"] == trade]
+                if len(t) == 2:
+                    fee = t["native_amount"].diff()
+                    self.std_df.loc[
+                        (self.std_df["trade_id"] == trade), "total_fee"
+                    ] = (fee[-1] / 2)
+
+                else:
+                    self.errors.append({
+                        "descr":"Can't split trade because not found 2 trades with matching trade_id",
+                        "t":trade
+                    })
+
+    def total_fees(self) -> float:
+        """total fees paid
+
+        Returns:
+            float: total fees paid for handled transactions.
+
+        """
+        return self.std_df.total_fee.sum()
+
+    def total_investment(self) -> pd.DataFrame:
+        """get total investment
+
+        Returns:
+            pd.DataFrame: dataframe resuming investment
+        """
+        investment = (
+            self.coinbase.history.df[
+                self.coinbase.history.df.type.isin(
+                    ["fiat_deposit", "fiat_withdrawal"]
+                )
+            ]
+            .groupby("type")[["amount"]]
+            .sum()
+        )
+        return investment
+
+    def start_tracker(self,stocks_start="1-1-2021", benchmark=None):
+        self.tracker = Tracker(
+            self.std_df, stocks_start=stocks_start, benchmark=benchmark, configuration=self.coinbase.configuration
+        )
+
+    def _init_log(self):
+        """
+        prints log of initialization status.
+        """
+        print("### PORTFOLIO ")
+        print(self.coinbase)
+        if len(self.std_df) != len(self.coinbase.history.df):
+            n = len(self.coinbase.history.df) - len(self.std_df)
+            print(
+                f"Warning! There are {n} transactions"
+                "that were EXCLUDED in std_df."
+            )
+        if not self.coinbase.history.executed_without_errors():
+            print("Warning! Errors while handling transactions:")
+            print(self.coinbase.history)
---
+++ packages/surfingcrypto/surfingcrypto-0.2.0-py3-none-any.whl/surfingcrypto/reporting/plotting.py
@@ -0,0 +1,246 @@
+"""
+plotting methods.
+"""
+import mplfinance as mplf
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+# warning di mplfinance per too many data in candlestick plot
+import warnings
+
+warnings.filterwarnings("ignore")
+
+
+def candlesticks(ts, ax, volume=False, vol_ax=None, style="candlesticks"):
+    """
+    plotting candlesticks into a matplotlib.axes.Axes object.
+
+    Args:
+        ts (:class:`surfingcrypto.ts.TS`) : `surfingcrypto.ts.TS` object
+        ax (:class:`matplotlib.axes.Axes`) : matplotlib ax to plot
+            candlesticks into.
+        volume (bool,optional): plot volume histogram data into a
+            another ax specified with `vol_ax`
+        vol_ax (:class:`matplotlib.axes.Axes`) : matplotlib ax
+             to plot volume histogram into.
+        style (str,optional): style of plotting candlesticks, `candlesticks`
+            is default but `ohlc` style is used in BB bands plotting.
+    """
+    if style == "candlesticks":
+        if volume is False:
+            mplf.plot(
+                ts.df,
+                ax=ax,
+                type="candle",
+                style="yahoo",
+                show_nontrading=True,
+                loc="lower left",
+            )
+        elif volume and vol_ax is None:
+            raise ValueError("Must specify ax for volume plot.")
+        else:
+            mplf.plot(
+                ts.df,
+                ax=ax,
+                volume=vol_ax,
+                type="candle",
+                style="yahoo",
+                show_nontrading=True,
+            )
+    elif style == "ohlc":
+        mplf.plot(
+            ts.df, ax=ax, type="ohlc", style="mike", show_nontrading=True
+        )
+    else:
+        raise ValueError("Must specify style.")
+    return
+
+
+def plot_moving_averages(ts, ax, windows=None):
+    """
+    Plot two simple moving averages.
+    Default windows are 12 and 26 days. Can be customized by using
+    lists of window-color pairs.
+
+    Args:
+        ts (:class:`surfingcrypto.ts.TS`) : `surfingcrypto.ts.TS` object
+        windows (obj:`list` of obj:`int`,optional): list of
+            windows to compute MA
+    """
+
+    colors = ["yellow", "orange"]
+    if windows is None:
+        windows = [ts.ta_params["sma"]["fast"], ts.ta_params["sma"]["slow"]]
+    for window, color in zip(windows, colors):
+        ax.plot(
+            ts.df["SMA_" + str(window)],
+            linestyle="-",
+            color=color,
+            label="SMA" + str(window),
+            alpha=0.3,
+            linewidth=1,
+        )
+        l = ax.legend(loc="upper left", prop={"size": 3})
+        l.get_frame().set_linewidth(0.5)
+
+
+def plot_macd(ts, ax, plot_lines=True):
+    """
+    plot macd indicator into a matplotlib.axes.Axes object.
+
+
+    Args:
+        ts (:class:`surfingcrypto.ts.TS`) : `surfingcrypto.ts.TS` object
+        ax (:class:`matplotlib.axes.Axes`) : matplotlib ax to plot MACD into.
+        plot_lines (bool): plot lines in addition to histogram.
+
+    """
+
+    fast = str(ts.ta_params["macd"]["fast"])
+    slow = str(ts.ta_params["macd"]["slow"])
+    sign = str(ts.ta_params["macd"]["signal"])
+
+    prices = ts.df["Open"]
+    macd = ts.df["MACD_" + fast + "_" + slow + "_" + sign]
+    signal = ts.df["MACDs_" + fast + "_" + slow + "_" + sign]
+    hist = ts.df["MACDh_" + fast + "_" + slow + "_" + sign]
+
+    if plot_lines:
+        # ax1.plot(prices)
+        ax.plot(
+            macd,
+            color="grey",
+            linewidth=1.5,
+            label="MACD(" + fast + "-" + slow,
+        )
+        ax.plot(
+            signal,
+            color="skyblue",
+            linewidth=1.5,
+            label="SIGNAL(" + sign + ")",
+        )
+
+    for i in range(len(prices)):
+        if str(hist[i])[0] == "-":
+            ax.bar(prices.index[i], hist[i], color="#ef5350")
+        else:
+            ax.bar(prices.index[i], hist[i], color="#26a69a")
+
+    if plot_lines:
+        l = ax.legend(loc="lower left", prop={"size": 3})
+        l.get_frame().set_linewidth(0.5)
+
+    ax.annotate(
+        "MACD(" + fast + "-" + slow + ")-S(" + sign + ")",
+        xy=(0.99, 0.1),
+        xycoords="axes fraction",
+        ha="right",
+    )
+    ax.set_ylabel("MACD")
+
+
+def plot_bb(ts, iax):
+    """
+    plot Bollinger bands indicator into a matplotlib.axes.Axes object.
+
+    Args:
+        ts (:class:`surfingcrypto.ts.TS`) : `surfingcrypto.ts.TS` object
+        ax (:class:`matplotlib.axes.Axes`) : matplotlib ax
+            to plot BB bands into.
+    """
+    length = str(ts.ta_params["bbands"]["length"])
+    std = "{0:.1f}".format(ts.ta_params["bbands"]["std"])
+
+    iax.plot(
+        ts.df["BBM_" + length + "_" + std],
+        label=f"MA{length}-STD{str(std)}",
+        color="dodgerblue",
+        alpha=0.35,
+    )  # middle band
+    iax.plot(
+        ts.df["BBU_" + length + "_" + std],
+        label="_",
+        color="greenyellow",
+        alpha=0.35,
+    )  # Upper band
+    iax.plot(
+        ts.df["BBL_" + length + "_" + std],
+        label="_",
+        color="coral",
+        alpha=0.35,
+    )  # lower band
+    iax.fill_between(
+        ts.df.index,
+        ts.df["BBL_" + length + "_" + std],
+        ts.df["BBU_" + length + "_" + std],
+        alpha=0.1,
+    )
+    l = iax.legend(loc="lower left", prop={"size": 3})
+    l.get_frame().set_linewidth(0.5)
+    iax.set_ylabel("B Bands")
+
+
+def plot_RSI(ts, iax):
+    """
+    plot RSI indicator into a matplotlib.axes.Axes object.
+
+    Args:
+        ts (:class:`surfingcrypto.ts.TS`) : `surfingcrypto.ts.TS` object
+        ax (:class:`matplotlib.axes.Axes`) : matplotlib ax to plot RSI into.
+    """
+    timeperiod = str(ts.ta_params["rsi"]["timeperiod"])
+    iax.plot(ts.df["RSI_" + timeperiod], color="magenta", alpha=0.5)
+    iax.set_ylim([0, 100])
+    iax.set_ylabel("RSI")
+    iax.axhspan(30, 70, facecolor="white", alpha=0.1)
+    iax.axhline(30, color="coral", linewidth=0.5, alpha=0.3)
+    iax.axhline(70, color="greenyellow", linewidth=0.5, alpha=0.3)
+
+
+def shiftedColorMap(cmap, start=0, midpoint=0.5, stop=1.0, name="shiftedcmap"):
+    """
+    Function to offset the "center" of a colormap. Useful for
+    data with a negative min and positive max and you want the
+    middle of the colormap's dynamic range to be at zero.
+
+    Args:
+        cmap : The matplotlib colormap to be altered
+        start : Offset from lowest point in the colormap's range.
+            Defaults to 0.0 (no lower offset). Should be between
+            0.0 and `midpoint`.
+        midpoint : The new center of the colormap. Defaults to
+            0.5 (no shift). Should be between 0.0 and 1.0. In
+            general, this should be  1 - vmax / (vmax + abs(vmin))
+            For example if your data range from -15.0 to +5.0 and
+            you want the center of the colormap at 0.0, `midpoint`
+            should be set to  1 - 5/(5 + 15)) or 0.75
+        stop : Offset from highest point in the colormap's range.
+            Defaults to 1.0 (no upper offset). Should be between
+            `midpoint` and 1.0.
+    """
+    cdict = {"red": [], "green": [], "blue": [], "alpha": []}
+
+    # regular index to compute the colors
+    reg_index = np.linspace(start, stop, 257)
+
+    # shifted index to match the data
+    shift_index = np.hstack(
+        [
+            np.linspace(0.0, midpoint, 128, endpoint=False),
+            np.linspace(midpoint, 1.0, 129, endpoint=True),
+        ]
+    )
+
+    for ri, si in zip(reg_index, shift_index):
+        r, g, b, a = cmap(ri)
+
+        cdict["red"].append((si, r, r))
+        cdict["green"].append((si, g, g))
+        cdict["blue"].append((si, b, b))
+        cdict["alpha"].append((si, a, a))
+
+    newcmap = matplotlib.colors.LinearSegmentedColormap(name, cdict)
+    plt.register_cmap(cmap=newcmap)
+
+    return newcmap
