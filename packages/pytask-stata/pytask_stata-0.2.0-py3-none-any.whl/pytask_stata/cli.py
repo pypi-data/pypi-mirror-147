6a8fadaef7b8ab05ca4e267897ddc8a9b87e18f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:35:55 +0000

    Add package pytask-stata/0.2.0/pytask_stata-0.2.0-py3-none-any.whl

---
+++ packages/pytask-stata/pytask_stata-0.2.0-py3-none-any.whl/pytask_stata/cli.py
@@ -0,0 +1,31 @@
+"""Everything related to the CLI."""
+from __future__ import annotations
+
+import click
+from pytask import hookimpl
+
+
+@hookimpl
+def pytask_extend_command_line_interface(cli: click.Group) -> None:
+    """Add stata related options to the command line interface."""
+    additional_parameters = [
+        click.Option(
+            ["--stata-keep-log"],
+            help=(
+                "Do not remove the log files produced while running do-files.  "
+                "[default: False]"
+            ),
+            is_flag=True,
+            default=None,
+        ),
+        click.Option(
+            ["--stata-check-log-lines"],
+            help=(
+                "Number of lines of the log file to consider when searching for "
+                "non-zero exit codes.  [default: 10]"
+            ),
+            type=int,
+            default=None,
+        ),
+    ]
+    cli.commands["build"].params.extend(additional_parameters)
