1235d27dbc4f85078ff12ad10a36b73f73ad7660
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:52 +0000

    Add package recmetrics/0.1.4/recmetrics-0.1.4.tar.gz

---
+++ packages/recmetrics/recmetrics-0.1.4.tar.gz/recmetrics-0.1.4/setup.py
@@ -0,0 +1,30 @@
+import io
+import os
+
+from setuptools import setup
+
+
+def read(file_name):
+    """Read a text file and return the content as a string."""
+    with io.open(os.path.join(os.path.dirname(__file__), file_name),
+                 encoding='utf-8') as f:
+        return f.read()
+
+setup(
+    name='recmetrics',
+    url='https://github.com/statisticianinstilettos/recommender_metrics',
+    author='Claire Longo',
+    author_email='longoclaire@gmail.com',
+    packages=['recmetrics'],
+    install_requires=['funcsigs',
+        'numpy',
+        'pandas',
+        'plotly',
+        'scikit-learn',
+        'seaborn'],
+    license='MIT',
+    version='0.1.4',
+    description='Evaluation metrics for recommender systems',
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+)
