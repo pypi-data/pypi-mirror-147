0649ebb00bde4fd916bb189f0d2190832ad335a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:16 +0000

    Add package distpygv/0.2/distpygv-0.2.tar.gz

---
+++ packages/distpygv/distpygv-0.2.tar.gz/distpygv-0.2/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import setuptools
+
+setuptools.setup(
+name = 'distpygv',
+version = '0.2',
+author = 'Gaurav Gadekar',
+author_email = 'gauravgadekar43@gmail.com',
+description = 'Calculation of distance with lat and long',
+packages = setuptools.find_packages(),
+classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
