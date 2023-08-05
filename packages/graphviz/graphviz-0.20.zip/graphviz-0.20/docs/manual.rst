073b36fef6febed92e5de5d906b819c06d2bb3b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:35 +0000

    Add package graphviz/0.20/graphviz-0.20.zip

---
+++ packages/graphviz/graphviz-0.20.zip/graphviz-0.20/docs/manual.rst
@@ -0,0 +1,66 @@
+.. _manual:
+
+User Guide
+==========
+
+
+.. include:: installation.rst
+
+
+.. include:: basic_usage.rst
+
+
+.. include:: formats.rst
+
+
+.. include:: piped_output.rst
+
+
+.. include:: jupyter_notebooks.rst
+
+
+.. include:: styling.rst
+
+
+.. include:: attributes.rst
+
+
+.. include:: node_ports.rst
+
+
+.. _Escapes:
+
+.. include:: escapes.rst
+
+
+.. _Quoting:
+
+.. include:: quoting.rst
+
+
+.. include:: subgraphs_and_clusters.rst
+
+
+.. _Engines:
+
+.. include:: engines.rst
+
+
+.. include:: neato_no_op.rst
+
+
+.. include:: unflatten.rst
+
+
+.. include:: custom_dot.rst
+
+
+.. _Sources:
+
+.. include:: raw_dot.rst
+
+
+.. include:: existing_files.rst
+
+
+.. include:: integration_with_viewers.rst
---
+++ packages/graphviz/graphviz-0.20.zip/graphviz-0.20/setup.py
@@ -0,0 +1,44 @@
+import pathlib
+from setuptools import setup, find_packages
+
+setup(
+    name='graphviz',
+    version='0.20',
+    author='Sebastian Bank',
+    author_email='sebastian.bank@uni-leipzig.de',
+    description='Simple Python interface for Graphviz',
+    keywords='graph visualization dot render',
+    license='MIT',
+    url='https://github.com/xflr6/graphviz',
+    project_urls={
+        'Documentation': 'https://graphviz.readthedocs.io',
+        'Changelog': 'https://graphviz.readthedocs.io/en/latest/changelog.html',
+        'Issue Tracker': 'https://github.com/xflr6/graphviz/issues',
+        'CI': 'https://github.com/xflr6/graphviz/actions',
+        'Coverage': 'https://codecov.io/gh/xflr6/graphviz',
+    },
+    packages=find_packages(),
+    platforms='any',
+    python_requires='>=3.7',
+    extras_require={
+        'dev': ['tox>=3', 'flake8', 'pep8-naming', 'wheel', 'twine'],
+        'test': ['pytest>=7',
+                 'pytest-mock>=3', 'mock>=4',
+                 'pytest-cov', 'coverage'],
+        'docs': ['sphinx>=4', 'sphinx-autodoc-typehints', 'sphinx-rtd-theme'],
+    },
+    long_description=pathlib.Path('README.rst').read_text(encoding='utf-8'),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Scientific/Engineering :: Visualization',
+    ],
+)
