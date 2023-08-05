3bb93fff4016a27fd40cfcf8cefd4a590616aa26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:44 +0000

    Add package sirisay/0.1.4/sirisay-0.1.4.tar.gz

---
+++ packages/sirisay/sirisay-0.1.4.tar.gz/sirisay-0.1.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='sirisay',
+    version='0.1.4',    
+    description='The fastes way to make your mac speak within python scripts.',
+    url='https://github.com/chrischma/siri_say',
+    author='Christoph Schmalfuß',
+    author_email='post@christoph-schmalfuss.de',
+    license='MIT',
+
+    classifiers=[
+        'Programming Language :: Python :: 3.9',
+    ],
+)
