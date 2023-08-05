00a3145b06c0713798e48785e33a04a471d4099b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:49 +0000

    Add package drawio-parser/0.0.7/drawio-parser-0.0.7.tar.gz

---
+++ packages/drawio-parser/drawio-parser-0.0.7.tar.gz/drawio-parser-0.0.7/src/drawio_parser/component.py
@@ -0,0 +1,62 @@
+from typing import Dict
+import json 
+
+class Component: 
+
+   
+
+    def __init__(self, id: str, attributes: Dict) -> None:
+        self.id = id
+        self.attributes = attributes
+        self.__create_styles(attributes.get('style'))
+        
+    def __create_styles(self, style_list: str):
+        self.styles = {}
+        if not style_list == None:
+            style_json = style_list.split(";")[-1]
+            if "{" in style_json:
+              self.styles = json.loads(style_json) 
+            
+            
+    def get_styles(self):
+        return self.styles
+
+    def get_value(self):
+        return self.attributes.get('value')
+    
+    def get_id(self):
+        return self.id
+    
+    def get_component_status(self):
+        return self.styles.get('cvc-component-status')
+
+    def get_lib_version(self):
+        return self.styles.get('cvc-lib-version')
+
+    def get_parent(self):
+        return self.attributes.get('parent')
+    
+    def get_icon_id(self):
+        return self.styles.get('cvc-icon-id')
+    
+    def get_architecture_layer(self):
+        return self.styles.get('cvc-architecture-layer')
+
+    def get_landscape_layer(self):
+        return self.styles.get('cvc-landscape-layer')
+
+    def get_technology(self):
+        return self.styles.get('cvc-technology')
+
+    def get_technology_framework(self):
+        return self.styles.get('cvc-technology-framework')   
+
+    def get_template_type(self):
+        return self.styles.get('cvc-template-type')
+
+    def get_template_name(self):
+        return self.styles.get('cvc-template-name')
+
+
+
+  
