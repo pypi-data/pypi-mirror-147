fc511eb930e9f20857894bb8b716adc5448b5cc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:21:58 +0000

    Add package linearAlgebraCalc/0.0.14/linearAlgebraCalc-0.0.14-py3-none-any.whl

---
+++ packages/linearAlgebraCalc/linearAlgebraCalc-0.0.14-py3-none-any.whl/linearAlgebraCalc/basicOperations.py
@@ -0,0 +1,51 @@
+from linearAlgebraCalc.globalFunctions import errorTraceback, printError, arrDim
+
+def matrixMultiply(a, b):
+
+    if len(b) == len(a[0]):
+
+        result = [[0 for x in range(len(b[0]))] for y in range(len(a))]
+        
+        for i in range(len(a)):
+            for j in range(len(b[0])):
+                result[i][j] = 0
+                for x in range(len(b[0])):           
+                    result[i][j] += (a[i][x] * b[x][j])
+
+        return result
+    
+    else:
+        errorTraceback()
+        printError('Columns of first matrix must match rows of second matrix!')
+
+def matrixAdd(a, b):
+
+    if arrDim(a) == arrDim(b):
+
+        result = []
+        for i in range(len(a)):
+            appendList = []
+            for k in range(len(a[0])):
+                appendList.append(a[i][k] + b[i][k])
+            result.append(appendList)
+
+        return result
+    else:
+            errorTraceback()
+            printError('Both matrices must have the same dimensions!')
+
+def matrixSub(a, b):
+
+    if arrDim(a) == arrDim(b):
+
+        result = []
+        for i in range(len(a)):
+            appendList = []
+            for k in range(len(a[0])):
+                appendList.append(a[i][k] - b[i][k])
+            result.append(appendList)
+
+        return result
+    else:
+            errorTraceback()
+            printError('Both matrices must have the same dimensions!')
