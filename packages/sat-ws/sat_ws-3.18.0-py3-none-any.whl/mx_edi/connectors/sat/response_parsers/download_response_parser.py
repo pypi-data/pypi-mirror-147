b67247a1209d5148af239c77fad39631336a1eaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:17:06 +0000

    Add package sat-ws/3.18.0/sat_ws-3.18.0-py3-none-any.whl

---
+++ packages/sat-ws/sat_ws-3.18.0-py3-none-any.whl/mx_edi/connectors/sat/response_parsers/download_response_parser.py
@@ -0,0 +1,18 @@
+from typing import Dict
+
+import xmltodict
+
+from .response_parser import ResponseParser
+
+
+class DownloadParser(ResponseParser):
+    @staticmethod
+    def parse(response: str) -> Dict[str, str]:
+        """Gets the Download data from the raw response"""
+        response_dict = xmltodict.parse(response)
+        package = response_dict["Envelope"]["Body"]["RespuestaDescargaMasivaTercerosSalida"][
+            "Paquete"
+        ]
+        return {
+            "Content": package,
+        }
