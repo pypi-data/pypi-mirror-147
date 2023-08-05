329b8ae0c7c60173ff578312c60ed6f1de25188e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:19:31 +0000

    Add package PyScoreSaber/1.0.5/PyScoreSaber-1.0.5.tar.gz

---
+++ packages/PyScoreSaber/PyScoreSaber-1.0.5.tar.gz/PyScoreSaber-1.0.5/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+# Requirements
+requirements = []
+with open("requirements.txt", "r", encoding="utf-8") as f:
+    requirements += f.read().splitlines()
+
+setuptools.setup(
+    name="PyScoreSaber",
+    version="1.0.5",
+    author="LuCkEr-",
+    author_email="lucker@lucker.xyz",
+    description="Score Saber API wrapper",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Kiyomi-Parents/PyScoreSaber",
+    project_urls={
+        "Bug Tracker": "https://github.com/Kiyomi-Parents/PyScoreSaber/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.8",
+    install_requires=requirements
+)
