a3c94ed817995834a89881d59bd414172e1bff4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:13 +0000

    Add package pwntools-tube-websocket/0.0.2/pwntools-tube-websocket-0.0.2.tar.gz

---
+++ packages/pwntools-tube-websocket/pwntools-tube-websocket-0.0.2.tar.gz/pwntools-tube-websocket-0.0.2/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="pwntools-tube-websocket",
+    version="0.0.2",
+    author="Frank",
+    author_email="frankli0324@hotmail.com",
+    description="websocket tube for pwntools",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires='>=3.7',
+    install_requires=["websocket-client", "pwntools"],
+)
