567400e27da2d04b4cbff5844899e6d85448bbcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:36:29 +0000

    Add package sql-query-parser/0.0.1/sql-query-parser-0.0.1.tar.gz

---
+++ packages/sql-query-parser/sql-query-parser-0.0.1.tar.gz/sql-query-parser-0.0.1/README.md
@@ -0,0 +1,8 @@
+# SQL-PARSER
+
+### ALPHA VERSION
+
+An advanced python sql query pareser
+
+## Quickstart
+Running on python 3.10
---
+++ packages/sql-query-parser/sql-query-parser-0.0.1.tar.gz/sql-query-parser-0.0.1/sql_query_parser/keywords/keyword_parser.py
@@ -0,0 +1,20 @@
+import os
+from typing import Tuple
+
+from sql_query_parser import keywords
+from sql_query_parser.configurations import Configurations
+
+
+def compute(
+        word: str,
+        config: Configurations
+) -> Tuple[bool, Configurations]:
+    for class_ in keywords.__dict__.items():
+        if class_[0].startswith("_") or \
+                class_[0] == os.path.basename(__file__).replace(".py", "") or \
+                class_[0].endswith("_test"):
+            continue
+        result, config = class_[1].compute(word=word, config=config)
+        if result is True:
+            return True, config
+    return False, config
