c4e91accbd2f2f96555e982b9615f8a6bee42822
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:26:19 +0000

    Add package q5/0.1.0/q5-0.1.0-cp39-cp39-win_amd64.whl

---
+++ packages/q5/q5-0.1.0-cp39-cp39-win_amd64.whl/q5/__init__.py
@@ -0,0 +1,11 @@
+from .engine import *  #noqa F403
+from .app import BaseApp, Vector
+
+try:
+    from ._version import version
+    __version__ = version
+except ImportError:
+    pass
+
+__author__ = 'eqs'
+__all__ = [BaseApp, Vector]
