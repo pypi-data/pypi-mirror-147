73867e88853ebb34c2afb30372e925a474189e3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:12 +0000

    Add package dj-rest-auth-social/2.2.4/dj_rest_auth_social-2.2.4-py2.py3-none-any.whl

---
+++ packages/dj-rest-auth-social/dj_rest_auth_social-2.2.4-py2.py3-none-any.whl/dj_rest_auth/__version__.py
@@ -0,0 +1,8 @@
+__title__ = "dj-rest-auth-social"
+__description__ = "Authentication and Registration in Django Rest Framework."
+__url__ = "https://github.com/robertwt7/dj-rest-auth-social"
+__version__ = "2.2.4"
+__author__ = "@robertwt7 https://github.com/robertwt7"
+__author_email__ = "robert@sharkware.org"
+__license__ = "MIT"
+__copyright__ = "Copyright 2020 @robertwt7 https://github.com/robertwt7"
