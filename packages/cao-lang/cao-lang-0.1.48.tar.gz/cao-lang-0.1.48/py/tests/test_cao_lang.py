b7ddcc26d1356e144663d6646f3fb1065264e9fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:16 +0000

    Add package cao-lang/0.1.48/cao-lang-0.1.48.tar.gz

---
+++ packages/cao-lang/cao-lang-0.1.48.tar.gz/cao-lang-0.1.48/py/tests/test_cao_lang.py
@@ -0,0 +1,50 @@
+import cao_lang as caoc
+
+
+def test_compile_and_run():
+    """
+    Test if we can take a simple program and parse, compile and run it without error
+    """
+
+    PROGRAM_YAML = """
+lanes:
+    main: 
+        cards:
+            - ty: ScalarInt
+              val: 5
+            - ty: ScalarInt
+              val: 5
+            - ty: Add
+"""
+
+    program = caoc.CompilationUnit.from_yaml(PROGRAM_YAML)
+    options = caoc.CompilationOptions()
+
+    program = caoc.compile(program, options)
+
+    caoc.run(program)
+
+
+def test_get_version():
+    v = caoc.native_version()
+    assert isinstance(v, str)
+
+
+def test_json():
+    PROGRAM_JSON = """
+    {
+        "lanes": {
+            "main": {
+                "cards": [
+                    { "ty": "Noop" }
+                ]
+            }
+        }
+    }
+    """
+    program = caoc.CompilationUnit.from_json(PROGRAM_JSON)
+    options = caoc.CompilationOptions()
+
+    program = caoc.compile(program, options)
+
+    caoc.run(program)
