462bc64a8eed286b4db6b607d9d048703626ba0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:43:32 +0000

    Add package py-auto-recon/0.0.1/py-auto-recon-0.0.1.tar.gz

---
+++ packages/py-auto-recon/py-auto-recon-0.0.1.tar.gz/py-auto-recon-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='py-auto-recon',
+    version='0.0.1',
+    description='asd',
+    long_description='asdasdasd',
+    url='https://www.mustansirg.in/pyrecon',
+    author='Mustansir Godhrawala',
+    author_email='me@mustansirg.in',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
