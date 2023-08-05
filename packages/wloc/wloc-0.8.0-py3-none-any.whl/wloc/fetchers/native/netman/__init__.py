91a523b181b6764d056b3617fe3a0761ccd92a15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:33 +0000

    Add package wloc/0.8.0/wloc-0.8.0-py3-none-any.whl

---
+++ packages/wloc/wloc-0.8.0-py3-none-any.whl/wloc/fetchers/native/netman/__init__.py
@@ -0,0 +1,32 @@
+# coding=utf-8
+
+# SPDX-FileCopyrightText: 2015-2022 EasyCoding Team
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from NetworkManager import NetworkManager, Wireless
+from dbus.mainloop.glib import DBusGMainLoop
+
+from ...native import NativeBackendCommon
+from ....helpers import Helpers
+
+
+class NetworkManagerNativeAPI(NativeBackendCommon):
+    """
+    Class for working with Network Manager API using public D-Bus
+    methods.
+    """
+
+    def _fetch_list(self):
+        """
+        Fetches the list of available Wi-Fi networks using public
+        D-Bus methods.
+        """
+        # Applying workaround to python-networkmanager#84...
+        DBusGMainLoop(set_as_default=True)
+
+        # Using DBus to fetch the list of available networks...
+        for nmdevice in NetworkManager.GetDevices():
+            if isinstance(nmdevice, Wireless):
+                for accesspoint in nmdevice.AccessPoints:
+                    self._network_list.append([accesspoint.HwAddress, Helpers.percents2dbm(accesspoint.Strength)])
