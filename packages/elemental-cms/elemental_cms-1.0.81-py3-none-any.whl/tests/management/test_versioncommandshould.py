60d5a0e2cbb1faa33f7ff70a8e70a3a3ff8400b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:08 +0000

    Add package elemental-cms/1.0.81/elemental_cms-1.0.81-py3-none-any.whl

---
+++ packages/elemental-cms/elemental_cms-1.0.81-py3-none-any.whl/tests/management/test_versioncommandshould.py
@@ -0,0 +1,18 @@
+import json
+import os
+
+from click.testing import CliRunner
+from elementalcms.management import cli
+
+
+class TestVersionCommandShould:
+
+    def test_display_current_version(self, default_settings_fixture):
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            os.makedirs('settings')
+            with open('settings/prod.json', 'w') as f:
+                f.write(json.dumps(default_settings_fixture))
+            # noinspection PyTypeChecker
+            result = runner.invoke(cli, ['version'])
+            assert result.exit_code == 0
