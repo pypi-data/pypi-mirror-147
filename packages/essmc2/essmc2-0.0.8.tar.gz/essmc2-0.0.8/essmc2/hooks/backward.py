ea65d05d8671408aa3d06aa52dc8904b60c8b96a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:25:28 +0000

    Add package essmc2/0.0.8/essmc2-0.0.8.tar.gz

---
+++ packages/essmc2/essmc2-0.0.8.tar.gz/essmc2-0.0.8/essmc2/hooks/backward.py
@@ -0,0 +1,25 @@
+# Copyright 2021 Alibaba Group Holding Limited. All Rights Reserved.
+import warnings
+
+from .hook import Hook
+from .registry import HOOKS
+
+_DEFAULT_BACKWARD_PRIORITY = 0
+
+
+@HOOKS.register_class()
+class BackwardHook(Hook):
+    def __init__(self, **kwargs):
+        priority = kwargs.pop("priority") if "priority" in kwargs else _DEFAULT_BACKWARD_PRIORITY
+        super(BackwardHook, self).__init__(priority=priority)
+
+    def after_iter(self, solver):
+        if solver.optimizer is not None \
+                and solver.is_train_mode:
+            if solver.loss is None:
+                warnings.warn("solver.loss should not be None in train mode, remember to call solver._reduce_scalar()!")
+                return
+            solver.optimizer.zero_grad()
+            solver.loss.backward()
+            solver.optimizer.step()
+            solver.loss = None
