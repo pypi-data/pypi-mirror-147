68eb30168d9bddfa5b66a6a0365ea4d6f174a96e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:20:49 +0000

    Add package bice/0.3.3/bice-0.3.3.tar.gz

---
+++ packages/bice/bice-0.3.3.tar.gz/bice-0.3.3/bice/pde/__init__.py
@@ -0,0 +1,11 @@
+
+from .pde import PartialDifferentialEquation
+from .finite_differences import FiniteDifferencesEquation
+from .pseudospectral import PseudospectralEquation
+
+
+__all__ = [
+    "PartialDifferentialEquation",
+    "FiniteDifferencesEquation",
+    "PseudospectralEquation"
+]
