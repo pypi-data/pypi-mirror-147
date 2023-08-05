9c1588a7f64d0aa2e04f416340610a7cabc6e66a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:47 +0000

    Add package gcp-secretmanager-cache/0.2.0/gcp_secretmanager_cache-0.2.0-py3-none-any.whl

---
+++ packages/gcp-secretmanager-cache/gcp_secretmanager_cache-0.2.0-py3-none-any.whl/gcp_secretmanager_cache/__init__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""gcp_secretmanager_cache
+
+Some utilities to cache GCP secrets with a bit of intelligence and manage when they rotate and
+trades off reliability, usability, predictability and performance vs accuracy and timeliness
+
+"""
+
+from __future__ import absolute_import
+
+from gcp_secretmanager_cache.cache_secret import GCPCachedSecret
+from gcp_secretmanager_cache.exceptions import NoActiveSecretVersion, \
+    SecretCacheError, \
+    NewSecretCreateError, \
+    SecretRotatorError
+from gcp_secretmanager_cache.decorators import InjectKeywordedSecretString, InjectSecretString
+from gcp_secretmanager_cache.managers import SecretRotator, \
+    SecretRotatorMechanic, \
+    ChangeSecretMeta, \
+    APIKeyRotator, \
+    SAKeyRotator
+from ._version import __version__
+
+__all__ = ["__version__",
+           "NoActiveSecretVersion",
+           "GCPCachedSecret",
+           "InjectSecretString",
+           "SecretCacheError",
+           "InjectKeywordedSecretString",
+           "SecretRotator",
+           "SecretRotatorMechanic",
+           "ChangeSecretMeta",
+           "NewSecretCreateError",
+           "SecretRotatorError",
+           "APIKeyRotator",
+           "SAKeyRotator"]
