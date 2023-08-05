f257fb765bc9ae14b944b79e213ab368f46a3233
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:19:42 +0000

    Add package marquedown/0.6.0/marquedown-0.6.0-py3-none-any.whl

---
+++ packages/marquedown/marquedown-0.6.0-py3-none-any.whl/marquedown/__init__.py
@@ -0,0 +1,23 @@
+__version__ = '0.6.0'
+
+import markdown as md
+
+
+def marquedown(document: str, **kwargs):
+    """Convert both Marquedown and Markdown into HTML."""
+
+    if kwargs.get('citation', True):
+        from .citation import citation
+        document = citation(document)
+
+    if kwargs.get('video', True):
+        from .video import video
+        document = video(document)
+
+    if kwargs.get('bblike', True):
+        from .bblike import bblike
+        document = bblike(document)
+        
+    html = md.markdown(document)
+
+    return html
