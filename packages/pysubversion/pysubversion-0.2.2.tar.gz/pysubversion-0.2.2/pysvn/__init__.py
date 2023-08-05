b1f9b424cc45e5631f7ad7c451d266c35051844d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:16:26 +0000

    Add package pysubversion/0.2.2/pysubversion-0.2.2.tar.gz

---
+++ packages/pysubversion/pysubversion-0.2.2.tar.gz/pysubversion-0.2.2/pysvn/__init__.py
@@ -0,0 +1,11 @@
+from pysvn.client import Client
+from pysvn.utils import *
+from pysvn.models import *
+from pysvn.errors import *
+
+
+name = "pysvn"
+
+__doc__ = """
+A Python package that can operate svn, provide log, diff, numstat operation.
+"""
