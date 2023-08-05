941cb17de370409c87c605ac51ff171549b7e43e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:56 +0000

    Add package pierogis/0.4.1/pierogis-0.4.1-cp37-cp37m-manylinux1_x86_64.whl

---
+++ packages/pierogis/pierogis-0.4.1-cp37-cp37m-manylinux1_x86_64.whl/pierogis/ingredients/__init__.py
@@ -0,0 +1,16 @@
+"""
+all Ingredient types live here
+"""
+
+from .crop import Crop
+from .dish import Dish
+from .flip import Flip
+from .ingredient import Ingredient
+from .mmpx import MMPX
+from .pierogi import Pierogi
+from .quantize import Quantize, SpatialQuantize
+from .recipe import Recipe
+from .resize import Resize
+from .rotate import Rotate
+from .seasonings import Threshold
+from .sort import Sort
