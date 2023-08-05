a49d34a58b37eefbac0db6c235e2f00069d4daeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:16 +0000

    Add package Flask-Limiter/2.3.2/Flask_Limiter-2.3.2-py3-none-any.whl

---
+++ packages/Flask-Limiter/Flask_Limiter-2.3.2-py3-none-any.whl/flask_limiter/typing.py
@@ -0,0 +1,22 @@
+from typing_extensions import ParamSpec
+from typing import cast
+from typing import Callable, Dict, List, Optional, Union, Sequence, Set, Tuple, TypeVar
+
+R = TypeVar("R")
+P = ParamSpec("P")
+
+
+__all__ = [
+    "R",
+    "P",
+    "Callable",
+    "Dict",
+    "List",
+    "Optional",
+    "Union",
+    "cast",
+    "Sequence",
+    "Tuple",
+    "TypeVar",
+    "Set",
+]
