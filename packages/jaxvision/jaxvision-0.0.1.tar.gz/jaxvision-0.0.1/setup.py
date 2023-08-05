a43231eb0de6ebf3686e53cd926f1843baaf6ff5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:26:27 +0000

    Add package jaxvision/0.0.1/jaxvision-0.0.1.tar.gz

---
+++ packages/jaxvision/jaxvision-0.0.1.tar.gz/jaxvision-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='jaxvision',
+    version='0.0.1',
+    description='Computer Vision in Jax',
+    long_description='',
+    url='https://github.com/netw0rkf10w/jaxvision',
+    author='D.Khue Le-Huu',
+    author_email='me@khue.fr',
+    packages=[]
+)
