72a083e12285e599d8b67b20488f2e7aa6b1eb0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:25:28 +0000

    Add package essmc2/0.0.8/essmc2-0.0.8-py3-none-any.whl

---
+++ packages/essmc2/essmc2-0.0.8-py3-none-any.whl/essmc2/datasets/__init__.py
@@ -0,0 +1,6 @@
+# Copyright 2021 Alibaba Group Holding Limited. All Rights Reserved.
+
+from .base_dataset import BaseDataset
+from .image_classify_json_dataset import ImageClassifyJsonDataset
+from .registry import DATASETS
+from .videos import *
