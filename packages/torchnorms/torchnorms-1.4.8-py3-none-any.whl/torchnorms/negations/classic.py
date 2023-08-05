cd1de94b543263b5d7fd548c1e63364ca8c335f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:48:58 +0000

    Add package torchnorms/1.4.8/torchnorms-1.4.8-py3-none-any.whl

---
+++ packages/torchnorms/torchnorms-1.4.8-py3-none-any.whl/torchnorms/negations/classic.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+import torch
+from torch import Tensor
+from torchnorms.negations.base import BaseNegation
+
+
+class StandardNegation(BaseNegation):
+    def __init__(self) -> None:
+        super().__init__()
+        self.__name__ = 'standard'
+
+    @classmethod
+    def __call__(cls,
+                 a: Tensor) -> Tensor:
+        return 1.0 - a
+
+
+class AffineNegation(BaseNegation):
+    def __init__(self) -> None:
+        super().__init__()
+        self.__name__ = 'affine'
+        self.linear = torch.nn.Linear(1, 1, bias=True)
+        self.linear.weight[0, 0].data = 1
+        self.linear.bias[0].data = - 1
+
+    def __call__(self,
+                 a: Tensor) -> Tensor:
+        return self.linear(a)
+
+
+class StrictNegation(BaseNegation):
+    def __init__(self) -> None:
+        super().__init__()
+        self.__name__ = 'strict'
+
+    @classmethod
+    def __call__(cls,
+                 a: Tensor) -> Tensor:
+        return 1.0 - torch.pow(a, 2)
+
+
+class StrictCosNegation(BaseNegation):
+    def __init__(self) -> None:
+        super().__init__()
+        self.__name__ = 'strict_cosine'
+
+    @classmethod
+    def __call__(cls,
+                 a: Tensor) -> Tensor:
+        pi = torch.acos(torch.zeros(1)).item() * 2
+        res = 0.5 * (torch.cos(pi * a))
+        return res
