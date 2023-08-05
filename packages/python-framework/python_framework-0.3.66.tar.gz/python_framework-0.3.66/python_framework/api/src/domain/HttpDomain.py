a306fe6c6dfea2f6fb24be1a8fe6a920cebfa28f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:48 +0000

    Add package python-framework/0.3.66/python_framework-0.3.66.tar.gz

---
+++ packages/python-framework/python_framework-0.3.66.tar.gz/python_framework-0.3.66/python_framework/api/src/domain/HttpDomain.py
@@ -0,0 +1,31 @@
+class HeaderKey:
+    CONTENT_TYPE = 'Content-Type'
+
+class HeaderValue:
+    APPLICATION_JSON = 'application/json'
+    MULTIPART_X_MIXED_REPLACE = 'multipart/x-mixed-replace'
+    TEXT_HTML = 'text/html; charset=UTF-8'
+    TEXT_PLAIN = 'text/plain'
+    FROM_DATA = 'multipart/form-data; boundary=value'
+    DEFAULT_CONTENT_TYPE = APPLICATION_JSON
+
+
+class Verb:
+    GET = 'GET'
+    POST = 'POST'
+    PUT = 'PUT'
+    DELETE = 'DELETE'
+    PATCH = 'PATCH'
+    OPTIONS = 'OPTIONS'
+
+CONTROLLER_CONTEXT = 'Controller'
+CLIENT_CONTEXT = 'Client'
+LISTENER_CONTEXT = 'Listener'
+EMITTER_CONTEXT = 'Emitter'
+SCHEDULER_CONTEXT = 'Scheduler'
+
+
+REQUEST_HEADERS_KEY = 'requestHeaders'
+RESPONSE_HEADERS_KEY = 'responseHeaders'
+REQUEST_BODY_KEY = 'requestBody'
+RESPONSE_BODY_KEY = 'responseBody'
