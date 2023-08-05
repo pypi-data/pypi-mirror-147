33b1c1d8ac353abd054e50558d24a2004eded6e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:27:47 +0000

    Add package iamjjintta/0.0.1/iamjjintta-0.0.1-py3-none-any.whl

---
+++ packages/iamjjintta/iamjjintta-0.0.1-py3-none-any.whl/iamjjintta/iamjjintta.py
@@ -0,0 +1,12 @@
+
+import webbrowser
+
+
+def open_blog():
+    url = 'https://iamjjintta.tistory.com/'
+    webbrowser.open(url)
+
+
+def open_github():
+    url = 'https://github.com/iam-jjintta'
+    webbrowser.open(url)
