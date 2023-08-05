7b7618e4d58856c84e475f90903c93d5b9cc4829
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:17:16 +0000

    Add package dailymotion-api/0.4.0/dailymotion_api-0.4.0.tar.gz

---
+++ packages/dailymotion-api/dailymotion_api-0.4.0.tar.gz/dailymotion_api-0.4.0/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['dailymotion_api',
+ 'dailymotion_api.routes',
+ 'dailymotion_api.static',
+ 'dailymotion_api.templates']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Jinja2>=3.0.3,<4.0.0', 'fastapi>=0.74.1,<0.75.0', 'uvicorn>=0.17.5,<0.18.0']
+
+setup_kwargs = {
+    'name': 'dailymotion-api',
+    'version': '0.4.0',
+    'description': '',
+    'long_description': None,
+    'author': 'EmilRueda',
+    'author_email': 'vladimir.rueda@udea.edu.co',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
