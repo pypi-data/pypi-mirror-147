f0077a112e48a10c8b95dba3921d5dfa419e9581
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:37 +0000

    Add package plastic-yellow-bird/0.1.dev0/plastic-yellow-bird-0.1.dev0.tar.gz

---
+++ packages/plastic-yellow-bird/plastic-yellow-bird-0.1.dev0.tar.gz/plastic-yellow-bird-0.1.dev0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='plastic-yellow-bird',
+    version='0.1dev',
+    packages=['pyb',],
+    license='MIT',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown'
+)
