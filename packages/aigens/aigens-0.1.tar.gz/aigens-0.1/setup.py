9d9c7421c052e457089f9814583662b1d7c0e962
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:33 +0000

    Add package aigens/0.1/aigens-0.1.tar.gz

---
+++ packages/aigens/aigens-0.1.tar.gz/aigens-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages  # type: ignore
+
+
+setup(
+    name='aigens',
+    version='0.1',
+    license='MIT',
+    author='TryCrime',
+    author_email='lol@trycri.me',
+    url='https://bitbucket.org/trycrimegroup/aigens/',
+    keywords='rapidapi text generation',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=['requests',],
+)
