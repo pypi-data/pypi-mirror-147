ddb3390dcf9732cd49ad120975baecc4bbf1d096
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:18:23 +0000

    Add package benchmark-runner/1.0.333/benchmark_runner-1.0.333-py3-none-any.whl

---
+++ packages/benchmark-runner/benchmark_runner-1.0.333-py3-none-any.whl/benchmark_runner/benchmark_operator/benchmark_operator_exceptions.py
@@ -0,0 +1,32 @@
+
+class BenchmarkOperatorError(Exception):
+    """ Base class for all benchmark operator error classes.
+        All exceptions raised by the benchmark runner library should inherit from this class. """
+    pass
+
+
+class ODFNonInstalled(BenchmarkOperatorError):
+    """
+    This class is error that ODF operator is not installed
+    """
+    def __init__(self):
+        self.message = "ODF is not installed, set 'ODF_PVC' to False"
+        super(ODFNonInstalled, self).__init__(self.message)
+
+
+class SystemMetricsRequiredElasticSearch(BenchmarkOperatorError):
+    """
+    This class for raise error when running system metrics without ElasticSearch
+    """
+    def __init__(self):
+        self.message = "System metrics is required ElasticSearch, set 'SYSTEM_METRICS' to False"
+        super(SystemMetricsRequiredElasticSearch, self).__init__(self.message)
+
+
+class PrometheusSnapshotFailed(BenchmarkOperatorError):
+    """
+    Prometheus snapshot failed
+    """
+    def __init__(self, err):
+        self.message = f'Prometheus snapshot failed: {err}'
+        super(PrometheusSnapshotFailed, self).__init__(self.message)
