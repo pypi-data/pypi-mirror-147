56cf854b75d6edd91cbdf1d29ad41c5d74c79080
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:51 +0000

    Add package tablebase/1.2/tablebase-1.2.tar.gz

---
+++ packages/tablebase/tablebase-1.2.tar.gz/tablebase-1.2/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+setup(
+      name='tablebase',
+      version='1.2',
+      description='To make tables in python as easy as printing to the console',
+      long_description="Table Base is a python package to make coding with tables effortless. You can also read from a CSV file.",
+      author='Maximilian Lange',
+      license='MIT',
+      author_email='maxhlange@gmail.com',
+      url='https://pypi.org/project/tablebase/',
+      py_modules=['tablebase'],
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+      ])
