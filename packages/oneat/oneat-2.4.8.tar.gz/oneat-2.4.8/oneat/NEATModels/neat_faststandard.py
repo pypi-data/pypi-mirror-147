657ba5152acb743c0fb85f6e02e5718882e4c9a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:27:47 +0000

    Add package oneat/2.4.8/oneat-2.4.8.tar.gz

---
+++ packages/oneat/oneat-2.4.8.tar.gz/oneat-2.4.8/oneat/NEATModels/neat_faststandard.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 28 13:49:35 2021
+
+@author: vkapoor
+"""
+
+from .neat_goldstandard import NEATDynamic
+
+class NEATSynamic(NEATDynamic):
+    
+
+    def __init__(self, config, model_dir, model_name, catconfig = None, cordconfig = None):
+
+        super().__init__(config = config, model_dir = model_dir, model_name = model_name, catconfig = catconfig, cordconfig = cordconfig)
+        
+    def predict_synamic(self,imagename, savedir, n_tiles = (1,1), overlap_percent = 0.8, event_threshold = 0.5, event_confidence = 0.5, iou_threshold = 0.1, fidelity = 5,
+     downsamplefactor = 1, erosion_iterations = 10,start_project_mid = 4, end_project_mid = 4, maskmodel = None, segdir = None, normalize = True, center_oneat = True):
+
+        self.predict(imagename,savedir,n_tiles = n_tiles, overlap_percent = overlap_percent, event_threshold = event_threshold, event_confidence = event_confidence, iou_threshold = iou_threshold, 
+        fidelity = fidelity, start_project_mid = start_project_mid, end_project_mid = end_project_mid, downsamplefactor = downsamplefactor, erosion_iterations = erosion_iterations, marker_tree = None,
+         remove_markers = None,  normalize = normalize, center_oneat = center_oneat )
+
