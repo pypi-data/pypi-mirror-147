7f5d3a245069b70a199adc2805f9f027326eba2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:51 +0000

    Add package apple-data/1.0.424/apple-data-1.0.424.tar.gz

---
+++ packages/apple-data/apple-data-1.0.424.tar.gz/apple-data-1.0.424/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['apple_data']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'apple-data',
+    'version': '1.0.424',
+    'description': 'Static data from https://docs.hackdiffe.rent',
+    'long_description': '# Apple Data resource package\n\nThis package contains static assets from <https://docs.hackdiffe.rent>\n\nThe contents of the `_data` folder are included in `share/`',
+    'author': 'Rick Mark',
+    'author_email': 'rickmark@outlook.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+}
+
+
+setup(**setup_kwargs)
