78a3bd4ec2c56f3aed8411a1c4a4d6feb98f5016
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:49:32 +0000

    Add package pdcool/1.1.0/pdcool-1.1.0.tar.gz

---
+++ packages/pdcool/pdcool-1.1.0.tar.gz/pdcool-1.1.0/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='pdcool',
+    version='1.1.0',
+    keywords='pdcool',
+    description='a library for revang developer',
+    license='MIT License',
+    url='https://github.com/revang/pdcool',
+    author='revang',
+    author_email='revang.alternative@foxmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'requests==2.26.0',
+        'pymysql==1.0.2',
+        'sqlalchemy==1.4.28',
+        'pandas==1.3.5'
+    ],
+)
---
+++ packages/pdcool/pdcool-1.1.0.tar.gz/pdcool-1.1.0/tests/test_database.py
@@ -0,0 +1,13 @@
+import unittest
+from pdtool.database import DBUtil
+
+
+class DatabaseTest(unittest.TestCase):
+    def test_queryone(self):
+        db = DBUtil()
+        val = db.queryone("select * from stock")[0]
+        self.assertIsNotNone(val)
+
+
+if __name__ == "__main__":
+    unittest.main()
