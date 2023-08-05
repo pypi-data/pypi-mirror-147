903f8b7eb44ca1993f4669ff1eb0bc24b38f9dc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:05 +0000

    Add package azion-api-client/0.0.1a0/azion-api-client-0.0.1a0.tar.gz

---
+++ packages/azion-api-client/azion-api-client-0.0.1a0.tar.gz/azion-api-client-0.0.1a0/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import io
+from setuptools import find_packages, setup
+from os import path
+
+
+setup(
+    name='azion-api-client',
+    version='0.0.1.a0',
+    description="A Python API client for Azion management and automation",
+    long_description_content_type='text/markdown',
+    url='https://github.com/diegosouza/azion-api-client',
+    include_package_data=True,
+    license='GPL3',
+    author='Diego de Souza Mendes',
+    author_email='diegosouza.br@gmail.com',
+    python_requires='>=3.8',
+    entry_points={"console_scripts": ["azion = azion.cli:cli"]},
+    install_requires=[
+        'pendulum',
+        'requests',
+        'click',
+    ]
+)
