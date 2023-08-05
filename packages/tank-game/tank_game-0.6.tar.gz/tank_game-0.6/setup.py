3003573bffe848938bbf789c81a60094ff1b243f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:49 +0000

    Add package tank-game/0.6/tank_game-0.6.tar.gz

---
+++ packages/tank-game/tank_game-0.6.tar.gz/tank_game-0.6/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tank_game',
+    version='0.6',
+    author_email='votintsev_99@mail.ru',
+    packages=find_packages('src'),
+    zip_safe=False,
+)
