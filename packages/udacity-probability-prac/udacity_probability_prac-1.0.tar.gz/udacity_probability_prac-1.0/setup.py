cf8a688f264488514d0741b198af22267b72705a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:51 +0000

    Add package udacity-probability-prac/1.0/udacity_probability_prac-1.0.tar.gz

---
+++ packages/udacity-probability-prac/udacity_probability_prac-1.0.tar.gz/udacity_probability_prac-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='udacity_probability_prac',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['udacity_probability_prac'],
+      author = 'Chan Liang',
+      author_email = 'mitbbskg@gmail.com',
+      zip_safe=False)
