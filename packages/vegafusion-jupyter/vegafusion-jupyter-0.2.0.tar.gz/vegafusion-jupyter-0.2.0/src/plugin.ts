ac87b82065d056dabfd421afafaa59689e03f665
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:23 +0000

    Add package vegafusion-jupyter/0.2.0/vegafusion-jupyter-0.2.0.tar.gz

---
+++ packages/vegafusion-jupyter/vegafusion-jupyter-0.2.0.tar.gz/vegafusion-jupyter-0.2.0/src/plugin.ts
@@ -0,0 +1,58 @@
+/*
+ * VegaFusion
+ * Copyright (C) 2022 VegaFusion Technologies LLC
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program.
+ * If not, see http://www.gnu.org/licenses/.
+ */
+
+import { Application, IPlugin } from '@phosphor/application';
+
+import { Widget } from '@phosphor/widgets';
+
+import { IJupyterWidgetRegistry } from '@jupyter-widgets/base';
+
+import * as widgetExports from './widget';
+
+import { MODULE_NAME, MODULE_VERSION } from './version';
+
+const EXTENSION_ID = 'vegafusion-jupyter:plugin';
+
+/**
+ * The example plugin.
+ */
+const examplePlugin: IPlugin<Application<Widget>, void> = {
+  id: EXTENSION_ID,
+  requires: [IJupyterWidgetRegistry],
+  activate: activateWidgetExtension,
+  autoStart: true,
+} as unknown as IPlugin<Application<Widget>, void>;
+// the "as unknown as ..." typecast above is solely to support JupyterLab 1
+// and 2 in the same codebase and should be removed when we migrate to Lumino.
+
+export default examplePlugin;
+
+/**
+ * Activate the widget extension.
+ */
+function activateWidgetExtension(
+  app: Application<Widget>,
+  registry: IJupyterWidgetRegistry
+): void {
+  registry.registerWidget({
+    name: MODULE_NAME,
+    version: MODULE_VERSION,
+    exports: widgetExports,
+  });
+}
