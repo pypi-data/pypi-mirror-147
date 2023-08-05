f27cd157477694d72579ac7be4358f09969a7087
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:51:02 +0000

    Add package slack-async-alert/1.0.2/slack_async_alert-1.0.2-py3-none-any.whl

---
+++ packages/slack-async-alert/slack_async_alert-1.0.2-py3-none-any.whl/slack_alert/run.py
@@ -0,0 +1,22 @@
+import typer
+from .configure import set_configure
+from .slack import SlackClient
+
+
+app = typer.Typer()
+
+
+@app.command()
+def success(command):
+    client = SlackClient()
+    client(f"[Success]process command: {command} \nwhere: {client.hardware_identifier}")
+
+
+@app.command()
+def fail(command):
+    client = SlackClient()
+    client(f"[Fail]process command: {command} \nwhere: {client.hardware_identifier}")
+
+
+if __name__ == "__main__":
+    app()
