a465a167d5a656be5a8a9deae59e5bf43ab29621
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:15:28 +0000

    Add package clip-server/0.1.15/clip-server-0.1.15.tar.gz

---
+++ packages/clip-server/clip-server-0.1.15.tar.gz/clip-server-0.1.15/clip_server/executors/clip_torch.py
@@ -0,0 +1,114 @@
+import os
+import numpy as np
+from multiprocessing.pool import ThreadPool, Pool
+from typing import Optional, List, Tuple
+
+from jina import Executor, requests, DocumentArray
+from jina.logging.logger import JinaLogger
+
+from clip_server.model import clip
+
+
+class CLIPEncoder(Executor):
+    def __init__(
+        self,
+        name: str = 'ViT-B/32',
+        device: Optional[str] = None,
+        jit: bool = False,
+        num_worker_preprocess: int = 4,
+        minibatch_size: int = 64,
+        pool_backend: str = 'thread',
+        **kwargs,
+    ):
+        super().__init__(**kwargs)
+        self.logger = JinaLogger(self.__class__.__name__)
+
+        import torch
+
+        if not device:
+            self._device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        else:
+            self._device = device
+
+        if self._device != 'cuda' and (not os.environ.get('OMP_NUM_THREADS')):
+            num_threads = torch.get_num_threads() // self.runtime_args.replicas
+            if num_threads < 2:
+                self.logger.warning(
+                    f'Too many encoder replicas ({self.runtime_args.replicas})'
+                )
+
+            # NOTE: make sure to set the threads right after the torch import,
+            # and `torch.set_num_threads` always take precedence over environment variables `OMP_NUM_THREADS`.
+            # For more details, please see https://pytorch.org/docs/stable/generated/torch.set_num_threads.html
+            # FIXME: This hack would harm the performance in K8S deployment.
+            torch.set_num_threads(max(num_threads, 1))
+            torch.set_num_interop_threads(1)
+
+        self._minibatch_size = minibatch_size
+        self._model, self._preprocess_blob, self._preprocess_tensor = clip.load(
+            name, device=self._device, jit=jit
+        )
+        if pool_backend == 'thread':
+            self._pool = ThreadPool(processes=num_worker_preprocess)
+        else:
+            self._pool = Pool(processes=num_worker_preprocess)
+
+    def _preproc_image(self, da: 'DocumentArray') -> 'DocumentArray':
+        for d in da:
+            if d.tensor is not None:
+                d.tensor = self._preprocess_tensor(d.tensor)
+            else:
+                if not d.blob and d.uri:
+                    # in case user uses HTTP protocol and send data via curl not using .blob (base64), but in .uri
+                    d.load_uri_to_blob()
+                d.tensor = self._preprocess_blob(d.blob)
+        da.tensors = da.tensors.to(self._device)
+        return da
+
+    def _preproc_text(self, da: 'DocumentArray') -> Tuple['DocumentArray', List[str]]:
+        texts = da.texts
+        da.tensors = clip.tokenize(texts).to(self._device)
+        da[:, 'mime_type'] = 'text'
+        return da, texts
+
+    @requests
+    async def encode(self, docs: 'DocumentArray', **kwargs):
+        _img_da = docs.find(
+            {'$or': [{'blob': {'$exists': True}}, {'tensor': {'$exists': True}}]}
+        )
+        _txt_da = docs.find({'text': {'$exists': True}})
+
+        import torch
+
+        with torch.inference_mode():
+            # for image
+            if _img_da:
+                for minibatch in _img_da.map_batch(
+                    self._preproc_image,
+                    batch_size=self._minibatch_size,
+                    pool=self._pool,
+                ):
+                    minibatch.embeddings = (
+                        self._model.encode_image(minibatch.tensors)
+                        .cpu()
+                        .numpy()
+                        .astype(np.float32)
+                    )
+
+            # for text
+            if _txt_da:
+                for minibatch, _texts in _txt_da.map_batch(
+                    self._preproc_text,
+                    batch_size=self._minibatch_size,
+                    pool=self._pool,
+                ):
+                    minibatch.embeddings = (
+                        self._model.encode_text(minibatch.tensors)
+                        .cpu()
+                        .numpy()
+                        .astype(np.float32)
+                    )
+                    minibatch.texts = _texts
+
+        # drop tensors
+        docs.tensors = None
