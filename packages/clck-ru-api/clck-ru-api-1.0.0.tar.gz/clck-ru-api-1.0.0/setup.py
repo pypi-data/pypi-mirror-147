e3944eb90dc2b3319ebc66976f8920b80a45b06c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:52 +0000

    Add package clck-ru-api/1.0.0/clck-ru-api-1.0.0.tar.gz

---
+++ packages/clck-ru-api/clck-ru-api-1.0.0.tar.gz/clck-ru-api-1.0.0/setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup
+setup(name="clck-ru-api", author="wer310Libs", author_email="oficalkapudovo@gmail.com", description="import clck.ru \nclck.ru.link(\"http://clck.ru\")", packages=["clck"], version="1.0.0")
+
