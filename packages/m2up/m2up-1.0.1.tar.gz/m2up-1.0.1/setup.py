0e9f31a05c6a2bebece2e5ca3512221b33a83e6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:37 +0000

    Add package m2up/1.0.1/m2up-1.0.1.tar.gz

---
+++ packages/m2up/m2up-1.0.1.tar.gz/m2up-1.0.1/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+from os import path
+import shutil
+
+if path.isfile('README.md'):
+    shutil.copyfile('README.md', 'README')
+
+setup(
+    name='m2up',
+    description='Mark Markdown Up',
+    version='1.0.1',
+    author='John Reese, Hai Liang W.',
+    author_email='hailiang.hl.wang@gmail.com',
+    url='https://github.com/hailiang-wang/markdown-pp',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Utilities',
+        'Development Status :: 5 - Production/Stable',
+    ],
+    license='MIT License',
+    packages=['m2up', 'm2up/Modules'],
+    entry_points={
+        'console_scripts': [
+            'm2up = m2up.main:main'
+        ],
+    },
+    install_requires=[
+        'Watchdog >= 0.8.3',
+    ],
+)
