d331a0efaf26b0a72fca6169c8f8aa6b936139a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:26:55 +0000

    Add package gerritaction/1.2.0/gerritaction-1.2.0.tar.gz

---
+++ packages/gerritaction/gerritaction-1.2.0.tar.gz/gerritaction-1.2.0/gerritaction/proto/proto.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+
+class Proto:
+    ADD_REVIEWER = "add-reviewer"
+    DELETE_REVIEWER = "delete-reviewer"
+    ADD_ATTENTION = "add-attention"
+    REMOVE_ATTENTION = "remove-attention"
+    APPROVE_CHANGE = "approve-change"
+    SUBMIT_CHANGE = "submit-change"
+
+
+class Separator:
+    ACTION = ":"
+    CONTENT = ","
+    GROUP = " "
+    LABEL = "="
