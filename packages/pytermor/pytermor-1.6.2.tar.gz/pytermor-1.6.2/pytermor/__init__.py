4ee7344b0685aca4e146fcd6055c8e828735c248
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:54 +0000

    Add package pytermor/1.6.2/pytermor-1.6.2.tar.gz

---
+++ packages/pytermor/pytermor-1.6.2.tar.gz/pytermor-1.6.2/pytermor/__init__.py
@@ -0,0 +1,23 @@
+# -----------------------------------------------------------------------------
+# pytermor [ANSI formatted terminal output toolset]
+# (C) 2022 A. Shavykin <0.delameter@gmail.com>
+# -----------------------------------------------------------------------------
+from .seq import build, build_c256, build_rgb, SequenceSGR
+from .fmt import autof, Format
+from .util import apply_filters, StringFilter, ReplaceCSI, ReplaceSGR, ReplaceNonAsciiBytes
+
+__all__ = [
+    'build',
+    'build_c256',
+    'build_rgb',
+    'SequenceSGR',
+
+    'autof',
+    'Format',
+
+    'apply_filters',
+    'StringFilter',
+    'ReplaceCSI',
+    'ReplaceSGR',
+    'ReplaceNonAsciiBytes',
+]
