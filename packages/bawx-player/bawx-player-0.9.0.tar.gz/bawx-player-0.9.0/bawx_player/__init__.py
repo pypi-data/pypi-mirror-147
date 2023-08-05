5ad24cdedf5e3eeeb34f861e5ff8fedcb85e8d8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:19 +0000

    Add package bawx-player/0.9.0/bawx-player-0.9.0.tar.gz

---
+++ packages/bawx-player/bawx-player-0.9.0.tar.gz/bawx-player-0.9.0/bawx_player/__init__.py
@@ -0,0 +1,10 @@
+from . import pycdg
+# from . import pycdgAux
+# from . import pykar
+# from . import pykconstants
+# from . import pykdb
+# from . import pykenv
+# from . import pykmanager
+# from . import pykplayer
+# from . import pykversion
+# from . import pympg
---
+++ packages/bawx-player/bawx-player-0.9.0.tar.gz/bawx-player-0.9.0/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='bawx-player',
+    version='0.9.0',
+    description='Karaoke player library (and more)',
+    long_description= open('../README.md', 'r').read(),
+    long_description_content_type="text/markdown",
+    author='Michael Ottoson',
+    author_email='michael@pointw.com',
+    url='https://github.com/pointw-dev/bawx-player',
+    keywords=['karaoke'],
+    packages=['bawx_player'],
+    license='MIT',
+    install_requires=['numpy>=1.22.3', 'pygame>=2.1.2'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers'
+    ]
+)
