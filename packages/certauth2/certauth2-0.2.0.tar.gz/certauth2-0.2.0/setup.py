2c1c90a2cf64a137f49923e120e664fdfa42eabe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:54 +0000

    Add package certauth2/0.2.0/certauth2-0.2.0.tar.gz

---
+++ packages/certauth2/certauth2-0.2.0.tar.gz/certauth2-0.2.0/setup.py
@@ -0,0 +1,65 @@
+from pathlib import Path
+import shutil, sys
+from setuptools import setup as _setup, find_packages
+
+
+root = Path(__file__).parent
+PKG = sys.argv.pop(1)
+src = root / "src" / PKG
+
+(root / "dist").mkdir(exist_ok=True)
+if sys.argv[1] == "clean_dist":
+    for path in (root / "dist").iterdir():
+        path.unlink()
+    exit()
+
+
+def clean():
+    for path in (root / "src").iterdir():
+        if path.suffix == ".egg-info":
+            shutil.rmtree(path)
+    for path in root.iterdir():
+        if path.suffix == ".egg-info":
+            shutil.rmtree(path)
+    shutil.rmtree(root / "build", ignore_errors=True)
+
+
+pkgs = find_packages(str(src))
+import mimetypes
+
+readme_file = next((f for f in src.iterdir() if f.stem == "README"), None)
+if readme_file:
+    readme = readme_file.read_text()
+    readme_type = mimetypes.guess_type(readme_file)[0]
+    if readme_type is None:
+        readme_type = f"text/x-{readme_file.suffix[1:]}"
+    if readme_type == "text/x-md":
+        readme_type = "text/markdown"
+else:
+    readme = None
+    readme_type = None
+print(readme_type)
+def setup(*args, **kwargs):
+    clean()
+    _setup(
+        *args,
+        long_description=readme,
+        long_description_content_type=readme_type
+        if readme_type != "text/x-rst"
+        else None,
+        author="Jose A.",
+        author_email="jose-pr@coqui.dev",
+        url=f"https://github.com/jose-pr/{root.resolve().name}",
+        package_dir={"": "src"},
+        packages=[PKG, *pkgs],
+        install_requires=(src / "requirements.txt").read_text().splitlines(),
+        **kwargs,
+    )
+    clean()
+
+
+setup(
+    name=PKG.replace("_", "-"),
+    version=(src / "VERSION").read_text(),
+    description=(src / "DESCRIPTION").read_text(),
+)
---
+++ packages/certauth2/certauth2-0.2.0.tar.gz/certauth2-0.2.0/src/certauth2/stores.py
@@ -0,0 +1,106 @@
+from io import BytesIO, FileIO
+from typing import Iterator, overload
+from x509creds import X509Credentials, Encoding, ValidPath
+import tarfile
+
+from x509creds.utils import Encoded
+
+
+from .cache import FileCache, FileStorage, LRUCache, Transform, T
+
+
+def _ondiskStoreFuncs(
+    encoding: "Encoding|None" = None,
+    password: str = None,
+    transform: Transform[X509Credentials, T] = None,
+):
+    encoding = encoding or Encoding.PKCS12
+    transform = transform or (lambda x: x)
+    _suffix = "." + encoding.exts()[0]
+
+    def dump(iterio: Iterator[FileIO], creds: X509Credentials):
+        if encoding is Encoding.DER:
+            cert, key, chain = creds.dump(encoding, password=password)
+            next(iterio).write(cert)
+            next(iterio).write(key)
+            with tarfile.open(fileobj=next(iterio), mode="w") as bundle:
+                for i, ca in enumerate(chain):
+                    ca_io = BytesIO(ca)
+                    ca_info = tarfile.TarInfo(f"{i}.crt.der")
+                    ca_info.size = len(ca)
+                    bundle.addfile(ca_info, ca_io)
+        else:
+            next(iterio).write(creds.dump(encoding, password=password))
+
+    def load(iterio: Iterator[FileIO]) -> T:
+        if encoding is Encoding.DER:
+            cert = next(iterio).read()
+            key = next(iterio).read()
+            chain: "list[Encoded]" = []
+            with tarfile.open(fileobj=next(iterio), mode="r") as bundle:
+                for member in bundle.getmembers():
+                    if member.name.endswith(".crt.der"):
+                        chain.append(
+                            (bundle.extractfile(member).read(), encoding, password)
+                        )
+
+            creds = X509Credentials.load(
+                (cert, encoding, password), (key, encoding, password), *chain
+            )
+        else:
+            creds = X509Credentials.load((next(iterio).read(), encoding, password))
+        return transform(creds)
+
+    def stored_as(host: str):
+        if encoding is Encoding.DER:
+            base = host.replace(":", "-")
+            return [base + ".crt.der", base + ".key.der", base + ".chain.der.tar"]
+        return [host.replace(":", "-") + _suffix]
+
+    return dump, load, stored_as
+
+
+@overload
+def ondiskCredentialStore(
+    directory: ValidPath, encoding: "Encoding|None" = None, password: str = None
+) -> FileCache[str, X509Credentials, X509Credentials]:
+    ...
+
+
+def ondiskCredentialStore(
+    directory: ValidPath,
+    encoding: "Encoding|None" = None,
+    password: str = None,
+    transform: Transform[X509Credentials, T] = None,
+):
+    dump, load, stored_as = _ondiskStoreFuncs(encoding, password, transform)
+
+    return FileCache[str, X509Credentials, T](
+        directory, load=load, dump=dump, stored_as=stored_as
+    )
+
+
+@overload
+def ondiskPathStore(
+    directory: ValidPath, encoding: "Encoding|None" = None, password: str = None
+) -> FileStorage[str, X509Credentials, X509Credentials]:
+    ...
+
+
+def ondiskPathStore(
+    directory: ValidPath,
+    encoding: "Encoding|None" = None,
+    password: str = None,
+    transform: Transform[X509Credentials, T] = None,
+):
+    dump, load, stored_as = _ondiskStoreFuncs(encoding, password, transform)
+    return FileStorage[str, X509Credentials, T](
+        directory, load=load, dump=dump, stored_as=stored_as
+    )
+
+
+def onMemoryCredentialStore(
+    max_size: int, transform: Transform[X509Credentials, T] = None
+):
+    transform = transform or (lambda x: x)
+    return LRUCache[str, X509Credentials, T](max_size, transform=transform)
