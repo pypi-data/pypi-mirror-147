acefc8a0e6b022d0ef17991f5d67ef8d4f1c701e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:24:03 +0000

    Add package bb-krl-Tank-Battle/1.0.0/bb_krl%20Tank%20Battle-1.0.0.tar.gz

---
+++ packages/bb-krl-Tank-Battle/bb_krl%20Tank%20Battle-1.0.0.tar.gz/bb_krl Tank Battle-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='bb_krl Tank Battle',
+    version='1.0.0',
+    packages=['app'],
+    url='https://github.com/bbkrl/TMS_homework',
+    license='',
+    author='bb_krl',
+    author_email='bagushevich2003@mail.ru',
+    description='package for consol tank battle'
+)
