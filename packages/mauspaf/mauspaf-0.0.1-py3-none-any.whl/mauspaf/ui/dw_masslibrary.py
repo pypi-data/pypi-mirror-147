f7085c270d0bdd6ee0772e743faf4cf07eb91e8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:22 +0000

    Add package mauspaf/0.0.1/mauspaf-0.0.1-py3-none-any.whl

---
+++ packages/mauspaf/mauspaf-0.0.1-py3-none-any.whl/mauspaf/ui/dw_masslibrary.py
@@ -0,0 +1,46 @@
+from PyQt5 import QtWidgets
+import sympy as sym
+import mauspaf
+from mauspaf.generated.dw_masslibrary import Ui_DockWidget
+import mauspaf.guifun.masslib as masslib
+import mauspaf.masslibrary.solids as solids
+import mauspaf.masslibrary.aircraft as aircraft
+import mauspaf.masslibrary.misc as misc
+
+
+class DW_MassLibrary(QtWidgets.QDockWidget):
+    def __init__(self):
+        super(DW_MassLibrary, self).__init__()
+        self.ui = Ui_DockWidget()
+        self.ui.setupUi(self)
+        # # # Initialise input sets
+        formulas_list = mauspaf.guifun.masslib.ML_FUNCTIONS
+        self.mass_formula_priority = {}
+        self.mass_formula_symbols = {}
+        self.mass_formula_status = {}
+        self.mass_formula_sets = {}
+        for i_id, i_fun, i_prio in formulas_list[1:]:
+            i_idi = int(i_id)
+            self.mass_formula_priority[i_idi] = [
+                sym.sympify(x) for x in i_prio]
+            self.mass_formula_symbols[i_idi] = set(
+                self.mass_formula_priority[i_idi])
+            self.mass_formula_status[i_idi] = {
+                symbol: getattr(self.ui, "pb_" + str(i_idi) + "_"
+                                + str(symbol)).isChecked()
+                for symbol in self.mass_formula_symbols[i_idi]}
+            self.mass_formula_sets[i_idi] = list(eval(
+                i_fun + "(calc_mode='sets')"))
+        # Show
+        self.show()
+
+    def show_frame(self, item, column):
+        if item.text(0) == 'Full' and item.parent().text(0) == 'Cylinder':
+            self.ui.stackedWidget.setCurrentIndex(0)
+        elif item.text(0) == 'Hollow' and item.parent().text(0) == 'Cylinder':
+            self.ui.stackedWidget.setCurrentIndex(1)
+        elif item.text(0) == 'Raymer' and item.parent().text(0) == 'Wing' \
+                and item.parent().parent().text(0) == 'GA-Aircraft':
+            self.ui.stackedWidget.setCurrentIndex(2)
+        else:
+            self.ui.stackedWidget.setCurrentIndex(3)
