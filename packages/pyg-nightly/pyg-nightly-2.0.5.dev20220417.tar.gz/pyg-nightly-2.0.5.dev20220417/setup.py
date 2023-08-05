384096964cee5e0241f95f950db4a1ad5807ae89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:51 +0000

    Add package pyg-nightly/2.0.5.dev20220417/pyg-nightly-2.0.5.dev20220417.tar.gz

---
+++ packages/pyg-nightly/pyg-nightly-2.0.5.dev20220417.tar.gz/pyg-nightly-2.0.5.dev20220417/setup.py
@@ -0,0 +1,66 @@
+from setuptools import find_packages, setup
+
+__version__ = '2.0.5.dev20220417'
+URL = 'https://github.com/pyg-team/pytorch_geometric'
+
+install_requires = [
+    'tqdm',
+    'numpy',
+    'scipy',
+    'jinja2',
+    'requests',
+    'pyparsing',
+    'scikit-learn',
+]
+
+full_install_requires = [
+    'h5py',
+    'yacs',
+    'numba',
+    'pandas',
+    'captum',
+    'rdflib',
+    'trimesh',
+    'networkx',
+    'tabulate',
+    'hydra-core',
+    'matplotlib',
+    'scikit-image',
+    'pytorch-memlab',
+    'torchmetrics>=0.7',
+]
+
+test_requires = [
+    'pytest',
+    'pytest-cov',
+]
+
+dev_requires = test_requires + [
+    'pre-commit',
+]
+
+setup(
+    name='pyg-nightly',
+    version=__version__,
+    description='Graph Neural Network Library for PyTorch',
+    author='Matthias Fey',
+    author_email='matthias.fey@tu-dortmund.de',
+    url=URL,
+    download_url=f'{URL}/archive/{__version__}.tar.gz',
+    keywords=[
+        'deep-learning',
+        'pytorch',
+        'geometric-deep-learning',
+        'graph-neural-networks',
+        'graph-convolutional-networks',
+    ],
+    python_requires='>=3.7',
+    install_requires=install_requires,
+    extras_require={
+        'full': full_install_requires,
+        'test': test_requires,
+        'dev': dev_requires,
+    },
+    packages=find_packages(),
+    include_package_data=True,
+)
