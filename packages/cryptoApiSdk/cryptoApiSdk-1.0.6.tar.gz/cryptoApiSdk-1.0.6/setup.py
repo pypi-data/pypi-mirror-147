c54b74a522423bd3fa67bac59fb5e6e2c1a2b2bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:18:23 +0000

    Add package cryptoApiSdk/1.0.6/cryptoApiSdk-1.0.6.tar.gz

---
+++ packages/cryptoApiSdk/cryptoApiSdk-1.0.6.tar.gz/cryptoApiSdk-1.0.6/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name="cryptoApiSdk",
+    version="1.0.6",
+    description="",
+    long_description='',
+    long_description_content_type="text/markdown",
+    url="https://github.com/shadow-twitter/sdk.git",
+    author="Jared Williams",
+    author_email="williamsjared62@gmail.com",
+    #To find more licenses or classifiers go to: https://pypi.org/classifiers/
+    license="",
+    classifiers=[
+    ],
+    zip_safe=True,
+    python_requires=">=3.0",
+    py_modules=['cryptoApiSdk'],
+    install_requires=['requests']
+)
