b99cac051ac88cc889fb2e31c66af2f744a4e158
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:46 +0000

    Add package aebo-pi/0.6/aebo_pi-0.6.tar.gz

---
+++ packages/aebo-pi/aebo_pi-0.6.tar.gz/aebo_pi-0.6/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='aebo_pi',
+      version='0.6',
+      description='This package is for ABG robot running on a RPI',
+      url='http://www.aeroboticsglobal.com/',
+      author='Aerobotics Global',
+      author_email='lokeshkode@aeroboticsglobal.com',
+      license='MIT',
+      packages=['aebo_pi'],
+      install_requires=[
+          'nltk',
+          'scikit-learn>=0.24',
+          'numpy>=1.21',
+          'lobe',
+          'mediapipe-rpi4',
+          
+      ],
+      zip_safe=False)
