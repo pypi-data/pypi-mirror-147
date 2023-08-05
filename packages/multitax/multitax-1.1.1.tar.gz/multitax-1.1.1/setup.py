278e8bea15a3a902b5efa1b3acaad44682170804
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:15:09 +0000

    Add package multitax/1.1.1/multitax-1.1.1.tar.gz

---
+++ packages/multitax/multitax-1.1.1.tar.gz/multitax-1.1.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="multitax",
+    version="1.1.1",
+    url="https://www.github.com/pirovc/multitax",
+    license='MIT',
+
+    author="Vitor C. Piro",
+    author_email="pirovc@posteo.net",
+
+    description="Python package to obtain, parse and explore biological and custom taxonomies",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    packages=['multitax'],
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.4",
+)
