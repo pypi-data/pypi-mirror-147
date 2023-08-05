d502db019e0c905ebe04ee600793e0305e714a0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:24:04 +0000

    Add package gfFermentation/0.0.5/gfFermentation-0.0.5-py3-none-any.whl

---
+++ packages/gfFermentation/gfFermentation-0.0.5-py3-none-any.whl/gfFermentation/__init__.py
@@ -0,0 +1,6 @@
+__author__ = 'github.com/wardsimon'
+__version__ = '0.0.5'
+
+GRAINFATHER_AUTH_URL = "https://community.grainfather.com/api/auth/login"
+GRAINFATHER_TOKENS_URL = "https://community.grainfather.com/api/particle/tokens"
+PARTICLE_EVENT_URL = "https://api.particle.io/v1/devices"
