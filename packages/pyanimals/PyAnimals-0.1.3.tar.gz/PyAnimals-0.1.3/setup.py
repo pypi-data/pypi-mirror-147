96c2bde04e8918ab4548a3c815c30f6bcee9b64e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:17:38 +0000

    Add package pyanimals/0.1.3/PyAnimals-0.1.3.tar.gz

---
+++ packages/pyanimals/PyAnimals-0.1.3.tar.gz/PyAnimals-0.1.3/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pyanimals']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'pyanimals',
+    'version': '0.1.3',
+    'description': 'Get animal images and facts with easy plug-and-play syntax!',
+    'long_description': None,
+    'author': 'ksIsCute, Cesiyi',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
