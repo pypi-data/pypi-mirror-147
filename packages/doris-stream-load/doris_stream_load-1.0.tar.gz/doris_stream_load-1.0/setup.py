09a337ef9934a2b12596641650a0fb55c6de91c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:18:24 +0000

    Add package doris-stream-load/1.0/doris_stream_load-1.0.tar.gz

---
+++ packages/doris-stream-load/doris_stream_load-1.0.tar.gz/doris_stream_load-1.0/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='doris_stream_load',
+    version = '1.0',
+    py_modules = ['doris_stream_load'],
+    author = 'zsx',
+    author_email = '429385304@qq.com',
+    description = 'python doris stream load')
