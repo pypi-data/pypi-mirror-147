505b6ccb4af38a37fccd230c46bdeea0f5b15d5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:10 +0000

    Add package clickpy/0.11.0/clickpy-0.11.0.tar.gz

---
+++ packages/clickpy/clickpy-0.11.0.tar.gz/clickpy-0.11.0/clickpy/__main__.py
@@ -0,0 +1,9 @@
+"""Clickpy, Automated mouse clicking script."""  # pragma: no cover
+
+
+if __name__ == "__main__":  # pragma: no cover
+    import typer  # pragma: no cover
+
+    from clickpy import main  # pragma: no cover
+
+    typer.run(main)  # pragma: no cover
