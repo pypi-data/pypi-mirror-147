62f9a14a0f2096b9c2adac87b7cdeb280f9c5c34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:37:23 +0000

    Add package keras-flower/0.3/keras_flower-0.3-py3-none-any.whl

---
+++ packages/keras-flower/keras_flower-0.3-py3-none-any.whl/keras_flower/__init__.py
@@ -0,0 +1,6 @@
+__version__ = 0.3
+from.util import predict, embed, embed_by_path
+from.util import predict_name
+from.util import labels
+from.util import predict_by_path
+from.util import predict_name_by_path
