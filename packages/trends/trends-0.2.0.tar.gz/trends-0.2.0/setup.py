c40fc8ef7889eaf87b980cc9bc630174c3aac76e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:03 +0000

    Add package trends/0.2.0/trends-0.2.0.tar.gz

---
+++ packages/trends/trends-0.2.0.tar.gz/trends-0.2.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['trends']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'trends',
+    'version': '0.2.0',
+    'description': 'Generate and study quasi-monotonic sequences.',
+    'long_description': None,
+    'author': 'Jeffrey S. Haemer',
+    'author_email': 'jeffrey.haemer@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
