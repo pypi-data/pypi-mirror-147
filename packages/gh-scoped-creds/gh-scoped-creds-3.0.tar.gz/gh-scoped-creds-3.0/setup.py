13271be6bde69c2a7f9b332ae228301cab8c4503
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:18:45 +0000

    Add package gh-scoped-creds/3.0/gh-scoped-creds-3.0.tar.gz

---
+++ packages/gh-scoped-creds/gh-scoped-creds-3.0.tar.gz/gh-scoped-creds-3.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="gh-scoped-creds",
+    version="3.0",
+    url="https://github.com/yuvipanda/gh-scoped-creds",
+    license="3-clause BSD",
+    author="Yuvi Panda",
+    author_email="yuvipanda@gmail.com",
+    description="Temporary, well scoped credentials for pushing to GitHub",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    platforms="any",
+    install_requires=["requests"],
+    entry_points={
+        "console_scripts": [
+            # Backwards compatible script providing former (pre 2.0) name
+            "github-app-user-auth = gh_scoped_creds:main",
+            "gh-scoped-creds = gh_scoped_creds:main",
+        ],
+    },
+)
