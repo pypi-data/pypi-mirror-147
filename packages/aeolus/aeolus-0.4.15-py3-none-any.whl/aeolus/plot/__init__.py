afc7680e8c03ca74eeb6f54c148c5b532e590477
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:18 +0000

    Add package aeolus/0.4.15/aeolus-0.4.15-py3-none-any.whl

---
+++ packages/aeolus/aeolus-0.4.15-py3-none-any.whl/aeolus/plot/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""Contains functionality for making special plots."""
+from .cart import GeoAxesGrid, label_global_map_gridlines
+from .cloud import CloudPlotter
+from .cm_custom import cloudtypes_cmap
+from .mpl import add_custom_legend
+from .text import fmt_lonlat, subplot_label_generator, tex2cf_units, unit_format
+
+
+__all__ = (
+    "add_custom_legend",
+    "CloudPlotter",
+    "cloudtypes_cmap",
+    "fmt_lonlat",
+    "GeoAxesGrid",
+    "label_global_map_gridlines",
+    "subplot_label_generator",
+    "tex2cf_units",
+    "unit_format",
+)
