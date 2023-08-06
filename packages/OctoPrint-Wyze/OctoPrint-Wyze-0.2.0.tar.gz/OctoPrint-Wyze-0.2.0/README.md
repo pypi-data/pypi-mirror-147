5951052131f43d8d5cd884e9b537e9dd028403dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:42 +0000

    Add package OctoPrint-Wyze/0.2.0/OctoPrint-Wyze-0.2.0.tar.gz

---
+++ packages/OctoPrint-Wyze/OctoPrint-Wyze-0.2.0.tar.gz/OctoPrint-Wyze-0.2.0/README.md
@@ -0,0 +1,28 @@
+# OctoPrint-Wyze
+
+`OctoPrint-Wyze` lets you control and automate Wyze home devices through OctoPrint. You can register plugs, lights and cameras to turn on or off whenever specific events occur. For example, you can set a light to turn on whenever the web client is opened, or a print or timelapse is started, then off when a print has finished.
+
+This plugin has **no** support for camera streams or controls. This would be [very difficult](https://github.com/eshapiro42/OctoPrint-Wyze/issues/4#issuecomment-1075802410) to add. It is exclusively for automating the powering on or off of devices to coincide with the [available printer events](https://github.com/eshapiro42/OctoPrint-Wyze/blob/b2e42489d3a5bf51ef6369d570e03e22fb96dab6/octoprint_wyze/events.py#L10).
+
+![OctoPrint-Wyze Screenshot](/OctoPrint-Wyze.png)
+
+| :warning: This plugin relies on the reverse-engineered [`wyze_sdk`](https://github.com/shauntarves/wyze-sdk/) module and will break if Wyze makes significant changes to their API or otherwise renders it unusable. |
+| --- |
+
+## Setup
+
+Ensure that OctoPrint is running Python 3.8 or higher. OctoPrint's Python version can be found at the bottom left of the web client.
+
+If you are running OctoPi and had to build Python 3.8+ from source, make sure you can import `sqlite3`. Otherwise, run `sudo apt install libsqlite3-dev` and then rebuild Python with `./configure --enable-loadable-sqlite-extensions && make`.
+
+Install via the bundled [Plugin Manager](https://docs.octoprint.org/en/master/bundledplugins/pluginmanager.html)
+or manually using this URL:
+
+    https://github.com/eshapiro42/OctoPrint-Wyze/archive/main.zip
+
+## Configuration
+
+Add your Wyze username and password in the plugin settings and reload the server. 
+
+| :warning: Your Wyze username and password are encrypted by the plugin before being stored on your filesystem, but can be decrypted with relative ease by anyone on your system with access to OctoPrint's `config.yaml` file. Please ensure that you're taking appropriate precautions and not reusing passwords between sites! |
+| --- |
---
+++ packages/OctoPrint-Wyze/OctoPrint-Wyze-0.2.0.tar.gz/OctoPrint-Wyze-0.2.0/octoprint_wyze/static/js/wyze.js
@@ -0,0 +1,163 @@
+/*
+ * View model for OctoPrint-Wyze
+ *
+ * Author: Eric Shapiro
+ * License: AGPLv3
+ */
+
+$(function() {
+    function WyzeViewModel(parameters, js_event) {
+        var self = this;
+
+        self.actions = [];
+        self.events = [];
+        self.pendingActions = ko.observableArray([]);
+        self.devices = ko.observableArray([]);
+
+        OctoPrint.simpleApiCommand(
+            "wyze",
+            "get_enums",
+        ).done(function(response) {
+            self.events = response.events;
+            self.actions = response.actions;
+        });
+
+        function getPendingActions() {
+            OctoPrint.simpleApiCommand(
+                "wyze",
+                "get_pending_actions",
+            ).done(function(response) {
+                self.pendingActions(response);
+            });
+        }
+
+        var getPendingActionsInterval = window.setInterval(getPendingActions, 500);
+
+        function Device(data) {
+            var this_device = this;
+
+            this_device.mac = data.device_mac;
+            this_device.name = data.device_name;
+            this_device.type = data.device_type;
+
+            this_device.turnOnDevice = function() {
+                OctoPrint.simpleApiCommand(
+                    "wyze",
+                    "turn_on", 
+                    {
+                        "device_mac": this_device.mac,
+                    }
+                );
+            };
+    
+            this_device.turnOffDevice = function() {
+                OctoPrint.simpleApiCommand(
+                    "wyze",
+                    "turn_off", 
+                    {
+                        "device_mac": this_device.mac,
+                    }
+                );
+            };
+
+            this_device.registerDevice = function(event_index, action_name, delay) {
+                event_name = self.events[event_index];
+                OctoPrint.simpleApiCommand(
+                    "wyze",
+                    "register",
+                    {
+                        "device_mac": this_device.mac,
+                        "event_name": event_name,
+                        "action_name": action_name,
+                        "delay": delay,
+                    }
+                )
+            }
+
+            this_device.unregisterDevice = function(event_index, action_name) {
+                event_name = self.events[event_index];
+                OctoPrint.simpleApiCommand(
+                    "wyze",
+                    "unregister",
+                    {
+                        "device_mac": this_device.mac,
+                        "event_name": event_name,
+                        "action_name": action_name,
+                    }
+                )
+            }
+
+            this_device.turnOnCheckBoxClicked = function(data, js_event) {
+                var context = ko.contextFor(js_event.target);
+                var event_index = context.$index();
+                var checked = js_event.currentTarget.checked;
+                if (checked) {
+                    var delay = js_event.currentTarget.nextElementSibling.valueAsNumber;
+                    this_device.registerDevice(event_index, "TurnOn", delay);
+                }
+                else {
+                    this_device.unregisterDevice(event_index, "TurnOn");
+                }
+                return true;
+            }
+
+            this_device.turnOffCheckBoxClicked = function(data, js_event) {
+                var context = ko.contextFor(js_event.target);
+                var event_index = context.$index();
+                var checked = js_event.currentTarget.checked;
+                if (checked) {
+                    var delay = js_event.currentTarget.nextElementSibling.valueAsNumber;
+                    this_device.registerDevice(event_index, "TurnOff", delay);
+                }
+                else {
+                    this_device.unregisterDevice(event_index, "TurnOff");
+                }
+                return true;
+            }
+
+            this_device.turn_on_registrations = $.map(data.turn_on_registrations, function(item, event_index) {
+                var registration = {
+                    registered: ko.observable(item[0]),
+                    delay: ko.observable(item[1]),
+                };
+                return registration;
+            });
+
+            this_device.turn_off_registrations = $.map(data.turn_off_registrations, function(item, event_index) {
+                var registration = {
+                    registered: ko.observable(item[0]),
+                    delay: ko.observable(item[1]),
+                };
+                return registration;
+            });
+        }
+
+        OctoPrint.simpleApiCommand(
+            "wyze",
+            "get_devices",
+        ).done(function(response) {
+            var devices = $.map(response, function(item) {
+                return new Device(item);
+            });
+            self.devices(devices);
+        });
+
+        // assign the injected parameters, e.g.:
+        // self.loginStateViewModel = parameters[0];
+        // self.settingsViewModel = parameters[1];
+
+        // TODO: Implement your plugin's view model here.
+    }
+
+    /* view model class, parameters for constructor, container to bind to
+     * Please see http://docs.octoprint.org/en/master/plugins/viewmodels.html#registering-custom-viewmodels for more details
+     * and a full list of the available options.
+     */
+    OCTOPRINT_VIEWMODELS.push({
+        construct: WyzeViewModel,
+        // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
+        dependencies: [],
+        // Elements to bind to, e.g. #settings_plugin_wyze, #tab_plugin_wyze, ...
+        elements: ["#tab_plugin_wyze"]
+    });
+});
