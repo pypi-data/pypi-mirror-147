b2dffa12d99191c5438b7d01f7cd1ebcd370a9b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:43 +0000

    Add package pulp-deb/2.16.2/pulp_deb-2.16.2-py3-none-any.whl

---
+++ packages/pulp-deb/pulp_deb-2.16.2-py3-none-any.whl/pulp_deb/app/models/repository.py
@@ -0,0 +1,57 @@
+from pulpcore.plugin.models import Repository
+
+from pulpcore.plugin.repo_version_utils import remove_duplicates, validate_repo_version
+
+from pulp_deb.app.models import (
+    AptRemote,
+    GenericContent,
+    InstallerFileIndex,
+    InstallerPackage,
+    Package,
+    PackageIndex,
+    PackageReleaseComponent,
+    Release,
+    ReleaseArchitecture,
+    ReleaseComponent,
+    ReleaseFile,
+)
+
+
+class AptRepository(Repository):
+    """
+    A Repository for DebContent.
+    """
+
+    TYPE = "deb"
+    CONTENT_TYPES = [
+        GenericContent,
+        InstallerFileIndex,
+        InstallerPackage,
+        Package,
+        PackageIndex,
+        PackageReleaseComponent,
+        Release,
+        ReleaseArchitecture,
+        ReleaseComponent,
+        ReleaseFile,
+    ]
+    REMOTE_TYPES = [
+        AptRemote,
+    ]
+
+    class Meta:
+        default_related_name = "%(app_label)s_%(model_name)s"
+
+    def finalize_new_version(self, new_version):
+        """
+        Finalize and validate the new repository version.
+
+        Ensure there are no duplication of added package in debian repository.
+
+        Args:
+            new_version (pulpcore.app.models.RepositoryVersion): The incomplete RepositoryVersion to
+                finalize.
+
+        """
+        remove_duplicates(new_version)
+        validate_repo_version(new_version)
