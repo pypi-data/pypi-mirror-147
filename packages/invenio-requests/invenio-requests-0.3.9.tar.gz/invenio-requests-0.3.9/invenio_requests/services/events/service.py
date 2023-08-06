585677dd401afadc483770bf8a59c5128d7258a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:21:43 +0000

    Add package invenio-requests/0.3.9/invenio-requests-0.3.9.tar.gz

---
+++ packages/invenio-requests/invenio-requests-0.3.9.tar.gz/invenio-requests-0.3.9/invenio_requests/services/events/service.py
@@ -0,0 +1,190 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 CERN.
+# Copyright (C) 2021 Northwestern University.
+# Copyright (C) 2021 TU Wien.
+#
+# Invenio-Requests is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""RequestEvents Service."""
+
+from elasticsearch_dsl import Q
+from invenio_access.permissions import system_process
+from invenio_records_resources.services import RecordService, ServiceSchemaWrapper
+from invenio_records_resources.services.base.links import LinksTemplate
+from invenio_records_resources.services.uow import RecordCommitOp, unit_of_work
+
+from ...records.api import RequestEventType
+from ..schemas import RequestEventDumpSchema
+
+
+class RequestEventsService(RecordService):
+    """Request Events service."""
+
+    @property
+    def event_dump_schema(self):
+        """Schema for creation."""
+        return ServiceSchemaWrapper(self, schema=RequestEventDumpSchema)
+
+    @unit_of_work()
+    def create(self, identity, request_id, data, uow=None):
+        """Create a request event.
+
+        :param request_id: Identifier of the request (data-layer id).
+        :param identity: Identity of user creating the event.
+        :param dict data: Input data according to the data schema.
+        """
+        request = self._get_request(request_id)
+        # If you can read the request you can create events for the request.
+        self.require_permission(identity, "read", request=request)
+
+        # Validate data (if there are errors, .load() raises)
+        data, errors = self.schema.load(
+            data,
+            context={"identity": identity},
+        )
+
+        event = self.record_cls.create(
+            {},
+            request=request.model,
+            request_id=str(request_id),
+            type=data["type"],
+        )
+        event.update(data)
+        event.created_by = self._get_creator(identity)
+
+        # Persist record (DB and index)
+        uow.register(RecordCommitOp(event, indexer=self.indexer))
+
+        return self.result_item(
+            self,
+            identity,
+            event,
+            links_tpl=self.links_item_tpl,
+            schema=self.event_dump_schema
+        )
+
+    def read(self, identity, id_):
+        """Retrieve a record."""
+        record = self._get_event(id_)
+        request = self._get_request(record.request_id)
+
+        self.require_permission(identity, "read", request=request)
+
+        return self.result_item(
+            self,
+            identity,
+            record,
+            links_tpl=self.links_item_tpl,
+            schema=self.event_dump_schema
+        )
+
+    @unit_of_work()
+    def update(self, identity, id_, data, revision_id=None, uow=None):
+        """Update a comment (only comments can be updated)."""
+        event = self._get_event(id_)
+        request = self._get_request(event.request.id)
+        self.require_permission(
+            identity, "update_comment", request=request, event=event)
+        self.check_revision_id(event, revision_id)
+
+        if event.type != RequestEventType.COMMENT.value:
+            raise PermissionError("You cannot update events.")
+
+        data['type'] = RequestEventType.COMMENT.value
+        data, _ = self.schema.load(
+            data,
+            context=dict(
+                identity=identity,
+                record=event,
+            ),
+        )
+        event['payload']['content'] = data['payload']['content']
+        event['payload']['format'] = data['payload']['format']
+        uow.register(RecordCommitOp(event, indexer=self.indexer))
+
+        return self.result_item(
+            self,
+            identity,
+            event,
+            links_tpl=self.links_item_tpl,
+            schema=self.event_dump_schema
+        )
+
+    @unit_of_work()
+    def delete(self, identity, id_, revision_id=None, uow=None):
+        """Delete a a comment (only comments can be deleted)."""
+        event = self._get_event(id_)
+        request = self._get_request(event.request_id)
+
+        # Permissions
+        self.require_permission(
+            identity, "delete_comment", request=request, event=event)
+        self.check_revision_id(event, revision_id)
+
+        if event.type != RequestEventType.COMMENT.value:
+            raise PermissionError("You cannot delete events.")
+
+        event["payload"]["content"] = ""
+        event.type = RequestEventType.REMOVED.value
+
+        uow.register(
+            RecordCommitOp(event, indexer=self.indexer)
+        )
+        return True
+
+    def search(self, identity, request_id, params=None, es_preference=None, **kwargs):
+        """Search for events for a given request matching the querystring."""
+        params = params or {}
+        params.setdefault("sort", "oldest")
+
+        # Permissions - guarded by the request's can_read.
+        request = self._get_request(request_id)
+        self.require_permission(identity, "read", request=request)
+
+        # Prepare and execute the search
+        search = self._search(
+            "search",
+            identity,
+            params,
+            es_preference,
+            permission_action="unused",
+            extra_filter=Q("term", request_id=str(request_id)),
+            **kwargs,
+        )
+        search_result = search.execute()
+
+        return self.result_list(
+            self,
+            identity,
+            search_result,
+            params,
+            links_tpl=LinksTemplate(
+                self.config.links_search,
+                context={"request_id": request_id, "args": params},
+            ),
+            links_item_tpl=self.links_item_tpl,
+            schema=self.event_dump_schema
+        )
+
+    # Utilities
+    @property
+    def request_cls(self):
+        """Get associated request class."""
+        return self.config.request_cls
+
+    def _get_request(self, request_id):
+        """Get associated request."""
+        return self.request_cls.get_record(request_id)
+
+    def _get_event(self, event_id, with_deleted=True):
+        """Get associated event_id."""
+        return self.record_cls.get_record(event_id, with_deleted=with_deleted)
+
+    def _get_creator(self, identity):
+        """Get the creator dict from the identity."""
+        if system_process in identity.provides:
+            return None  # TODO: Change this for some agreed value
+        else:
+            return {"user": str(identity.id)}
---
+++ packages/invenio-requests/invenio-requests-0.3.9.tar.gz/invenio-requests-0.3.9/invenio_requests/services/schemas.py
@@ -0,0 +1,175 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 CERN.
+# Copyright (C) 2021 Northwestern University.
+# Copyright (C) 2021 - 2022 TU Wien.
+#
+# Invenio-Requests is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Request Event Schemas."""
+
+from datetime import timezone
+
+from flask import g
+from invenio_records_resources.services.records.schema import BaseRecordSchema
+from marshmallow import RAISE, Schema, fields, missing, validate
+from marshmallow_oneofschema import OneOfSchema
+from marshmallow_utils import fields as utils_fields
+
+from invenio_requests.proxies import current_requests
+
+from ..records.api import RequestEventFormat, RequestEventType
+
+
+class RequestSchema(BaseRecordSchema):
+    """Schema for requests.
+
+    Note that the payload schema and the entity reference schemas (i.e. creator,
+    receiver, and topic) are dynamically constructed and injected into this schema.
+    """
+
+    # load and dump
+    type = fields.String()
+    title = utils_fields.SanitizedUnicode(default="")
+    description = utils_fields.SanitizedUnicode()
+
+    # Dump-only
+    number = fields.String(dump_only=True)
+    status = fields.String(dump_only=True)
+    is_closed = fields.Boolean(dump_only=True)
+    is_open = fields.Boolean(dump_only=True)
+    expires_at = utils_fields.TZDateTime(
+        timezone=timezone.utc, format="iso", dump_only=True
+    )
+    is_expired = fields.Boolean(dump_only=True)
+
+    class Meta:
+        """Schema meta."""
+
+        unknown = RAISE
+
+
+class CommentExtra(Schema):
+    """Comment extras schema."""
+
+    content = utils_fields.SanitizedHTML(
+        required=True, validate=validate.Length(min=1)
+    )
+    format = fields.Str(
+        validate=validate.OneOf(choices=[e.value for e in RequestEventFormat]),
+        load_default=RequestEventFormat.HTML.value,
+    )
+
+
+class GenericRequestSchema(RequestSchema):
+    """Generic request schema.
+
+    CAUTION: This schema should not be used for the final validation of input
+    data. Use the request type's own defined schema instead.
+
+    This schema can be used in situations where you need to do basic validation
+    or dumping of a request without the payload.
+
+    This is used e.g. in Invenio-RDM-Records for dumping a request without
+    having to know the specific request type.
+    """
+
+    created_by = fields.Dict()
+    receiver = fields.Dict()
+    topic = fields.Dict()
+
+
+class ModelFieldStr(fields.Str):
+    """Marshmallow field for serializing by attribute before item."""
+
+    def get_value(self, obj, attr, **kwargs):
+        """Return obj.attr or obj[attr] in this precedence order."""
+        return getattr(obj, attr, obj.get(attr, missing))
+
+
+class BaseEventSchema(BaseRecordSchema):
+    """Base Event schema that other schemas should inherit from."""
+
+    type = ModelFieldStr(required=True)
+    created_by = fields.Dict(dump_only=True)
+
+
+class CommentSchema(BaseEventSchema):
+    """Comment schema."""
+
+    permissions = fields.Dict(dump_only=True)
+
+    payload = fields.Nested(CommentExtra, required=True)
+
+
+class NoExtrasSchema(BaseEventSchema):
+    """No extras schema."""
+
+    permissions = fields.Dict(dump_only=True)
+
+
+class RequestEventSchema(BaseRecordSchema, OneOfSchema):
+    """Schema."""
+
+    # TODO: Make this schema hookable? how?
+
+    type_schemas = {
+        RequestEventType.COMMENT.value: CommentSchema,
+        RequestEventType.ACCEPTED.value: NoExtrasSchema,
+        RequestEventType.DECLINED.value: NoExtrasSchema,
+        RequestEventType.CANCELLED.value: NoExtrasSchema,
+        RequestEventType.REMOVED.value: NoExtrasSchema,
+        RequestEventType.EXPIRED.value: NoExtrasSchema,
+    }
+    type_field_remove = False
+
+    def get_obj_type(self, obj):
+        """Return key of type_schemas to use given object to dump."""
+        return obj.type
+
+
+class CommentPermissionShema(Schema):
+    """Add permissions to the dump data."""
+
+    permissions = fields.Method("get_permissions")
+
+    def get_permissions(self, obj):
+        """Get certain permissions of the user with the current hit."""
+        service = current_requests.request_events_service
+        return {
+            "can_update_comment": service.check_permission(
+                self.context['identity'], "update_comment", event=obj
+            ),
+            "can_delete_comment": service.check_permission(
+                self.context['identity'], "delete_comment", event=obj
+            ),
+        }
+
+
+class CommentDumpSchema(BaseEventSchema, CommentPermissionShema):
+    """Comment dump schema."""
+
+    payload = fields.Nested(CommentExtra, required=True)
+
+
+class NoExtrasDumpSchema(BaseEventSchema, CommentPermissionShema):
+    """Comment dump schema."""
+
+
+class RequestEventDumpSchema(BaseRecordSchema, OneOfSchema):
+    """Dump schema."""
+
+    type_schemas = {
+        RequestEventType.COMMENT.value: CommentDumpSchema,
+        RequestEventType.ACCEPTED.value: NoExtrasDumpSchema,
+        RequestEventType.DECLINED.value: NoExtrasDumpSchema,
+        RequestEventType.CANCELLED.value: NoExtrasDumpSchema,
+        RequestEventType.REMOVED.value: NoExtrasDumpSchema,
+        RequestEventType.EXPIRED.value: NoExtrasDumpSchema,
+    }
+    type_field_remove = False
+
+    def get_obj_type(self, obj):
+        """Return key of type_schemas to use given object to dump."""
+        return obj.type
