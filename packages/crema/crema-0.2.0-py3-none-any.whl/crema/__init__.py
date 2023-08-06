a51cf28832b81c83d017db55df806ca6c0abdfa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:48 +0000

    Add package crema/0.2.0/crema-0.2.0-py3-none-any.whl

---
+++ packages/crema/crema-0.2.0-py3-none-any.whl/crema/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''Convolutional-recurrent estimators for music analysis'''
+
+from .version import version as __version__
+
+from .exceptions import *
+from . import layers
+from . import models
