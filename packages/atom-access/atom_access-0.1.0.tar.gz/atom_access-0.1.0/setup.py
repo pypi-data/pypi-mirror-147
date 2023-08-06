97e7a9820a8c51f8decbceeb5d3ecfceb2cc501d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:48:21 +0000

    Add package atom-access/0.1.0/atom_access-0.1.0.tar.gz

---
+++ packages/atom-access/atom_access-0.1.0.tar.gz/atom_access-0.1.0/setup.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# pylint: disable=line-too-long,missing-module-docstring,exec-used
+
+import setuptools
+from textwrap import dedent
+
+with open('README.md', 'r') as file:
+    long_description = file.read()
+
+# DO NOT EDIT THIS NUMBER!
+# IT IS AUTOMATICALLY CHANGED BY python-semantic-release
+__version__ = "0.1.0"
+
+setuptools.setup(
+    name='atom_access',
+    version=__version__,
+    author='Chilton Group',
+    author_email='chilton.group@dummy.com',
+    description=dedent(
+        'Atom access is a ray tracing package for determining steric \
+         hinderance'),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent'
+        ],
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+    install_requires=[
+        'numpy',
+        'xyz_py',
+        ],
+    entry_points={
+        'console_scripts': [
+            'atom_access = atom_access.cli:main'
+            ]
+        }
+    )
