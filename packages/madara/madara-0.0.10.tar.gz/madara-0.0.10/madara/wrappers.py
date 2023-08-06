a5fd04e01a72ed2d3e42ef88f1668f57419f5d5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:18:37 +0000

    Add package madara/0.0.10/madara-0.0.10.tar.gz

---
+++ packages/madara/madara-0.0.10.tar.gz/madara-0.0.10/madara/wrappers.py
@@ -0,0 +1,14 @@
+from werkzeug.wrappers import Request as RequestBase
+from werkzeug.wrappers import Response as ResponseBase
+from werkzeug.wrappers.json import JSONMixin as _JSONMixin
+import typing as t
+
+
+class Request(RequestBase, _JSONMixin):
+
+    view_args: t.Optional[t.Dict[str, t.Any]] = None
+    endpoint: t.Optional[str] = None
+
+
+class Response(ResponseBase):
+    pass
