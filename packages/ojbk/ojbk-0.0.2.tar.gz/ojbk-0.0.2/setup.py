704f50dec36edac72e02e5bb81d59f2194db20f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:39 +0000

    Add package ojbk/0.0.2/ojbk-0.0.2.tar.gz

---
+++ packages/ojbk/ojbk-0.0.2.tar.gz/ojbk-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ojbk",
+    version="0.0.2",  # Latest version .
+    author="slipper",
+    author_email="r2fscg@gmail.com",
+    description="ok",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/private_repo/uuidentifier",
+    packages=setuptools.find_packages(),
+    install_requires=['codefast', 'authc'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
