b536594804759dc9dd4ba8e6d467bcea02a039ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:25 +0000

    Add package pymultigit/0.0.46/pymultigit-0.0.46.tar.gz

---
+++ packages/pymultigit/pymultigit-0.0.46.tar.gz/pymultigit-0.0.46/pymultigit/configs.py
@@ -0,0 +1,65 @@
+"""
+All configurations for pymultigit
+"""
+
+
+from pytconf import Config, ParamCreator
+
+
+class ConfigDebug(Config):
+    """
+    Parameters for debug
+    """
+    verbose = ParamCreator.create_bool(
+        help_string="be verbose?",
+        default=True,
+    )
+    git_verbose = ParamCreator.create_bool(
+        help_string="add --verbose when running git?",
+        default=False,
+    )
+    terse = ParamCreator.create_bool(
+        help_string="be terse?",
+        default=False,
+    )
+    quiet = ParamCreator.create_bool(
+        help_string="be quiet?",
+        default=False,
+    )
+    git_quiet = ParamCreator.create_bool(
+        help_string="add --quiet when running git?",
+        default=False,
+    )
+    stats = ParamCreator.create_bool(
+        help_string="show statistics are the end?",
+        default=False,
+    )
+    sort = ParamCreator.create_bool(
+        help_string="sort results by project name?",
+        default=True,
+    )
+
+
+class ConfigGrep(Config):
+    """
+    Parameters for the grep command
+    """
+
+    regexp = ParamCreator.create_str(
+        help_string="what regexp to look for?",
+    )
+    files = ParamCreator.create_str_or_none(
+        help_string="what regexp to look for?",
+        default=None,
+    )
+
+
+class ConfigPull(Config):
+    """
+    Parameters for the pull command
+    """
+
+    git_quiet = ParamCreator.create_bool(
+        help_string="add --quiet when running git?",
+        default=True,
+    )
