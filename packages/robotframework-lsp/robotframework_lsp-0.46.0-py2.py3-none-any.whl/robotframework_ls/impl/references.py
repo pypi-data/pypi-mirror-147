4c7202a5f448f06003853120e5a3fa50e7dd97de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:49 +0000

    Add package robotframework-lsp/0.46.0/robotframework_lsp-0.46.0-py2.py3-none-any.whl

---
+++ packages/robotframework-lsp/robotframework_lsp-0.46.0-py2.py3-none-any.whl/robotframework_ls/impl/references.py
@@ -0,0 +1,283 @@
+from typing import List, Optional, Dict, Iterator
+
+from robocorp_ls_core.lsp import LocationTypedDict, RangeTypedDict
+from robocorp_ls_core.robotframework_log import get_logger
+from robotframework_ls.impl.protocols import (
+    ICompletionContext,
+    IRobotDocument,
+    IKeywordFound,
+    IVariablesCollector,
+    IVariableFound,
+    cast_to_keyword_definition,
+    AbstractVariablesCollector,
+)
+import typing
+from robocorp_ls_core.protocols import check_implements
+from robocorp_ls_core.basic import isinstance_name, normalize_filename
+
+
+log = get_logger(__name__)
+
+
+def matches_source(s1: str, s2: str) -> bool:
+    if s1 == s2:
+        return True
+
+    return normalize_filename(s1) == normalize_filename(s2)
+
+
+class _VariableDefinitionsCollector(AbstractVariablesCollector):
+    def __init__(self, robot_string_matcher):
+        from robotframework_ls.impl.string_matcher import RobotStringMatcher
+
+        self.robot_string_matcher: RobotStringMatcher = robot_string_matcher
+        self.matches: List[IVariableFound] = []
+
+    def accepts(self, variable_name):
+        return self.robot_string_matcher.is_same_variable_name(variable_name)
+
+    def on_variable(self, variable_found: IVariableFound):
+        self.matches.append(variable_found)
+
+    def __typecheckself__(self) -> None:
+        _: IVariablesCollector = check_implements(self)
+
+
+def iter_variable_references_in_doc(
+    completion_context: ICompletionContext,
+    doc: IRobotDocument,
+    normalized_name: str,
+) -> Iterator[RangeTypedDict]:
+    from robotframework_ls.impl import ast_utils
+    from robotframework_ls.impl.ast_utils import create_range_from_token
+    from robotframework_ls.impl.variable_completions import collect_variables
+
+    from robotframework_ls.impl.string_matcher import RobotStringMatcher
+    from robocorp_ls_core.lsp import PositionTypedDict
+
+    robot_string_matcher = RobotStringMatcher(normalized_name)
+
+    ast = doc.get_ast()
+    if ast is not None:
+        for node_info in ast_utils.iter_variable_references(ast):
+            completion_context.check_cancelled()
+            if robot_string_matcher.is_same_variable_name(node_info.token.value):
+                yield create_range_from_token(node_info.token)
+
+        collector = _VariableDefinitionsCollector(robot_string_matcher)
+        collect_variables(completion_context, collector, only_current_doc=True)
+
+        variable: IVariableFound
+        for variable in collector.matches:
+            start: PositionTypedDict = {
+                "line": variable.lineno,
+                "character": variable.col_offset,
+            }
+            end: PositionTypedDict = {
+                "line": variable.lineno,
+                "character": variable.end_col_offset,
+            }
+            code_lens_range: RangeTypedDict = {"start": start, "end": end}
+            yield code_lens_range
+
+
+def iter_keyword_references_in_doc(
+    completion_context: ICompletionContext,
+    doc: IRobotDocument,
+    normalized_name: str,
+    keyword_found: Optional[IKeywordFound],
+) -> Iterator[RangeTypedDict]:
+    """
+    :param keyword_found: if given, we'll match if the definition actually
+    maps to the proper place (if not given, we'll just match based on the name
+    without verifying if the definition is the same).
+    """
+    from robotframework_ls.impl import ast_utils
+    from robotframework_ls.impl.find_definition import find_definition
+    from robotframework_ls.impl.completion_context import CompletionContext
+    from robotframework_ls.impl.text_utilities import normalize_robot_name
+
+    ast = doc.get_ast()
+    if ast is not None:
+        # Dict with normalized name -> whether it was found or not previously.
+        found_in_this_doc: Dict[str, bool] = {}
+
+        # Ok, we have the document, now, load the usages.
+        for keyword_usage_info in ast_utils.iter_keyword_usage_tokens(
+            ast, collect_args_as_keywords=True
+        ):
+            completion_context.check_cancelled()
+            keword_name_possibly_dotted = keyword_usage_info.name
+            found_dot_in_usage = "." in keword_name_possibly_dotted
+            if found_dot_in_usage:
+                keword_name_not_dotted = keword_name_possibly_dotted.split(".")[-1]
+            else:
+                keword_name_not_dotted = keword_name_possibly_dotted
+
+            if normalize_robot_name(keword_name_not_dotted) == normalized_name:
+                found_once_in_this_doc = found_in_this_doc.get(
+                    keword_name_possibly_dotted
+                )
+                token = keyword_usage_info.token
+
+                line = token.lineno - 1
+
+                if keyword_found is not None:
+                    if found_once_in_this_doc is None:
+                        # Verify if it's actually the same one (not one defined in
+                        # a different place with the same name).
+
+                        new_ctx = CompletionContext(
+                            doc,
+                            line,
+                            token.col_offset,
+                            workspace=completion_context.workspace,
+                            config=completion_context.config,
+                            monitor=completion_context.monitor,
+                        )
+                        definitions = find_definition(new_ctx)
+                        for definition in definitions:
+                            found = matches_source(
+                                definition.source, keyword_found.source
+                            )
+
+                            if found:
+                                found_once_in_this_doc = found_in_this_doc[
+                                    keword_name_possibly_dotted
+                                ] = True
+                                break
+                        else:
+                            found_once_in_this_doc = found_in_this_doc[
+                                keword_name_possibly_dotted
+                            ] = False
+                            continue
+
+                    if not found_once_in_this_doc:
+                        continue
+
+                token = keyword_usage_info.token
+                if found_dot_in_usage:
+                    # We need to create a new token because we just want to match the name part.
+                    col_offset = token.col_offset + (
+                        len(keword_name_possibly_dotted) - len(keword_name_not_dotted)
+                    )
+                    end_col_offset = token.end_col_offset
+                else:
+                    col_offset = token.col_offset
+                    end_col_offset = token.end_col_offset
+
+                # Ok, we found it, let's add it to the result.
+                yield {
+                    "start": {
+                        "line": line,
+                        "character": col_offset,
+                    },
+                    "end": {
+                        "line": line,
+                        "character": end_col_offset,
+                    },
+                }
+
+
+def references(
+    completion_context: ICompletionContext, include_declaration: bool
+) -> List[LocationTypedDict]:
+
+    token_info = completion_context.get_current_token()
+    if token_info is None:
+        return []
+
+    keyword_found: IKeywordFound
+    if token_info.token.type == token_info.token.KEYWORD_NAME:
+        if isinstance_name(token_info.node, "KeywordName"):
+            from robotframework_ls.impl.find_definition import find_keyword_definition
+
+            definitions = find_keyword_definition(completion_context, token_info)
+            if definitions:
+                for definition in definitions:
+                    as_keyword_definition = cast_to_keyword_definition(definition)
+                    if as_keyword_definition:
+                        keyword_found = as_keyword_definition.keyword_found
+                        return _references_for_keyword_found(
+                            completion_context, keyword_found, include_declaration
+                        )
+
+    current_keyword_definition_and_usage_info = (
+        completion_context.get_current_keyword_definition_and_usage_info()
+    )
+    if current_keyword_definition_and_usage_info is not None:
+        completion_context.monitor.check_cancelled()
+        keyword_definition, _usage_info = current_keyword_definition_and_usage_info
+
+        keyword_found = keyword_definition.keyword_found
+        return _references_for_keyword_found(
+            completion_context, keyword_found, include_declaration
+        )
+
+    return []
+
+
+def _references_for_keyword_found(
+    completion_context: ICompletionContext,
+    keyword_found: IKeywordFound,
+    include_declaration: bool,
+):
+    from robocorp_ls_core import uris
+    from robotframework_ls.impl.text_utilities import normalize_robot_name
+
+    ret: List[LocationTypedDict] = []
+
+    normalized_name = normalize_robot_name(keyword_found.keyword_name)
+    # Ok, we have the keyword definition, now, we must actually look for the
+    # references...
+    if include_declaration:
+        ret.append(
+            {
+                "uri": uris.from_fs_path(keyword_found.source),
+                "range": {
+                    "start": {
+                        "line": keyword_found.lineno,
+                        "character": keyword_found.col_offset,
+                    },
+                    "end": {
+                        "line": keyword_found.end_lineno,
+                        "character": keyword_found.end_col_offset,
+                    },
+                },
+            }
+        )
+
+    from robotframework_ls.impl.workspace_symbols import iter_symbols_caches
+
+    for symbols_cache in iter_symbols_caches(
+        None, completion_context, force_all_docs_in_workspace=True, timeout=999999
+    ):
+        completion_context.check_cancelled()
+        if symbols_cache.has_keyword_usage(normalized_name):
+            doc: Optional[IRobotDocument] = symbols_cache.get_doc()
+            if doc is None:
+                uri = symbols_cache.get_uri()
+                if uri is None:
+                    continue
+
+                doc = typing.cast(
+                    Optional[IRobotDocument],
+                    completion_context.workspace.get_document(
+                        doc_uri=uri, accept_from_file=True
+                    ),
+                )
+
+                if doc is None:
+                    log.debug(
+                        "Unable to load document for getting references with uri: %s",
+                        uri,
+                    )
+                    continue
+
+            ref_range: RangeTypedDict
+            for ref_range in iter_keyword_references_in_doc(
+                completion_context, doc, normalized_name, keyword_found
+            ):
+                ret.append({"uri": doc.uri, "range": ref_range})
+
+    return ret
---
+++ packages/robotframework-lsp/robotframework_lsp-0.46.0-py2.py3-none-any.whl/robotframework_ls/impl/robot_lsp_constants.py
@@ -0,0 +1,133 @@
+from typing import List
+
+OPTION_ROBOT_PYTHON_EXECUTABLE = "robot.python.executable"
+
+OPTION_ROBOT_PYTHON_ENV = "robot.python.env"
+
+OPTION_ROBOT_LANGUAGE_SERVER_TCP_PORT = "robot.language-server.tcp-port"
+
+OPTION_ROBOT_LANGUAGE_SERVER_ARGS = "robot.language-server.args"
+
+OPTION_ROBOT_VARIABLES = "robot.variables"
+OPTION_ROBOT_VARIABLES_LOAD_FROM_ARGUMENTS_FILE = "robot.loadVariablesFromArgumentsFile"
+
+OPTION_ROBOT_LINT_ROBOCOP_ENABLED = "robot.lint.robocop.enabled"
+
+OPTION_ROBOT_LINT_ENABLED = "robot.lint.enabled"
+OPTION_ROBOT_LINT_UNDEFINED_KEYWORDS = "robot.lint.undefinedKeywords"
+OPTION_ROBOT_LINT_UNDEFINED_LIBRARIES = "robot.lint.undefinedLibraries"
+OPTION_ROBOT_LINT_UNDEFINED_RESOURCES = "robot.lint.undefinedResources"
+OPTION_ROBOT_LINT_UNDEFINED_VARIABLE_IMPORTS = "robot.lint.undefinedVariableImports"
+OPTION_ROBOT_LINT_KEYWORD_CALL_ARGUMENTS = "robot.lint.keywordCallArguments"
+
+OPTION_ROBOT_LINT_VARIABLES = "robot.lint.variables"
+OPTION_ROBOT_LINT_IGNORE_VARIABLES = "robot.lint.ignoreVariables"
+
+
+OPTION_ROBOT_CODE_FORMATTER = "robot.codeFormatter"
+
+OPTION_ROBOT_CODE_FORMATTER_ROBOTIDY = "robotidy"
+OPTION_ROBOT_CODE_FORMATTER_BUILTIN_TIDY = "builtinTidy"
+
+OPTION_ROBOT_COMPLETION_KEYWORDS_FORMAT = "robot.completions.keywords.format"
+OPTION_ROBOT_COMPLETION_KEYWORDS_FORMAT_FIRST_UPPER = "First upper"
+OPTION_ROBOT_COMPLETION_KEYWORDS_FORMAT_TITLE = "Title Case"
+OPTION_ROBOT_COMPLETION_KEYWORDS_FORMAT_ALL_LOWER = "all lower"
+OPTION_ROBOT_COMPLETION_KEYWORDS_FORMAT_ALL_UPPER = "ALL UPPER"
+
+
+OPTION_ROBOT_PYTHONPATH = "robot.pythonpath"
+OPTION_ROBOT_LIBRARIES_LIBDOC_NEEDS_ARGS = "robot.libraries.libdoc.needsArgs"
+OPTION_ROBOT_LIBRARIES_LIBDOC_PRE_GENERATE = "robot.libraries.libdoc.preGenerate"
+
+OPTION_ROBOT_WORKSPACE_SYMBOLS_ONLY_FOR_OPEN_DOCS = (
+    "robot.workspaceSymbolsOnlyForOpenDocs"
+)
+
+OPTION_ROBOT_COMPLETION_SECTION_HEADERS_FORM = "robot.completions.section_headers.form"
+OPTION_ROBOT_COMPLETION_SECTION_HEADERS_FORM_PLURAL = "plural"
+OPTION_ROBOT_COMPLETION_SECTION_HEADERS_FORM_SINGULAR = "singular"
+OPTION_ROBOT_COMPLETION_SECTION_HEADERS_FORM_BOTH = "both"
+
+OPTION_ROBOT_SHOW_CODE_LENSES = "robot.codeLens.enable"
+
+# Options which must be set as environment variables.
+ENV_OPTION_ROBOT_DAP_TIMEOUT = "ROBOT_DAP_TIMEOUT"
+
+
+ALL_ROBOT_OPTIONS = frozenset(
+    (
+        OPTION_ROBOT_PYTHON_EXECUTABLE,
+        OPTION_ROBOT_PYTHON_ENV,
+        OPTION_ROBOT_LANGUAGE_SERVER_TCP_PORT,
+        OPTION_ROBOT_LANGUAGE_SERVER_ARGS,
+        OPTION_ROBOT_VARIABLES,
+        OPTION_ROBOT_LINT_ROBOCOP_ENABLED,
+        OPTION_ROBOT_PYTHONPATH,
+        OPTION_ROBOT_COMPLETION_SECTION_HEADERS_FORM,
+        OPTION_ROBOT_WORKSPACE_SYMBOLS_ONLY_FOR_OPEN_DOCS,
+        OPTION_ROBOT_COMPLETION_KEYWORDS_FORMAT,
+        OPTION_ROBOT_LIBRARIES_LIBDOC_NEEDS_ARGS,
+        OPTION_ROBOT_LINT_ENABLED,
+        OPTION_ROBOT_LINT_UNDEFINED_KEYWORDS,
+        OPTION_ROBOT_LINT_UNDEFINED_LIBRARIES,
+        OPTION_ROBOT_LINT_UNDEFINED_RESOURCES,
+        OPTION_ROBOT_LINT_KEYWORD_CALL_ARGUMENTS,
+    )
+)
+
+
+CHECK_IF_LIBRARIES_INSTALLED: List[str] = [
+    # robotframework-appiumlibrary
+    "AppiumLibrary",
+    # robotframework-datadriver
+    "DataDriver",
+    # robotframework-mainframe3270
+    "Mainframe3270",
+    # robotframework-sshlibrary
+    "SSHLibrary",
+    # robotframework-whitelibrary
+    "WhiteLibrary",
+    # rpaframework-google
+    "RPA.Cloud.Google",
+    # rpaframework
+    "RPA.Archive",
+    "RPA.Browser",
+    "RPA.Browser.Selenium",
+    "RPA.Cloud.AWS",
+    "RPA.Cloud.Azure",
+    "RPA.Crypto",
+    "RPA.Database",
+    "RPA.Desktop",
+    "RPA.Desktop.Clipboard",
+    "RPA.Desktop.OperatingSystem",
+    "RPA.Desktop.Windows",
+    "RPA.Dialogs",
+    "RPA.Email.Exchange",
+    "RPA.Email.ImapSmtp",
+    "RPA.Excel.Application",
+    "RPA.Excel.Files",
+    "RPA.FileSystem",
+    "RPA.FTP",
+    "RPA.HTTP",
+    "RPA.Images",
+    "RPA.JavaAccessBridge",
+    "RPA.JSON",
+    "RPA.Netsuite",
+    "RPA.Notifier",
+    "RPA.Outlook.Application",
+    "RPA.PDF",
+    "RPA.Robocloud.Items",
+    "RPA.Robocloud.Secrets",
+    "RPA.Robocorp.Process",
+    "RPA.Robocorp.Vault",
+    "RPA.Robocorp.WorkItems",
+    "RPA.RobotLogListener",
+    "RPA.Salesforce",
+    "RPA.SAP",
+    "RPA.Slack",
+    "RPA.Tables",
+    "RPA.Tasks",
+    "RPA.Twitter",
+    "RPA.Word.Application",
+]
