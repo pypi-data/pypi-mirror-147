7a91909847110dd295959611d3ecb1a5fa9263d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:12 +0000

    Add package TML-toolkit/1.0.1/TML-toolkit-1.0.1.zip

---
+++ packages/TML-toolkit/TML-toolkit-1.0.1.zip/TML-toolkit-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='TML-toolkit',
+    version='1.0.1',
+    author="Sigurd Solberg",
+    author_email="sigroll@gmail.com",
+    description='Tools for topological machine learning',
+    py_modules=[""], #name of python files in my library
+    package_dir={'': 'src'},
+)
