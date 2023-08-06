f08b3db3750998acd2397bd3fd0aa41849a189bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:45 +0000

    Add package imgdb/0.9.1/imgdb-0.9.1-py3-none-any.whl

---
+++ packages/imgdb/imgdb-0.9.1-py3-none-any.whl/imgdb/config.py
@@ -0,0 +1,288 @@
+import os
+from pathlib import Path
+import yaml
+import click
+import logging
+from imgdb.utils import Tcolors, logger
+from imgdb.exceptions import ParseError, ConfigError, ApiError
+
+
+class Config:
+    """Default configuration."""
+
+    DEV_MODE = os.getenv("IMGDB_DEV")
+    HOME = Path("./config_test").resolve() if DEV_MODE else Path.home()
+    XDG_CONFIG_HOME = Path(
+        os.getenv("XDG_CONFIG_HOME")
+        if os.getenv("XDG_CONFIG_HOME")
+        else HOME / ".config"
+    )
+    XDG_DATA_HOME = Path(
+        os.getenv("XDG_DATA_HOME")
+        if os.getenv("XDG_DATA_HOME")
+        else HOME / ".local/share"
+    )
+    XDG_CACHE_HOME = Path(
+        os.getenv("XDG_CACHE_HOME") if os.getenv("XDG_CACHE_HOME") else HOME / ".cache"
+    )
+    IMGDB_CONFIG_HOME = Path(XDG_CONFIG_HOME / "imgdb")
+    IMGDB_DATA_HOME = Path(XDG_DATA_HOME / "imgdb")
+    IMGDB_CACHE_HOME = Path(XDG_CACHE_HOME / "imgdb")
+    CONFIG = Path(IMGDB_CONFIG_HOME / "imgdb.yaml")
+    IMDB_DATASETS_DIR = Path(IMGDB_DATA_HOME / "imdb_datasets")
+
+    DEFAULT_CONFIG = {
+        "general": {
+            "update frequency": "weekly",
+            "download": False,
+            "image editing": False,
+            "log file path": str(Path(IMGDB_CACHE_HOME / "imgdb.log")),
+        },
+        "interface": {
+            "api": {
+                "google search api key": "",
+                "imdb custom search id": "",
+            }
+        },
+    }
+
+
+def check_config_file(debug):
+    """This function checks if a config file exists
+    in the CONFIG directory."""
+
+    # Preparing the necessary configuration & data directories
+    imdb_datasets_dir = (Config.IMDB_DATASETS_DIR, "Imdb datasets")
+    imgdb_cache_dir = (Config.IMGDB_CACHE_HOME, "cache")
+    dir_list = [imdb_datasets_dir, imgdb_cache_dir]
+    for d in dir_list:
+        if not d[0].is_dir():
+            try:
+                d[0].mkdir(parents=True)
+            except Exception as e:
+                click.echo(
+                    Tcolors.FAIL + "Something went wrong while trying"
+                    " to create the '%s' directory!" % d[1] + Tcolors.ENDC
+                )
+                return 0
+
+    logger(
+        debug,
+        Config.DEFAULT_CONFIG["general"]["log file path"],
+    )
+
+    config_file = Config.CONFIG
+    if config_file.is_file():
+        return read_config_yaml(config_file)
+    else:
+        return create_default_yaml(config_file)
+
+
+def create_default_yaml(config_file):
+    """This function creates and saves the default configuration file."""
+
+    config_file_path = config_file
+    imgdb_config_dir = Config.IMGDB_CONFIG_HOME
+
+    if not imgdb_config_dir.is_dir():
+        try:
+            imgdb_config_dir.mkdir(parents=True, exist_ok=True)
+        except Exception as e:
+            logging.critical(
+                "Something went wrong while trying to create"
+                " the configuration directory!"
+            )
+            logging.debug("Error: %s" % e)
+            click.echo(
+                Tcolors.FAIL + "Something went wrong while trying to create"
+                " the configuration directory!" + Tcolors.ENDC
+            )
+            return 0
+
+    try:
+        with open(config_file_path, "w", encoding="utf-8") as config:
+            yaml.safe_dump(
+                Config.DEFAULT_CONFIG,
+                config,
+                encoding="utf-8",
+                allow_unicode=True,
+                default_flow_style=False,
+            )
+        click.echo(
+            Tcolors.OK_GREEN
+            + "➜ The configuration file: %s \nhas been created successfully!"
+            % config_file_path
+            + Tcolors.ENDC
+        )
+    except Exception as e:
+        logging.critical(
+            "Something went wrong while trying to save"
+            " the program's configuration file!"
+        )
+        logging.debug("Error: %s" % e)
+        click.echo(
+            Tcolors.FAIL + "Something went wrong while trying to save"
+            " the program's configuration file!" + Tcolors.ENDC
+        )
+        return 0
+    return parse_config_yaml(Config.DEFAULT_CONFIG, first_run=True)
+
+
+def read_config_yaml(config_file):
+    """This function extracts configuration options
+    from a YAML configuration file."""
+
+    config_file_path = config_file
+    try:
+        with open(config_file_path, "r") as config:
+            current_config = yaml.safe_load(config)
+            logging.debug("Current configuration dictionary: %s" % current_config)
+            return parse_config_yaml(current_config)
+    except yaml.YAMLError as exc:
+        if hasattr(exc, "problem_mark"):
+            mark = exc.problem_mark
+            logging.critical(
+                "The configuration file is malformatted."
+                " Error(s) at line %s, column %s!" % (mark.line + 1, mark.column + 1)
+            )
+            click.echo(
+                Tcolors.FAIL + "The configuration file is malformatted."
+                " Error(s) at line %s, column %s!" % (mark.line + 1, mark.column + 1)
+                + Tcolors.ENDC
+            )
+        else:
+            logging.critical("Failed to parse the configuratiom file!")
+            click.echo(
+                Tcolors.FAIL + "Failed to parse the configuratiom file!" + Tcolors.ENDC
+            )
+        return 0
+    except Exception as e:
+        if hasattr(e, "error_ctx"):
+            logging.critical("Error: %s. Context: %s" % (e.name, e.error_ctx))
+            click.echo(Tcolors.FAIL + e.error_ctx + Tcolors.ENDC)
+        else:
+            logging.critical(
+                "Something went wrong while trying to read"
+                " the program's configuration file!"
+            )
+            logging.debug("Error: %s" % e)
+            click.echo(
+                Tcolors.FAIL + "Something went wrong while trying to read"
+                " the program's configuration file!" + Tcolors.ENDC
+            )
+        return 0
+
+
+def parse_config_yaml(current_config, first_run=False):
+    """This function parses the configuration values contained in the
+    configuration dictionary obtained by reading the YAML file."""
+
+    config_options = current_config
+    used_options = {}
+
+    if config_options is None:
+        return validate_config(
+            cfg_error=True, cfg_error_ctx="The configuration file cannot be empty!"
+        )
+
+    # Aggressively parsing the current configuration options allowing
+    # neither empty nor unknown options
+    for key, value in config_options.items():
+        if key == "general" and isinstance(value, dict):
+            for gen_key, gen_value in config_options[key].items():
+                if gen_key == "log file path" and not isinstance(gen_value, dict):
+                    used_options[gen_key] = gen_value
+                elif gen_key == "download" and not isinstance(gen_value, dict):
+                    used_options[gen_key] = gen_value
+                elif gen_key == "image editing" and not isinstance(gen_value, dict):
+                    used_options[gen_key] = gen_value
+                elif gen_key == "font" and not isinstance(gen_value, dict):
+                    used_options[gen_key] = gen_value
+                elif gen_key == "update frequency" and not isinstance(gen_value, dict):
+                    used_options[gen_key] = gen_value
+                else:
+                    raise ParseError(
+                        "Unknown or malformatted option <%s> in %s section!"
+                        % (gen_key, key)
+                        if not isinstance(gen_value, dict)
+                        else "Invalid value <%s> for option <%s> in '%s' section!"
+                        % (gen_value, gen_key, key)
+                    )
+
+        elif key == "interface" and isinstance(config_options[key], dict):
+            for inter_key, inter_value in value.items():
+                if inter_key == "api" and isinstance(inter_value, dict):
+                    for api_key, api_value in value[inter_key].items():
+                        if api_key == "google search api key" and not isinstance(
+                            api_value, dict
+                        ):
+                            used_options[api_key] = api_value
+                        elif api_key == "imdb custom search id" and not isinstance(
+                            api_value, dict
+                        ):
+                            used_options[api_key] = api_value
+                        else:
+                            raise ParseError(
+                                "Unknown or malformatted option <%s> in %s section!"
+                                % (api_key, key)
+                                if not isinstance(api_value, dict)
+                                else "Invalid value <%s> for option <%s> in '%s' section!"
+                                % (api_value, api_key, key)
+                            )
+                else:
+                    raise ParseError(
+                        "Unknown or malformatted option <%s> in '%s' section!"
+                        % (inter_key, key)
+                    )
+        else:
+            raise ParseError(
+                "Unknown or malformatted section <%s> in the configuration file!" % key
+            )
+    return validate_config(used_options, check_api=first_run)
+
+
+def validate_config(
+    config_options={}, check_api=False, cfg_error=None, cfg_error_ctx=None
+):
+    """This function checks if configuration values are not empty or null."""
+
+    if cfg_error and cfg_error_ctx:
+        raise ConfigError(cfg_error_ctx)
+        return 0
+
+    try:
+        if check_api:
+            raise ApiError(
+                "You are running the application for the first time."
+                " Please, provide a working Google Search API key in the configuration file!"
+            )
+    except ApiError as e:
+        logging.critical("Error: %s. Context: %s" % (e.name, e.error_ctx))
+        click.echo(Tcolors.FAIL + e.error_ctx + Tcolors.ENDC)
+        return 0
+
+    api_keys = ["google search api key", "imdb custom search id"]
+    for key in api_keys:
+        try:
+            if key not in config_options:
+                raise ApiError(
+                    "You must provide a working Google Custom Search"
+                    " API key in the configuration file!"
+                )
+                break
+        except ApiError as e:
+            logging.critical("Error: %s. Context: %s" % (e.name, e.error_ctx))
+            click.echo(Tcolors.FAIL + e.error_ctx + Tcolors.ENDC)
+            return 0
+
+    for key, value in config_options.items():
+        try:
+            if value is None or value == "":
+                raise ConfigError(
+                    "The <%s> option in the configuration file cannot be empty!" % key
+                )
+        except ConfigError as e:
+            logging.critical("Error: %s. Context: %s" % (e.name, e.error_ctx))
+            click.echo(Tcolors.FAIL + e.error_ctx + Tcolors.ENDC)
+            return 0
+    return config_options
---
+++ packages/imgdb/imgdb-0.9.1-py3-none-any.whl/imgdb/image_processor.py
@@ -0,0 +1,254 @@
+from wand.image import Image
+from wand.color import Color
+from wand.drawing import Drawing
+from wand.display import display
+from wand.font import Font
+import click
+from imgdb.utils import Tcolors
+from imgdb.config import Config
+import textwrap
+import logging
+from importlib.resources import files
+import imgdb.resources.images as img_dir
+
+
+def generate_media_image(
+    movie_title,
+    imdb_rating,
+    rt_rating,
+    poster_filename,
+    poster_filepath,
+    font=None,
+):
+
+    if poster_filename:
+        saved_image_filename = poster_filename + "_by_imgdb"
+    else:
+        click.echo(
+            Tcolors.WARNING + "Downloaded image filename"
+            " cannot be null!" + Tcolors.ENDC
+        )
+        logging.critical("Downloaded image filename cannot be null!")
+        return
+
+    try:
+        float(imdb_rating)
+    except (ValueError, TypeError):
+        imdb_is_rating_float = False
+    else:
+        imdb_is_rating_float = True
+        imdb_rating_to_perc = (
+            imdb_rating if isinstance(imdb_rating, float) else float(imdb_rating)
+        ) * 10
+
+    try:
+        float(rt_rating)
+    except (ValueError, TypeError):
+        rt_is_rating_int = False
+    else:
+        rt_is_rating_int = True
+        rt_rating_to_int = rt_rating if isinstance(rt_rating, int) else int(rt_rating)
+
+    if not imdb_is_rating_float and not rt_is_rating_int:
+        click.echo(
+            Tcolors.WARNING + "Cannot produce picture"
+            " without ratings!" + Tcolors.ENDC
+        )
+        logging.critical("Cannot produce picture without ratings!")
+        return
+
+    else:
+        if imdb_is_rating_float:
+            imdb_rounded_rating = round_ratings(imdb_rating_to_perc)
+            logging.debug("IMDb's rounded rating is: %s" % imdb_rounded_rating)
+        if rt_is_rating_int:
+            rt_rounded_rating = round_ratings(rt_rating_to_int)
+            logging.debug("RT's rounded rating is: %s" % rt_rounded_rating)
+
+    # Image processing
+    with Image(width=800, height=800, background="#3D3737") as canvas:
+        with Drawing() as context:
+            # Poster image
+            with Image(filename=poster_filepath) as poster:
+                poster_width = 380
+                poster.transform(resize=f"{poster_width}x")
+                while poster.height > 570:
+                    poster_width -= 5
+                    poster.transform(resize=f"{poster_width}x")
+                logging.debug("Poster's final height %s" % poster.height)
+                canvas.composite(poster, left=40, top=180)
+
+            # Ring charts
+            ## Charts' parameters
+            ring_size = 140
+            x_ring_chart = 460
+            y_imdb_rating = 200
+            y_rt_rating = 440
+            x_rating_text_offset = 130
+            y_rating_text_offset = 170
+            x_shadow_text_offset = 7
+            y_shadow_text_offset = 7
+
+            ## IMDb ring chart rating
+            if imdb_is_rating_float:
+                imdb_rating_img = files(img_dir).joinpath(
+                    f"imdb_{imdb_rounded_rating}.png"
+                )
+                with Image(filename=imdb_rating_img) as img_imdb:
+                    img_imdb.resize(ring_size, ring_size)
+                    canvas.composite(img_imdb, left=x_ring_chart, top=y_imdb_rating)
+
+            ## RT ring chart rating
+            if rt_is_rating_int:
+                rt_rating_img = files(img_dir).joinpath(f"rt_{rt_rounded_rating}.png")
+                with Image(filename=rt_rating_img) as img_rt:
+                    img_rt.resize(ring_size, ring_size)
+                    canvas.composite(img_rt, left=x_ring_chart, top=y_rt_rating)
+
+            # Text
+            x_title_text = 25
+            x_shadow_title_offset = 5
+            y_shadow_title_offset = 5
+            fontsize_title = 50
+            fontsize_ratings = 70
+            # context.font = "Mosk-Ultra-Bold-900"
+            if font:
+                context.font = font
+            else:
+                context.font = "Maler-Regular"
+
+            header_text = textwrap.fill(movie_title, width=15)
+            font_metrics = context.get_font_metrics(
+                canvas, header_text.upper(), multiline=True
+            )
+            logging.debug("Initial font metrics: %s" % str(font_metrics))
+
+            while font_metrics.text_width > 600 or font_metrics.text_height > 150:
+                context.font_size -= 1
+                font_metrics = context.get_font_metrics(
+                    canvas, header_text.upper(), multiline=True
+                )
+            else:
+                while font_metrics.text_width < 600 and font_metrics.text_height < 150:
+                    context.font_size += 1
+                    font_metrics = context.get_font_metrics(
+                        canvas, header_text.upper(), multiline=True
+                    )
+            logging.debug("Corrected font metrics: %s" % str(font_metrics))
+
+            y_title = int(
+                ((180 - font_metrics.text_height) / 2) + font_metrics.character_height
+            )
+            logging.debug("Title's y-axis value: %s" % y_title)
+            context.push()
+
+            ## Text shadow
+            context.fill_color = Color("rgba(29, 29, 29, 0.87)")
+            context.text(
+                x=x_title_text + x_shadow_title_offset,
+                y=y_title + y_shadow_title_offset,
+                body=header_text.upper(),
+            )
+            context.font_size = fontsize_ratings
+            if imdb_is_rating_float:
+                context.text(
+                    x=x_ring_chart + x_rating_text_offset + x_shadow_text_offset,
+                    y=y_imdb_rating + y_rating_text_offset + y_shadow_text_offset,
+                    body=str(imdb_rating),
+                )
+            if rt_is_rating_int:
+                context.text(
+                    x=x_ring_chart + x_rating_text_offset + x_shadow_text_offset,
+                    y=y_rt_rating + y_rating_text_offset + y_shadow_text_offset,
+                    body=str(rt_rating) + "%",
+                )
+
+            ## Main text
+            context.pop()
+            context.fill_color = Color("white")
+            context.text(x=x_title_text, y=y_title, body=header_text.upper())
+            context.font_size = fontsize_ratings
+            if imdb_is_rating_float:
+                context.text(
+                    x=x_ring_chart + x_rating_text_offset,
+                    y=y_imdb_rating + y_rating_text_offset,
+                    body=str(imdb_rating),
+                )
+            if rt_is_rating_int:
+                context.text(
+                    x=x_ring_chart + x_rating_text_offset,
+                    y=y_rt_rating + y_rating_text_offset,
+                    body=str(rt_rating) + "%",
+                )
+
+            # Text stroke (Obsolete)
+            # context.fill_color = Color("transparent")
+            # context.stroke_color = Color("rgba(29, 29, 29, 0.87)")
+            # context.stroke_width = 1
+            # context.text(x=21, y=75, body=movie_title.upper())
+            # context.font_size = fontsize_ratings
+            # if imdb_is_rating_float:
+            #     context.text(x=485, y=290, body=str(imdb_rating))
+            # if rt_is_rating_int:
+            #     context.text(x=485, y=510, body=str(rt_rating) + "%")
+
+            context(canvas)
+            canvas.format = "png"
+
+            if Config.DEV_MODE:
+                display(canvas)
+
+        canvas.save(filename=f"{saved_image_filename}.png")
+        logging.info("Edited image saved as: %s" % f"{saved_image_filename}.png")
+        click.echo(
+            Tcolors.OK_GREEN
+            + "Edited image saved as: %s" % f"{saved_image_filename}.png"
+            + Tcolors.ENDC
+        )
+
+
+def round_ratings(rating):
+    """This functions takes in a movie rating as an argument
+    and returns its corresponding rounded value
+    that can be used to choose the correct ring chart picture."""
+
+    rounded_rating_value = None
+    rating_steps = [0, 5, 10, 20, 25, 30, 40, 50, 60, 70, 75, 80, 90, 95, 100]
+
+    if rating in rating_steps:
+        rounded_rating_value = rating_steps[rating_steps.index(rating)]
+
+    # Using zero or 100 only when the rating exactly matches those values.
+    # In other words, we don't round values like 2 or 98 for example
+    # to zero or 100 respectively!
+    elif rating in range(1, 3):
+        rounded_rating_value = 5
+    elif rating in range(98, 100):
+        rounded_rating_value = 95
+    else:
+        for step in rating_steps:
+            rating_diff = rating - step
+            rating_diff_abs = abs(rating_diff)
+
+            if (
+                rating in range(3, 10)
+                or rating in range(21, 30)
+                or rating in range(71, 80)
+                or rating in range(91, 100)
+            ):
+                difference_threshold = 2.5
+            else:
+                difference_threshold = 5
+                rounding_increment = 10
+
+            if rating_diff_abs < difference_threshold:
+                rounded_rating_value = step
+                break
+            elif (
+                rating_diff_abs == difference_threshold and difference_threshold != 2.5
+            ):
+                rounded_rating_value = step + rounding_increment
+                break
+
+    logging.debug("Rounded rating value is: %s" % rounded_rating_value)
+    return rounded_rating_value
