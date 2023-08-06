3fe877ee9934d6ceeb99499879da983c8a8a3b7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:03 +0000

    Add package alactions/0.0.8/alactions-0.0.8-py3-none-any.whl

---
+++ packages/alactions/alactions-0.0.8-py3-none-any.whl/alactions/utils.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+import os
+
+def echo(value=None, product=None, upstream=None):
+    print('--')
+    print(upstream)
+    print('--')
+    Path(product).touch()
+
+def folder(value=None, product=None, upstream=None):
+    print('--')
+    print(upstream)
+    print('--')
+    Path(product).write_text('alactions')
+
+def secret(secret, product=None, upstream=None):  
+    if secret.startswith('$'):
+        secret = secret[1:]
+        
+    print(product, secret,"<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
+    Path(product).write_text(secret)    
