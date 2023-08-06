d701d0eaa9cc975176b9f67a9d0b83eb4bdb108d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:32 +0000

    Add package cosecurity-utility-internal-lib/0.0.3/cosecurity-utility-internal-lib-0.0.3.tar.gz

---
+++ packages/cosecurity-utility-internal-lib/cosecurity-utility-internal-lib-0.0.3.tar.gz/cosecurity-utility-internal-lib-0.0.3/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='cosecurity-utility-internal-lib',
+    version='0.0.3',
+    description='Built-in utility library for application development by the CoSecurity infrastructure',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'python-dotenv==0.20.0',
+        'psycopg2',
+        'boto3==1.21.36'
+    ]
+)
