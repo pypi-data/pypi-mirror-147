bc97b08e5792a73cf7f2280f37c59f762918dc6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:07 +0000

    Add package let-me-google-that/0.0.1/let_me_google_that-0.0.1.tar.gz

---
+++ packages/let-me-google-that/let_me_google_that-0.0.1.tar.gz/let_me_google_that-0.0.1/let_me_google_that/let_me_google_that/__init__.py
@@ -0,0 +1,4 @@
+__version__ = '0.1.0'
+import webbrowser
+def let_me_google_that(What_do_you_want_to_google):
+    webbrowser.open("https://www.google.com/search?q=" + What_do_you_want_to_google)
---
+++ packages/let-me-google-that/let_me_google_that-0.0.1.tar.gz/let_me_google_that-0.0.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['let_me_google_that', 'let_me_google_that.let_me_google_that']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'let-me-google-that',
+    'version': '0.0.1',
+    'description': 'Google for you because you to lazy to do that',
+    'long_description': None,
+    'author': 'Your Name',
+    'author_email': 'you@example.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.5,<4.0',
+}
+
+
+setup(**setup_kwargs)
