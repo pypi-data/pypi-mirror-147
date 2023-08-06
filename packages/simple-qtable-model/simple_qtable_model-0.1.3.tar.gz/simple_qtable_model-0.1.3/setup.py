fff406cba8926c7c3ad5f5d6ad1e62c3600bb71f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:39 +0000

    Add package simple-qtable-model/0.1.3/simple_qtable_model-0.1.3.tar.gz

---
+++ packages/simple-qtable-model/simple_qtable_model-0.1.3.tar.gz/simple_qtable_model-0.1.3/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="simple_qtable_model",
+    author="Dixon Leuenberger",
+    version="0.1.3",
+    description="create stylized table widget",
+    keywords="qTableModel, pypi, package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://git.is4s.com/DixonLeuenberger/table_model_package",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3 :: Only",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=["PyQt5"],
+    extras_require={
+        "dev": ["check-manifest"],
+    },
+)
