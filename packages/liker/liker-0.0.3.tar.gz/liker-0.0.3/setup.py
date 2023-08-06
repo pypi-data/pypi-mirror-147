60444be05312634121246e599640b20723510438
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:41 +0000

    Add package liker/0.0.3/liker-0.0.3.tar.gz

---
+++ packages/liker/liker-0.0.3.tar.gz/liker-0.0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+  
+setup(
+    name="liker",
+    version="0.0.3",
+    author="kwhkim",
+    description="functions that mimic R",
+    long_description ="file: README.md",
+    py_modules=["liker"]
+)
+# https://towardsdatascience.com/setuptools-python-571e7d5500f2
+
