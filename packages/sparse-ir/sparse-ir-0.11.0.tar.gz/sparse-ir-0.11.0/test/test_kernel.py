3d78c4e5edd333b0bb2a04af52f1ad706f8104e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:45:01 +0000

    Add package sparse-ir/0.11.0/sparse-ir-0.11.0.tar.gz

---
+++ packages/sparse-ir/sparse-ir-0.11.0.tar.gz/sparse-ir-0.11.0/test/test_kernel.py
@@ -0,0 +1,41 @@
+# Copyright (C) 2020-2022 Markus Wallerberger, Hiroshi Shinaoka, and others
+# SPDX-License-Identifier: MIT
+import numpy as np
+import pytest
+
+from sparse_ir import kernel
+from sparse_ir import gauss
+
+KERNELS = [
+    kernel.LogisticKernel(9),
+    kernel.RegularizedBoseKernel(8),
+    kernel.LogisticKernel(120_000),
+    kernel.RegularizedBoseKernel(127_500),
+    kernel.LogisticKernel(40_000).get_symmetrized(-1),
+    kernel.RegularizedBoseKernel(35_000).get_symmetrized(-1),
+    ]
+
+
+@pytest.mark.parametrize("K", KERNELS)
+def test_accuracy(K):
+    dtype = np.float32
+    dtype_x = np.float64
+
+    rule = gauss.legendre(10, dtype)
+    hints = K.sve_hints(2.2e-16)
+    gauss_x = rule.piecewise(hints.segments_x)
+    gauss_y = rule.piecewise(hints.segments_y)
+    eps = np.finfo(dtype).eps
+    tiny = np.finfo(dtype).tiny / eps
+
+    result = kernel.matrix_from_gauss(K, gauss_x, gauss_y)
+    result_x = kernel.matrix_from_gauss(
+                    K, gauss_x.astype(dtype_x), gauss_y.astype(dtype_x))
+    magn = np.abs(result_x).max()
+    np.testing.assert_allclose(result, result_x, atol = 2 * magn * eps, rtol=0,
+                               err_msg="absolute precision too poor")
+
+    with np.errstate(invalid='ignore'):
+        reldiff = np.where(np.abs(result_x) < tiny, 1, result / result_x)
+    np.testing.assert_allclose(reldiff, 1, atol=100 * eps, rtol=0,
+                               err_msg="relative precision too poor")
