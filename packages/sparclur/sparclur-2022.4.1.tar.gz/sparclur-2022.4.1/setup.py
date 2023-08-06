9881cc3bb0c229418142c0f94ceb6ec851e14178
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:24 +0000

    Add package sparclur/2022.4.1/sparclur-2022.4.1.tar.gz

---
+++ packages/sparclur/sparclur-2022.4.1.tar.gz/sparclur-2022.4.1/setup.py
@@ -0,0 +1,49 @@
+import setuptools
+
+from setuptools import setup
+import pathlib
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="sparclur",
+    version="2022.04.01",
+    author="Shawn Davis",
+    author_email="shawn@levelupresearch.com",
+    description="Tools for analyzing PDF files and comparing PDF parsers",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/levelupresearch/sparclur",
+    packages=setuptools.find_packages('sparclur', 'sparclur.*'),
+    python_requires='>=3.8',
+    license='Apache-2.0',
+    install_requires=[
+        'pandas',
+        'func-timeout',
+        'numpy',
+        'scikit-image',
+        'scikit-learn',
+        'Pebble',
+        'tqdm',
+        'PyMuPDF',
+        'Pillow',
+        'matplotlib',
+        'Pillow',
+        'pytesseract',
+        'spacy',
+        'dill',
+        'pdfminer.six',
+        'pypdfium2',
+        'opencv-python',
+        'ImageHash',
+        'PyYAML',
+        'mmh3',
+        'seaborn',
+        'plotly',
+        'docstring-inheritance'
+    ]
+)
