a2ac71c223b6ec492862ad931922cc9c5282b5cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:50 +0000

    Add package rev-ai/2.15.0/rev_ai-2.15.0.tar.gz

---
+++ packages/rev-ai/rev_ai-2.15.0.tar.gz/rev_ai-2.15.0/src/rev_ai/models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""Models"""
+
+from .customvocabulary import CustomVocabulary
+from .streaming import MediaConfig
+from .asynchronous import Job, JobStatus, Account, Transcript, Monologue, Element, CaptionType, \
+    SpeakerName
+from .insights import TopicExtractionJob, TopicExtractionResult, Topic, Informant
