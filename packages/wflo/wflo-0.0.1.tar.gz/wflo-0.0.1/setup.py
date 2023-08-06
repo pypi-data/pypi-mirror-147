e1c5ef101824e5107c4ca28e9ecae377cc477874
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:02 +0000

    Add package wflo/0.0.1/wflo-0.0.1.tar.gz

---
+++ packages/wflo/wflo-0.0.1.tar.gz/wflo-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+VERSION = "0.0.1"
+DESCRIPTION = "work in progress"
+LONG_DESCRIPTION = "work in progress"
+setup(
+    name="wflo",
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    url="https://github.com/el-mrt",
+    author='Martin Gruber',
+    author_email='Martin.Gruber@mailbox.tu-dresden.de',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
