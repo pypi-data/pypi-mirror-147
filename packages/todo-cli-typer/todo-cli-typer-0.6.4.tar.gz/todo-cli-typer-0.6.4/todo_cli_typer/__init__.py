016989c6d9b0aea54cf8cc05df51d224d73679e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:44:30 +0000

    Add package todo-cli-typer/0.6.4/todo-cli-typer-0.6.4.tar.gz

---
+++ packages/todo-cli-typer/todo-cli-typer-0.6.4.tar.gz/todo-cli-typer-0.6.4/todo_cli_typer/__init__.py
@@ -0,0 +1,80 @@
+"""Top-level package for todo-cli-typer."""
+
+__app_name__ = 'todot'
+__version__ = '0.6.4'
+
+from dataclasses import dataclass
+from enum import Enum
+from datetime import datetime
+from typing import Any
+
+from logging_utils_tddschn import get_logger # type: ignore
+logger, _ = get_logger(__app_name__)
+
+
+
+(
+    SUCCESS,
+    DIR_ERROR,
+    FILE_ERROR,
+    DB_CONNECTION_ERROR,
+    DB_READ_ERROR,
+    DB_WRITE_ERROR,
+    DB_DELETE_ERROR,
+    DB_INSERT_ERROR,
+    DB_UPDATE_ERROR,
+    JSON_ERROR,
+    ID_ERROR,
+) = range(11)
+
+ERRORS = {
+    DIR_ERROR: 'config directory error',
+    FILE_ERROR: 'config file error',
+    DB_CONNECTION_ERROR: 'database connection error',
+    DB_READ_ERROR: 'database read error',
+    DB_WRITE_ERROR: 'database write error',
+    ID_ERROR: 'to-do id error',
+    DB_DELETE_ERROR: 'database delete error',
+    DB_INSERT_ERROR: 'database insert error',
+    DB_UPDATE_ERROR: 'database update error',
+}
+class Priority(str, Enum):
+    """Priority levels for to-dos."""
+
+    LOW = 'low'
+    MEDIUM = 'medium'
+    HIGH = 'high'
+
+class Status(str, Enum):
+    """Status of a to-do."""
+    TODO = 'todo'
+    DONE = 'done'
+    DELETED = 'deleted'
+    CANCELLED = 'cancelled'
+    WIP = 'wip'
+
+@dataclass
+class TodoItem:
+    """Create a to-do item."""
+    id: int
+    description: str
+    priority: Priority
+    status: Status
+    project: str
+    tags: str
+    due_date: datetime | None
+
+@dataclass
+class DBResponse:
+    todo_list: list[TodoItem]
+    error: int
+
+@dataclass
+class SessionResponse:
+    session: Any
+    error: int
+
+@dataclass
+class CurrentTodo:
+    todo: TodoItem | None
+    error: int
