1fab7532975ebd5c3abb61c8f0fa1d756757d9ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:00 +0000

    Add package astartes/0.0.0/astartes-0.0.0-py3-none-any.whl

---
+++ packages/astartes/astartes-0.0.0-py3-none-any.whl/extended_train_test_split/extended_train_test_split.py
@@ -0,0 +1,36 @@
+import numpy as np
+from extended_train_test_split.samplers import (
+    random,
+    kennard_stone,
+)
+
+
+def train_test_split(
+    X: np.array,
+    y: np.array = None,
+    test_size: float = 0.25,
+    train_size: float = 0.75,
+    splitter: str = 'random',
+    hopts: dict = {},
+):
+    if splitter == 'random':
+        return random(
+            X,
+            y=y,
+            test_size=test_size,
+            train_size=train_size,
+            shuffle=hopts.get("shuffle", False),
+            random_state=hopts.get("random_state", None),
+            stratify=hopts.get("stratify", None),
+        )
+    elif splitter == 'kennard_stone':
+        return kennard_stone(
+            X,
+            y=y,
+            test_size=test_size,
+            train_size=train_size,
+        )
+    else:
+        raise NotImplementedError(
+            f'Splitter "{splitter}" has not been implemented.'
+        )
---
+++ packages/astartes/astartes-0.0.0-py3-none-any.whl/test/samplers/test_DBSCAN.py
@@ -0,0 +1,26 @@
+import os
+import sys
+import unittest
+
+import numpy as np
+
+from extended_train_test_split.samplers import dbscan
+
+
+class Test_dbscan(unittest.TestCase):
+    """
+    Test the various functionalities of dbscan.
+    """
+
+    @classmethod
+    def setUpClass(self):
+        return
+
+    def test_dbscan(self):
+        """
+        """
+        return
+
+
+if __name__ == '__main__':
+    unittest.main()
