d8fb41854fd630615d2e80aae858c190e8999213
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:00 +0000

    Add package cleverminer/0.0.91/cleverminer-0.0.91-py3-none-any.whl

---
+++ packages/cleverminer/cleverminer-0.0.91-py3-none-any.whl/cleverminer/__init__.py
@@ -0,0 +1,15 @@
+from cleverminer.cleverminer import cleverminer
+
+# print disclaimer
+print(
+    "***********************************************************************************************************************************************************************")
+print("Cleverminer version ", cleverminer._get_ver(cleverminer))
+print(
+    "IMPORTANT NOTE: this is preliminary development version of CleverMiner procedure. This procedure is under intensive development and early released for educational use,")
+print(
+    "    so there is ABSOLUTELY no guarantee of results, possible gaps in functionality and no guarantee of keeping syntax and parameters as in current version.")
+print(
+    "    (That means we need to tidy up and make proper design, input validation, documentation and instrumentation before launch)")
+print("This version is for personal and educational use only.")
+print(
+    "***********************************************************************************************************************************************************************")
