75b49ccbae3524ff4cfa5c5c11213689e774bbfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:44 +0000

    Add package EventHubPublisher/0.0.19/EventHubPublisher-0.0.19.tar.gz

---
+++ packages/EventHubPublisher/EventHubPublisher-0.0.19.tar.gz/EventHubPublisher-0.0.19/EventPublisher/OrderScheduled.py
@@ -0,0 +1,15 @@
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__)))
+
+import EventHub.SendEvents as SendEvents
+
+def run(env_name, client_code, account_name, events_json):
+    return SendEvents.send_events('order.scheduled', env_name, client_code, account_name, events_json)
+
+if len(sys.argv) > 0 and args.env != None:
+    print(SendEvents.run('order.scheduled'))
+    print('order.create events were sent.')
+
+
