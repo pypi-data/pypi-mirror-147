20db032d24499081b3b3b7704e96d2bbf5473260
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:44:20 +0000

    Add package learning-loop-node/0.7.16/learning_loop_node-0.7.16-py3-none-any.whl

---
+++ packages/learning-loop-node/learning_loop_node-0.7.16-py3-none-any.whl/learning_loop_node/detector/detections.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+from typing import List, Optional
+from dataclasses import dataclass, field
+
+from learning_loop_node.detector.box_detection import BoxDetection
+from learning_loop_node.detector.point_detection import PointDetection
+
+
+@dataclass
+class Detections:
+    box_detections: List[BoxDetection] = field(default_factory=list)
+    point_detections: List[PointDetection] = field(default_factory=list)
+    tags: Optional[List[str]] = field(default_factory=list)
+    date: Optional[str] = datetime.now().isoformat(sep='_', timespec='milliseconds')
+
+
+    def __len__(self):
+        return len(self.box_detections) + len(self.point_detections)
---
+++ packages/learning-loop-node/learning_loop_node-0.7.16-py3-none-any.whl/learning_loop_node/detector/tests/testing_detector.py
@@ -0,0 +1,19 @@
+from learning_loop_node.detector import BoxDetection, PointDetection, Detections
+from learning_loop_node import Detector, ModelInformation
+
+
+class TestingDetector(Detector):
+    __test__ = False
+
+    def __init__(self) -> None:
+        super().__init__('mocked')
+
+    def init(self,  model_info: ModelInformation, model_root_path: str):
+        self.model_info = model_info
+
+    def evaluate(self, image: bytes) -> Detections:
+        return Detections(
+            box_detections=[BoxDetection('some_category_name', 1, 2, 3, 4, 'some_model', .42)],
+            point_detections=[PointDetection('some_category_name', 10, 12, 'some_model', .42)]
+        )
+    
