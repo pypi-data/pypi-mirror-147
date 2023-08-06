8bd3cadc95d2d411020c6c806acba101a8a4271e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:02 +0000

    Add package WES-Calculation/1.0.13/WES_Calculation-1.0.13.tar.gz

---
+++ packages/WES-Calculation/WES_Calculation-1.0.13.tar.gz/WES_Calculation-1.0.13/WES_Calculation/greenampt.py
@@ -0,0 +1,20 @@
+'''
+Author: BDFD
+Date: 2022-03-01 17:28:34
+LastEditTime: 2022-03-02 13:01:39
+LastEditors: BDFD
+Description: 
+FilePath: \5.2-PyPi-WES_Calculation\WES_Calculation\greenampt.py
+'''
+
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FixedLocator, FixedFormatter
+import numpy as np
+from numpy import *
+import statistics
+import io
+import base64
+
+
+def greenampt(result, test2, test3, i1, i2, i3):
+    pass
