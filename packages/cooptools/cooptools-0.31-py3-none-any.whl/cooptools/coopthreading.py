3077033b04c8264172e941011aa575f05f0af194
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:50 +0000

    Add package cooptools/0.31/cooptools-0.31-py3-none-any.whl

---
+++ packages/cooptools/cooptools-0.31-py3-none-any.whl/cooptools/coopthreading.py
@@ -0,0 +1,12 @@
+import functools
+from threading import Lock
+
+def synchronized(lock: Lock):
+    """ Synchronization decorator """
+    def wrap(f):
+        @functools.wraps(f)
+        def newFunction(*args, **kw):
+            with lock:
+                return f(*args, **kw)
+        return newFunction
+    return wrap
