5577ec75c55678761c5611a176306f3346c0605b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:48:13 +0000

    Add package todo-cli-tddschn/0.2.11/todo-cli-tddschn-0.2.11.tar.gz

---
+++ packages/todo-cli-tddschn/todo-cli-tddschn-0.2.11.tar.gz/todo-cli-tddschn-0.2.11/setup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['todo_cli_tddschn']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['colorama==0.4.4',
+ 'fastapi[api]>=0.75.2,<0.76.0',
+ 'logging-utils-tddschn>=0.1.5,<0.2.0',
+ 'shellingham==1.4.0',
+ 'sqlmodel>=0.0.6,<0.0.7',
+ 'tabulate>=0.8.9,<0.9.0',
+ 'typer>=0.4.1,<0.5.0',
+ 'uvicorn[api]>=0.17.6,<0.18.0']
+
+entry_points = \
+{'console_scripts': ['todo = todo_cli_tddschn.cli:app']}
+
+setup_kwargs = {
+    'name': 'todo-cli-tddschn',
+    'version': '0.2.11',
+    'description': 'CLI Todo app made with typer and sqlite',
+    'long_description': None,
+    'author': 'Xinyuan Chen',
+    'author_email': '45612704+tddschn@users.noreply.github.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
