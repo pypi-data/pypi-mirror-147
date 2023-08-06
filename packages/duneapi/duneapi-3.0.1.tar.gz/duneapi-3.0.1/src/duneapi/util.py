f06bfd691879c568952f7c27c31bdd1ed8644fd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:50 +0000

    Add package duneapi/3.0.1/duneapi-3.0.1.tar.gz

---
+++ packages/duneapi/duneapi-3.0.1.tar.gz/duneapi-3.0.1/src/duneapi/util.py
@@ -0,0 +1,33 @@
+"""Utility methods to support Dune API"""
+import collections
+from datetime import datetime
+from typing import Any, Hashable
+
+
+def postgres_date(date_str: str) -> datetime:
+    """Parse a postgres compatible date string into datetime object"""
+    return datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S+00:00")
+
+
+def datetime_parser(dct: dict[str, Any]) -> dict[str, Any]:
+    """
+    Used as object hook in json loads method to parse postgres dates strings
+    """
+    for key, val in dct.items():
+        if isinstance(val, str):
+            try:
+                dct[key] = datetime.strptime(val, "%Y-%m-%dT%H:%M:%S+00:00")
+            except ValueError:
+                pass
+    return dct
+
+
+def open_query(filepath: str) -> str:
+    """Opens `filename` and returns as string"""
+    with open(filepath, "r", encoding="utf-8") as query_file:
+        return query_file.read()
+
+
+def duplicates(arr: list[Hashable]) -> list[Hashable]:
+    """Detects and returns duplicates in array"""
+    return [item for item, count in collections.Counter(arr).items() if count > 1]
