f37edbfd92d7e7d04eb21a9a09ed8874a8d789fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:55 +0000

    Add package probnode/0.1.2/probnode-0.1.2-py3-none-any.whl

---
+++ packages/probnode/probnode-0.1.2-py3-none-any.whl/probnode/probability/event.py
@@ -0,0 +1,18 @@
+from probnode.interface.ievent import IEvent
+
+
+class Event(IEvent):
+  name: str
+
+  def __init__(self, name: str):
+    super().__init__()
+    self.name = name
+
+  def __repr__(self) -> str:
+    return f"{self.__class__.__name__}{self.__dict__}"
+
+
+class SureEvent(IEvent):
+
+  def __repr__(self) -> str:
+    return f"SureEvent{self.__dict__}"
