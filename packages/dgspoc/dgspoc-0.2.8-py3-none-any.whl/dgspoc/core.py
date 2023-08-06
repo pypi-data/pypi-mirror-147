5441b9184843accdb150e5779ef3bfd9b188e313
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:41 +0000

    Add package dgspoc/0.2.8/dgspoc-0.2.8-py3-none-any.whl

---
+++ packages/dgspoc/dgspoc-0.2.8-py3-none-any.whl/dgspoc/core.py
@@ -0,0 +1,7 @@
+"""Module containing logic for describe-get-system proof of concept."""
+
+
+class Dgs:
+    """Describe-Get-System class"""
+    def __init__(self, *args, **kwargs):    # noqa
+        NotImplementedError('TODO: Need to implement Dgs class')
