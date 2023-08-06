a470545b34e56b6de8aa35b78544b2b957463ded
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:12 +0000

    Add package nextchat/1.0.0.2/nextchat-1.0.0.2.tar.gz

---
+++ packages/nextchat/nextchat-1.0.0.2.tar.gz/nextchat-1.0.0.2/nextchat/__init__.py
@@ -0,0 +1,11 @@
+"nextchat.py"
+
+__version__ = '1.0.0u1'
+__title__ = 'nextchat'
+__author__ = 'Bleekpie'
+
+from .bot import *
+from .context import *
+from .help import *
+from .message import *
+
