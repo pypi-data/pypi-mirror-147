7294e38f288383eec240bfd2f63afc2bb59fec21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:49:01 +0000

    Add package pytorch-widedeep/1.1.1/pytorch_widedeep-1.1.1-py3-none-any.whl

---
+++ packages/pytorch-widedeep/pytorch_widedeep-1.1.1-py3-none-any.whl/pytorch_widedeep/models/__init__.py
@@ -0,0 +1,20 @@
+from pytorch_widedeep.models.text import (
+    BasicRNN,
+    AttentiveRNN,
+    StackedAttentiveRNN,
+)
+from pytorch_widedeep.models.image import Vision
+from pytorch_widedeep.models.tabular import (
+    SAINT,
+    Wide,
+    TabMlp,
+    TabNet,
+    TabResnet,
+    TabPerceiver,
+    FTTransformer,
+    TabFastFormer,
+    TabTransformer,
+    SelfAttentionMLP,
+    ContextAttentionMLP,
+)
+from pytorch_widedeep.models.wide_deep import WideDeep
