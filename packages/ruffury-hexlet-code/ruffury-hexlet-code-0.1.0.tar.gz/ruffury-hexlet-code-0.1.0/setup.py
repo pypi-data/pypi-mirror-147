054ed4988fcd310dc1eda57f2fc7c98246b896d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:52 +0000

    Add package ruffury-hexlet-code/0.1.0/ruffury-hexlet-code-0.1.0.tar.gz

---
+++ packages/ruffury-hexlet-code/ruffury-hexlet-code-0.1.0.tar.gz/ruffury-hexlet-code-0.1.0/setup.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['brain_games', 'brain_games.games', 'brain_games.scripts']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['prompt>=0.4.1,<0.5.0']
+
+entry_points = \
+{'console_scripts': ['brain-calc = brain_games.games.brain_calc:main',
+                     'brain-even = brain_games.games.brain_even:main',
+                     'brain-games = brain_games.games.brain_games:main',
+                     'brain-gcd = brain_games.games.brain_gcd:main',
+                     'brain-prime = brain_games.games.brain_prime:main',
+                     'brain-progression = '
+                     'brain_games.games.brain_progression:main']}
+
+setup_kwargs = {
+    'name': 'ruffury-hexlet-code',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Your Name',
+    'author_email': 'you@example.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
