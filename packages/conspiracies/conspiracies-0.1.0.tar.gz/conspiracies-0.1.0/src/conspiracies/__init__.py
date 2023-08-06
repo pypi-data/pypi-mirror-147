50363e4bd89ebe88e72be43dc06f930904559a5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:04 +0000

    Add package conspiracies/0.1.0/conspiracies-0.1.0.tar.gz

---
+++ packages/conspiracies/conspiracies-0.1.0.tar.gz/conspiracies-0.1.0/src/conspiracies/__init__.py
@@ -0,0 +1,5 @@
+from .HeadWordExtractionComponent import (  # noqa
+    create_headwords_component,
+    HeadwordsExtractionComponent,
+)
+from .coref import create_coref_component, CoreferenceComponent  # noqa
