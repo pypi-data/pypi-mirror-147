f942258ea16c89e06b517342f3d765fd16b45ff4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:47 +0000

    Add package svcco/0.4.25/svcco-0.4.25.tar.gz

---
+++ packages/svcco/svcco-0.4.25.tar.gz/svcco-0.4.25/setup.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+"""
+This module provides a flexible package for autogenerating vascular networks.
+
+It provides:
+  - constrained constructive optimization routines for vasuclar construction
+  - implicit surface/volume handling for anatomic and engineered shapes
+  - integration with open-source vascular simulation software SimVascular
+  - gCode and Scl 3D printing file creation
+
+"""
+import re
+from pathlib import Path
+from setuptools import setup, find_packages
+
+here = Path(__file__).parent
+
+long_description = (here / "README.md").read_text("utf8")
+
+VERSION = re.search(
+    r'__version__ = "(.+?)"', (here / "svcco" / "__init__.py").read_text("utf8")
+).group(1)
+
+CLASSIFIERS = ['Intended Audience :: Science/Research',
+               'License :: OSI Approved :: MIT License',
+               'Programming Language :: Python',
+               'Programming Language :: Python :: 3.7',
+               'Programming Language :: Python :: 3.8',
+               'Programming Language :: Python :: 3.9',
+               'Topic :: Scientific/Engineering',
+               'Operating System :: Microsoft :: Windows',
+               'Operating System :: POSIX :: Linux',
+               'Operating System :: POSIX',
+               'Operating System :: Unix',
+               'Operating System :: MacOS']
+
+PACKAGES = find_packages(include=["svcco","svcco.*"]) #['svcco']+['svcco.'+ pkg for pkg in find_packages('svcco')]
+OPTIONS  = None
+INSTALL_REQUIREMENTS = ['numpy>=1.16.0',
+                        'numba>=0.53.0',
+                        'scipy>=1.5.0',
+                        'pyvista>=0.30.1',
+                        'matplotlib>=3.3.4',
+                        'seaborn>=0.11.0',
+                        'sympy>=1.8.0',
+                        'tqdm>=4.61.0',
+                        'vtk>=9.0.0',
+                        'scikit-image>=0.18.1',
+                        'pandas>=1.3.0',
+                        'plotly>=5.1.0',
+                        'pymeshfix>=0.15.0',
+                        'tetgen>=0.6.0']
+PROJECT_URLS = {}
+
+setup_info = dict(
+    name='svcco',
+    version=VERSION,
+    author='Zachary Sexton',
+    author_email='zsexton@stanford.edu',
+    url='https://github.com/zasexton/Tree',
+    description='Automated Vascular Generation and CFD Simulation',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT',
+    python_requires='>=3.7',
+    classifiers=CLASSIFIERS,
+    install_requires=INSTALL_REQUIREMENTS,
+    packages=PACKAGES
+    )
+
+setup(**setup_info)
