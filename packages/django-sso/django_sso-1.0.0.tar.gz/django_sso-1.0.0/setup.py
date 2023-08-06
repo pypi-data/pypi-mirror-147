7dc62deeaa0f08e3d860c890c2a0fb4caebd2b5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:51 +0000

    Add package django-sso/1.0.0/django_sso-1.0.0.tar.gz

---
+++ packages/django-sso/django_sso-1.0.0.tar.gz/django_sso-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django_sso',
+    version='1.0.0',
+    author="DAVIDhaker",
+    author_email='me@davidhaker.ru',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/davidhaker/django-sso',
+    keywords='Django SSO Single Sign-On',
+    install_requires=[
+        'django',
+    ],
+)
