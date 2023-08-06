70cc613ee172770fa429a616c7d208b8ed3d457a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:24 +0000

    Add package instrument-server/2.0.1/instrument-server-2.0.1.tar.gz

---
+++ packages/instrument-server/instrument-server-2.0.1.tar.gz/instrument-server-2.0.1/instrument_server/commands/quit_command.py
@@ -0,0 +1,23 @@
+from .basic_command import BasicCommand
+import sys
+
+
+class QuitCommand(BasicCommand):
+    """`quit`
+
+    Shuts down the server
+    """
+
+    # definition
+    COMMAND = 'quit'
+    ARGS    = {}
+
+    # implementation
+    def code(self, devices, args):
+        print('goodbye')
+        sys.exit(0)
+
+
+# export
+IS_COMMAND_PLUGIN = True
+plugin            = QuitCommand
