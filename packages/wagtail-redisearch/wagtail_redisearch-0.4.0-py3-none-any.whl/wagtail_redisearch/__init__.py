d2072d8ce76c982575c3240588ca5532351e2619
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:32 +0000

    Add package wagtail-redisearch/0.4.0/wagtail_redisearch-0.4.0-py3-none-any.whl

---
+++ packages/wagtail-redisearch/wagtail_redisearch-0.4.0-py3-none-any.whl/wagtail_redisearch/__init__.py
@@ -0,0 +1,5 @@
+from .backend import RediSearchBackend
+
+__version__ = "0.4.0"
+
+SearchBackend = RediSearchBackend
---
+++ packages/wagtail-redisearch/wagtail_redisearch-0.4.0-py3-none-any.whl/wagtail_redisearch/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WagtailRedisearchAppConfig(AppConfig):
+    name = "wagtail_redisearch"
