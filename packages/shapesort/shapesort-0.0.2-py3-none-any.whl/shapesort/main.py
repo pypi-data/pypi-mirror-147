976df78937290fd4f2da94ab9ef8614c35930289
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:03 +0000

    Add package shapesort/0.0.2/shapesort-0.0.2-py3-none-any.whl

---
+++ packages/shapesort/shapesort-0.0.2-py3-none-any.whl/shapesort/main.py
@@ -0,0 +1,127 @@
+import argparse
+import json
+import logging
+import sys
+from pathlib import Path
+from typing import Dict
+
+import nltk
+
+from shapesort import __version__, model
+
+__author__ = "Taylor Monacelli"
+__copyright__ = "Taylor Monacelli"
+__license__ = "MPL-2.0"
+
+_logger = logging.getLogger(__name__)
+
+
+def process_data_file(jsonfile: str):
+    path = Path(jsonfile).expanduser()
+    if not path.exists():
+        _logger.warning(f"can't find {path}")
+        return
+
+    dct: Dict = json.loads(path.read_text())
+    tbatch = model.TranscribeBatch(**dct)
+    assert len(tbatch) == 1
+
+    transcript = tbatch[0].transcript
+
+    nltk.download("punkt")
+    for token in nltk.tokenize.sent_tokenize(transcript):
+        print(token)
+        print()
+
+
+def parse_args(args):
+    """Parse command line parameters
+
+    Args:
+      args (List[str]): command line parameters as list of strings
+          (for example  ``["--help"]``).
+
+    Returns:
+      :obj:`argparse.Namespace`: command line parameters namespace
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        dest="json_path",
+        help="path to json file",
+        nargs="?",
+        type=str,
+        default="~/Downloads/asrOutput.json",
+    )
+    parser.add_argument(
+        "--version",
+        action="version",
+        version="shapesort {ver}".format(ver=__version__),
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        dest="loglevel",
+        help="set loglevel to INFO",
+        action="store_const",
+        const=logging.INFO,
+    )
+    parser.add_argument(
+        "-vv",
+        "--very-verbose",
+        dest="loglevel",
+        help="set loglevel to DEBUG",
+        action="store_const",
+        const=logging.DEBUG,
+    )
+    return parser.parse_args(args)
+
+
+def setup_logging(loglevel):
+    """Setup basic logging
+
+    Args:
+      loglevel (int): minimum loglevel for emitting messages
+    """
+    logformat = "[%(asctime)s] %(levelname)s:%(name)s:%(message)s"
+    logging.basicConfig(
+        level=loglevel, stream=sys.stdout, format=logformat, datefmt="%Y-%m-%d %H:%M:%S"
+    )
+
+
+def main(args):
+    """Wrapper allowing :func:`fib` to be called with string arguments in a CLI fashion
+
+    Instead of returning the value from :func:`fib`, it prints the result to the
+    ``stdout`` in a nicely formatted message.
+
+    Args:
+      args (List[str]): command line parameters as list of strings
+          (for example  ``["--verbose", "42"]``).
+    """
+    args = parse_args(args)
+    setup_logging(args.loglevel)
+    _logger.debug("Starting crazy calculations...")
+    process_data_file(args.json_path)
+    _logger.info("Script ends here")
+
+
+def run():
+    """Calls :func:`main` passing the CLI arguments extracted from :obj:`sys.argv`
+
+    This function can be used as entry point to create console scripts with setuptools.
+    """
+    main(sys.argv[1:])
+
+
+if __name__ == "__main__":
+    # ^  This is a guard statement that will prevent the following code from
+    #    being executed in the case someone imports this file instead of
+    #    executing it as a script.
+    #    https://docs.python.org/3/library/__main__.html
+
+    # After installing your project with pip, users can also run your Python
+    # modules as scripts via the ``-m`` flag, as defined in PEP 338::
+    #
+    #     python -m shapesort.skeleton 42
+    #
+    run()
