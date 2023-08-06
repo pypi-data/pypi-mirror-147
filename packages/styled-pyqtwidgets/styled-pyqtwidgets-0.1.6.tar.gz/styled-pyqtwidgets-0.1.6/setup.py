e43b1e740b8717d4ca6b5ffc32caa4df40c92493
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:27 +0000

    Add package styled-pyqtwidgets/0.1.6/styled-pyqtwidgets-0.1.6.tar.gz

---
+++ packages/styled-pyqtwidgets/styled-pyqtwidgets-0.1.6.tar.gz/styled-pyqtwidgets-0.1.6/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="styled-pyqtwidgets",
+    author="Dixon Leuenberger",
+    version="0.1.6",
+    description="create stylized table widget",
+    keywords="qTableWidget, pypi, package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3 :: Only",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=["pyqt5"],
+    extras_require={
+        "dev": ["check-manifest"],
+    },
+)
