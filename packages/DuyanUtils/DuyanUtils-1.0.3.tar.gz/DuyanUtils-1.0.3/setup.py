0282b338d7614ee3c110c72695753cd697884ffd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:09 +0000

    Add package DuyanUtils/1.0.3/DuyanUtils-1.0.3.tar.gz

---
+++ packages/DuyanUtils/DuyanUtils-1.0.3.tar.gz/DuyanUtils-1.0.3/setup.py
@@ -0,0 +1,34 @@
+# from setuptools import setup, find_packages
+#
+# setup(
+#     name="DuyanUtils",
+#     version="1.1",
+#     packages=find_packages("src"),
+#     package_dir={"": "src"}
+# )
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="DuyanUtils",
+    version="1.0.3",
+    author="Jianguo.Wang",
+    author_email="chunyang.wang@duyansoft.com",
+    description="常用工具 轻量化处理",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    # url="https://github.com/pypa/sampleproject",
+    # project_urls={
+    #     "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    # },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
