3f0224fceb321dce303e2410f867e92e3ca2f7c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:21 +0000

    Add package alacomba-multiply/0.1.0/alacomba-multiply-0.1.0.tar.gz

---
+++ packages/alacomba-multiply/alacomba-multiply-0.1.0.tar.gz/alacomba-multiply-0.1.0/README.md
@@ -0,0 +1,20 @@
+# Medium multiply
+A small demo library for a Medium publication about publishing libraries.
+
+### Installation
+```
+pip install alacomba-multiply
+```
+
+### Get started
+How to multiply one number by another with this lib:
+
+```Python
+from alacomba_multiply import Multiplication
+
+# Instantiate a Multiplication object
+multiplication = Multiplication(2)
+
+# Call the multiply method
+result = multiplication.multiply(5)
+```
---
+++ packages/alacomba-multiply/alacomba-multiply-0.1.0.tar.gz/alacomba-multiply-0.1.0/alacomba_multiply/multiplication.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 19 21:44:31 2022
+
+@author: Hernani
+"""
+import numpy as np
+
+class Multiplication:
+    """
+    Instantiate a multiplication operation.
+    Numbers will be multiplied by the given multiplier.
+    
+    :param multiplier: The multiplier.
+    :type multiplier: int
+    """
+    
+    
+    def __init__(self, multiplier):
+        self.multiplier = multiplier
+    
+    def multiply(self, number):
+        """
+        Multiply a given number by the multiplier.
+        
+        :param number: The number to multiply.
+        :type number: int
+    
+        :return: The result of the multiplication.
+        :rtype: int
+        """
+        
+        return np.dot(number,self.multiplier)
+
+# # Instantiate a Multiplication object
+# multiplication = Multiplication(2)
+
+# # Call the multiply method
+# print(multiplication.multiply(5))
