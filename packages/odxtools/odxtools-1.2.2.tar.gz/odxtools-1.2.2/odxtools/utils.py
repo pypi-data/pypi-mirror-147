c334cbc664f5c864c2da02e369f36b8d26df262e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:50 +0000

    Add package odxtools/1.2.2/odxtools-1.2.2.tar.gz

---
+++ packages/odxtools/odxtools-1.2.2.tar.gz/odxtools-1.2.2/odxtools/utils.py
@@ -0,0 +1,56 @@
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2022 MBition GmbH
+
+from typing import Dict, Literal
+from xml.etree import ElementTree
+
+
+def read_description_from_odx(et_element: ElementTree.Element):
+    """Read a DESCRIPTION element. The element usually has the name DESC."""
+    # TODO: Invent a better representation of a DESC element.
+    #       This just represents it as XHTML string. 
+    if et_element is None:
+        return None
+
+    raw_string = ElementTree.tostring(et_element,
+                                      encoding="unicode",
+                                      method="xml").strip()
+    # Remove DESC start and end tag.
+    assert raw_string.startswith("<DESC>"), raw_string
+    assert raw_string.endswith("</DESC>"), raw_string
+    # Remove starting and ending tag
+    raw_string = raw_string[len("<DESC>"):-len("</DESC>")]
+    return raw_string.strip()
+
+
+def read_element_id(et_element) -> Dict[Literal["short_name", "long_name", "description"], str]:
+    """Read the elements "SHORT-NAME", "LONG-NAME" and "DESCRIPTION".
+
+    Returns the dict
+    ```python
+    {
+        "short_name": et_element.find("SHORT-NAME"),
+        "long_name": et_element.find("LONG-NAME"),
+        "description": read_description_from_odx(et_element.find("DESC"))
+    }
+    ```
+    If `et_element` does not contain the elements "LONG-NAME" and "DESC",
+    the returned dict does not contain the correspondig keys.
+
+    A typical use for this function is reading an odx element
+    that contains the group "ELEMENT-ID", e.g.,
+    ```python
+    def read_type_with_element_id_from_odx(et_element):
+        element_id = read_element_id(et_element)
+        return TypeWithElementID(**element_id)
+    ```
+
+    """
+    d: Dict[Literal["short_name", "long_name", "description"], str] = {
+        "short_name": et_element.findtext("SHORT-NAME")
+    }
+    if et_element.find("LONG-NAME") is not None:
+        d["long_name"] = et_element.findtext("LONG-NAME")
+    if et_element.find("DESC") is not None:
+        d["description"] = read_description_from_odx(et_element.find("DESC"))
+    return d
