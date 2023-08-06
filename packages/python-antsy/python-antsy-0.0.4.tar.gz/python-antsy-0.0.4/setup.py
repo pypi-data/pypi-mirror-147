d44dbe63067510e4c92cf87897445ffe283667f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:25 +0000

    Add package python-antsy/0.0.4/python-antsy-0.0.4.tar.gz

---
+++ packages/python-antsy/python-antsy-0.0.4.tar.gz/python-antsy-0.0.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='python-antsy',
+    version='0.0.4',
+    description="Python package for integrating Antsy in other applications",
+    license='MIT',
+    author="Juan F. Duque",
+    author_email='jfelipe@grupodyd.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/grupodyd/python-antsy',
+    keywords='antsy',
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*",
+    install_requires=[
+          'requests',
+      ],
+
+)
