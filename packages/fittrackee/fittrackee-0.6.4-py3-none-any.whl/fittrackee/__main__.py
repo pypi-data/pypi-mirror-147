536303fc0bf15263dbd21834d346df566d748005
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:47 +0000

    Add package fittrackee/0.6.4/fittrackee-0.6.4-py3-none-any.whl

---
+++ packages/fittrackee/fittrackee-0.6.4-py3-none-any.whl/fittrackee/__main__.py
@@ -0,0 +1,51 @@
+# source for StandaloneApplication class:
+# http://docs.gunicorn.org/en/stable/custom.html
+import os
+from typing import Dict, Optional
+
+import gunicorn.app.base
+from flask import Flask
+from flask_migrate import upgrade
+
+from fittrackee import create_app
+
+HOST = os.getenv('HOST', '0.0.0.0')
+PORT = os.getenv('PORT', '5000')
+WORKERS = os.getenv('APP_WORKERS', 1)
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+app = create_app()
+
+
+class StandaloneApplication(gunicorn.app.base.BaseApplication):
+    def __init__(
+        self, current_app: Flask, options: Optional[Dict] = None
+    ) -> None:
+        self.options = options or {}
+        self.application = current_app
+        super().__init__()
+
+    def load_config(self) -> None:
+        config = {
+            key: value
+            for key, value in self.options.items()
+            if key in self.cfg.settings and value is not None
+        }
+        for key, value in config.items():
+            self.cfg.set(key.lower(), value)
+
+    def load(self) -> Flask:
+        return self.application
+
+
+def upgrade_db() -> None:
+    with app.app_context():
+        upgrade(directory=BASEDIR + '/migrations')
+
+
+def main() -> None:
+    options = {'bind': f'{HOST}:{PORT}', 'workers': WORKERS}
+    StandaloneApplication(app, options).run()
+
+
+if __name__ == '__main__':
+    app.run()
