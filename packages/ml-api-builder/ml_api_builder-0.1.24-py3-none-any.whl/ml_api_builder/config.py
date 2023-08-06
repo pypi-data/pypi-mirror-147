88f2b490e165460d6de5337b25333882fc25575d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:04 +0000

    Add package ml-api-builder/0.1.24/ml_api_builder-0.1.24-py3-none-any.whl

---
+++ packages/ml-api-builder/ml_api_builder-0.1.24-py3-none-any.whl/ml_api_builder/config.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+
+@dataclass
+class ml_config:
+    make_request_url: str
+
+@dataclass
+class jenkins_config:
+    jenkins_job_name: str        
+    Jenkins_url: str
+    jenkins_user: str
+    jenkins_pwd: str
+    buildWithParameters: bool
+    jenkins_params: dict
+
+@dataclass
+class api_config:
+    jenkins_job_name: str        
+    Jenkins_url: str
+    jenkins_user: str
+    jenkins_pwd: str
+    buildWithParameters: bool
+    jenkins_params: dict
+    aws_stack_name: str
+    model_name: str = 'default'
+    model_run_id: str = 'default'
