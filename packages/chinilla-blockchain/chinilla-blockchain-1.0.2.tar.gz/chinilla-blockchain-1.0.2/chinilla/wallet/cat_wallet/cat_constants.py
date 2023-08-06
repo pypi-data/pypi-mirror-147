0da6c1343121266c699710ea7cdca86026d800b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:25:40 +0000

    Add package chinilla-blockchain/1.0.2/chinilla-blockchain-1.0.2.tar.gz

---
+++ packages/chinilla-blockchain/chinilla-blockchain-1.0.2.tar.gz/chinilla-blockchain-1.0.2/chinilla/wallet/cat_wallet/cat_constants.py
@@ -0,0 +1,9 @@
+CHINILLA_HOLIDAY_TOKEN = {
+    "asset_id": "509deafe3cd8bbfbb9ccce1d930e3d7b57b40c964fa33379b18d628175eb7a8f",
+    "name": "Chinilla Holiday 2021 Token",
+    "symbol": "CH21",
+}
+
+DEFAULT_CATS = {
+    CHINILLA_HOLIDAY_TOKEN["asset_id"]: CHINILLA_HOLIDAY_TOKEN,
+}
