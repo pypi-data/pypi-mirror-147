bc99437a9956f0f5b99f27cc63c93675fff30c7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:27 +0000

    Add package mgtwr/1.0.5/mgtwr-1.0.5.tar.gz

---
+++ packages/mgtwr/mgtwr-1.0.5.tar.gz/mgtwr-1.0.5/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="mgtwr",
+    version="1.0.5",
+    author="Kun Sun",
+    author_email="849024477@qq.com",
+    packages=['mgtwr']
+    )
+          
+        
+            
