88265c9f9a8af409f92396420cd98dccf9fde7d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:47:54 +0000

    Add package socceraction/1.2.3/socceraction-1.2.3.tar.gz

---
+++ packages/socceraction/socceraction-1.2.3.tar.gz/socceraction-1.2.3/socceraction/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+SoccerAction
+~~~~~~~~~~~~
+
+SoccerAction is a Python package for objectively quantifying the impact of the
+individual actions performed by soccer players using event stream data.
+
+Full documentation is at <https://ml-kuleuven.github.io/socceraction/>.
+:copyright: (c) 2020 by DTAI KU Leuven.
+:license: MIT, see LICENSE for more details.
+"""
+__version__ = '1.2.3'
