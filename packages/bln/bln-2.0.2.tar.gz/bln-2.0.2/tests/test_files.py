c3d0dfb040a5c97b9651307a1290e38ab120ab4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:29 +0000

    Add package bln/2.0.2/bln-2.0.2.tar.gz

---
+++ packages/bln/bln-2.0.2.tar.gz/bln-2.0.2/tests/test_files.py
@@ -0,0 +1,15 @@
+import os
+
+from bln import Client
+
+
+def test_files():
+    """Test file methods."""
+    c = Client(tier=os.getenv("BLN_TEST_ENV", "dev"))
+    p = c.get_project_by_name("WARN Act Notices")
+    args = [p["id"], "test.csv"]
+    c.upload_file(*args)
+    c.download_file(*args)
+    c.deleteFile(*args)
+    c.upload_files(args[0], [args[1]])
+    c.deleteFile(*args)
