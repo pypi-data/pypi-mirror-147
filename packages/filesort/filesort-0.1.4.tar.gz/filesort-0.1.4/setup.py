61b60130a74517aaaf5c8d3fff42453023ace5b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:39 +0000

    Add package filesort/0.1.4/filesort-0.1.4.tar.gz

---
+++ packages/filesort/filesort-0.1.4.tar.gz/filesort-0.1.4/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="filesort",
+    version="0.1.4",
+    author="Vivian Hafener",
+    author_email="vhafener@outlook.com",
+    description="A package that aids sorting of files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/viv-codes/filesort",
+    # include_package_data=True,
+    project_urls={
+        "Bug Tracker": "https://github.com/viv-codes/filesort/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    # package_dir={"": "src"},
+    py_modules=["main"],
+    packages=find_packages(where="main.py"),
+    install_requires=[
+        "Click",
+        "prompt-toolkit",
+        "yaspin",
+    ],
+    entry_points={
+        "console_scripts": [
+            "filesort=main:cli",
+        ],
+    },
+    # packages=find_packages(include=['prompt-toolkit', 'Click'])
+    # install_requires=['prompt-toolkit', 'Click']
+    python_requires=">=3.6",
+)
