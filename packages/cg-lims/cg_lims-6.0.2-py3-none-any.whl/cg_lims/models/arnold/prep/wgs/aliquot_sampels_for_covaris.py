36b2c75e79e2458d6ee4fe98de7859845569aa61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:56 +0000

    Add package cg-lims/6.0.2/cg_lims-6.0.2-py3-none-any.whl

---
+++ packages/cg-lims/cg_lims-6.0.2-py3-none-any.whl/cg_lims/models/arnold/prep/wgs/aliquot_sampels_for_covaris.py
@@ -0,0 +1,35 @@
+from typing import Optional
+
+from genologics.lims import Lims
+from pydantic.main import BaseModel
+from pydantic import Field
+from cg_lims.objects import BaseAnalyte
+from cg_lims.models.arnold.base_step import BaseStep
+
+
+class ArtifactUDF(BaseModel):
+    sample_amount_needed: Optional[float] = Field(None, alias="Amount needed (ng)")
+
+
+class ArnoldStep(BaseStep):
+    artifact_udfs: ArtifactUDF
+
+    class Config:
+        allow_population_by_field_name = True
+
+
+def get_aliquot_samples_for_covaris(lims: Lims, sample_id: str, prep_id: str) -> ArnoldStep:
+    analyte = BaseAnalyte(
+        lims=lims,
+        sample_id=sample_id,
+        process_type="Aliquot Samples for Covaris",
+    )
+
+    return ArnoldStep(
+        **analyte.base_fields(),
+        artifact_udfs=ArtifactUDF(**analyte.artifact_udfs()),
+        sample_id=sample_id,
+        prep_id=prep_id,
+        step_type="aliquot_samples_for_covaris",
+        workflow="WGS",
+    )
