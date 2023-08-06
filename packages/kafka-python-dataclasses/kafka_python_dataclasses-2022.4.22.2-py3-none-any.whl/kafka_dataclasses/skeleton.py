d87400e298a44e3941dae4c9f1cf8cf1f6cb0dd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:07 +0000

    Add package kafka-python-dataclasses/2022.4.22.2/kafka_python_dataclasses-2022.4.22.2-py3-none-any.whl

---
+++ packages/kafka-python-dataclasses/kafka_python_dataclasses-2022.4.22.2-py3-none-any.whl/kafka_dataclasses/skeleton.py
@@ -0,0 +1,33 @@
+"""
+This is a skeleton file that can serve as a starting point for a Python
+console script. To run this script uncomment the following lines in the
+``[options.entry_points]`` section in ``setup.cfg``::
+
+    console_scripts =
+         fibonacci = kafka_dataclasses.skeleton:run
+
+Then run ``pip install .`` (or ``pip install -e .`` for editable mode)
+which will install the command ``fibonacci`` inside your current environment.
+
+Besides console scripts, the header (i.e. until ``_logger``...) of this file can
+also be used as template for Python modules.
+
+Note:
+    This skeleton file can be safely removed if not needed!
+
+References:
+    - https://setuptools.pypa.io/en/latest/userguide/entry_point.html
+    - https://pip.pypa.io/en/stable/reference/pip_install
+"""
+
+import argparse
+import logging
+import sys
+
+from kafka_dataclasses import __version__
+
+__author__ = "Collin Sage"
+__copyright__ = "Collin Sage"
+__license__ = "MIT"
+
+_logger = logging.getLogger(__name__)
