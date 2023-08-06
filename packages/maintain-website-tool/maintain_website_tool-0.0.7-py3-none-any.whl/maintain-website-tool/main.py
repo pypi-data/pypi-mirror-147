92d94a686084ac7279c6691307fdaffa15428d27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:30 +0000

    Add package maintain-website-tool/0.0.7/maintain_website_tool-0.0.7-py3-none-any.whl

---
+++ packages/maintain-website-tool/maintain_website_tool-0.0.7-py3-none-any.whl/maintain-website-tool/main.py
@@ -0,0 +1,15 @@
+#!/bin/env python3
+from link import link
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(prog="maintain-website-tool")
+    parser.set_defaults(func=lambda args: parser.print_usage())
+
+    subparsers = parser.add_subparsers()
+
+    # Init subcommands
+    link(subparsers)
+
+    args = parser.parse_args()
+    args.func(args)
