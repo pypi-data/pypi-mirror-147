5bd4554bd5007748508f539497b25d395808aae1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:49 +0000

    Add package openlineage-airflow/0.7.1/openlineage_airflow-0.7.1-py3-none-any.whl

---
+++ packages/openlineage-airflow/openlineage_airflow-0.7.1-py3-none-any.whl/openlineage/airflow/extractors/__init__.py
@@ -0,0 +1,6 @@
+# SPDX-License-Identifier: Apache-2.0.
+
+from openlineage.airflow.extractors.extractors import Extractors
+from openlineage.airflow.extractors.base import BaseExtractor, TaskMetadata
+
+__all__ = [Extractors, BaseExtractor, TaskMetadata]
