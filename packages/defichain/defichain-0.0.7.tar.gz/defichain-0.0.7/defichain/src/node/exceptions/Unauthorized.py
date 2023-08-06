e4751ee2afe3bd407458ecf21b1a6a260440af46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:30 +0000

    Add package defichain/0.0.7/defichain-0.0.7.tar.gz

---
+++ packages/defichain/defichain-0.0.7.tar.gz/defichain-0.0.7/defichain/src/node/exceptions/Unauthorized.py
@@ -0,0 +1,6 @@
+from .RPCErrorCode import RPCErrorCode
+
+
+class Unauthorized(Exception):
+    def __init__(self):
+        super().__init__(f"Unauthorized(401): Authorization failed: Incorrect rpcuser or rpcpassword")
---
+++ packages/defichain/defichain-0.0.7.tar.gz/defichain-0.0.7/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+from os import path
+
+VERSION = '0.0.7'
+DESCRIPTION = 'Defichain Python Library'
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    LONG_DESCRIPTION = f.read()
+
+# Setting up
+setup(
+    name="defichain",
+    version=VERSION,
+    author="Intr0c",
+    author_email="introc@volz.link",
+    url="https://github.com/eric-volz/DefichainPython",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    packages=['defichain', 'defichain.src', 'defichain.src.node',
+              'defichain.src.node.modules', 'defichain.src.node.exceptions',
+              'defichain.src.ocean', 'defichain.src.ocean.modules'],
+    install_requires=["requests"],
+    keywords=['python', 'defichain', 'node', 'ocean'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
