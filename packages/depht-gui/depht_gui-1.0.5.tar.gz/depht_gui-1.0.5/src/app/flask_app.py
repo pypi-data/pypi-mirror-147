52c5a57969a6f51ee4cbde47eecfaa544a1fe32c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:14 +0000

    Add package depht-gui/1.0.5/depht_gui-1.0.5.tar.gz

---
+++ packages/depht-gui/depht_gui-1.0.5.tar.gz/depht_gui-1.0.5/src/app/flask_app.py
@@ -0,0 +1,42 @@
+"""Flask app for DEPhT."""
+from pathlib import Path
+from flask import Flask
+
+from importlib.resources import files
+
+# import app
+from app.views import depht, train, models, documentation
+from app import config
+from app import templates, static
+# from app import static, templates
+
+PKG = "depht_gui"
+
+# templates_module = import_module("templates")
+# static_module = import_module("static")
+
+
+def create_app(name=PKG, **kwargs):
+    """
+    Create the Flask app.
+
+    :param name: name of the app
+    :type name: str
+    """
+    app = Flask(name, template_folder=files(templates),
+                static_folder=files(static))
+
+    # app = Flask(name)
+    app.config.from_object(config)
+
+    path = Path(app.instance_path)
+
+    if not path.is_dir():
+        path.mkdir(parents=True)
+
+    app.register_blueprint(depht.bp)
+    app.register_blueprint(train.bp)
+    app.register_blueprint(models.bp)
+    app.register_blueprint(documentation.bp)
+
+    return app
