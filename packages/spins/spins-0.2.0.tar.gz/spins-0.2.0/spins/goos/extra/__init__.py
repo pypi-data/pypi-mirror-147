a4a78237e8826bfb13713932deefa3586f92bef5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:22 +0000

    Add package spins/0.2.0/spins-0.2.0.tar.gz

---
+++ packages/spins/spins-0.2.0.tar.gz/spins-0.2.0/spins/goos/extra/__init__.py
@@ -0,0 +1,15 @@
+from spins.goos.optplan import (
+    GLOBAL_CONTEXT_STACK,
+    GLOBAL_PLAN_STACK,
+    ModelNameType,
+    PROBLEM_GRAPH_NAME_MAP,
+    polymorphic_model,
+)
+
+__all__ = [
+    "GLOBAL_CONTEXT_STACK",
+    "GLOBAL_PLAN_STACK",
+    "ModelNameType",
+    "PROBLEM_GRAPH_NAME_MAP",
+    "polymorphic_model",
+]
