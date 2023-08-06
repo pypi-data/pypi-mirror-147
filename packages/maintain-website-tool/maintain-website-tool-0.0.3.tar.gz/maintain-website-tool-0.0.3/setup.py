583c1d2964c9f705aeaa83a87cd45721b85fb6df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:10 +0000

    Add package maintain-website-tool/0.0.3/maintain-website-tool-0.0.3.tar.gz

---
+++ packages/maintain-website-tool/maintain-website-tool-0.0.3.tar.gz/maintain-website-tool-0.0.3/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="maintain-website-tool",
+    version="0.0.3",
+    author="CSDUMMI",
+    author_email="csdummi.misquality@simplelogin.co",
+    description="Maintain a Website. Includes link checker.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://codeberg.org/developers/maintain-website-tool",
+    project_urls={
+        "Bug Tracker": "https://codeberg.org/developers/maintain-website-tool/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+    ],
+    package_dir={"":"src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": [
+            "maintain-website-tool=maintain_website_tool.main:main",
+        ]
+    }
+)
