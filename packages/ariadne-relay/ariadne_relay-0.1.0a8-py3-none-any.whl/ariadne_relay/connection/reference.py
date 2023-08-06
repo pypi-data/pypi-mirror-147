e2f691320041652aaa3a7b626f32712cf52b154d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:15:30 +0000

    Add package ariadne-relay/0.1.0a8/ariadne_relay-0.1.0a8-py3-none-any.whl

---
+++ packages/ariadne-relay/ariadne_relay-0.1.0a8-py3-none-any.whl/ariadne_relay/connection/reference.py
@@ -0,0 +1,16 @@
+from typing import Optional
+
+from graphql_relay import ConnectionType, SizedSliceable
+
+from .base import BaseConnection, ConnectionArguments
+
+
+class ReferenceConnection(BaseConnection[ConnectionType]):
+    def __call__(
+        self,
+        data: SizedSliceable,
+        connection_args: ConnectionArguments,
+        *,
+        data_length: Optional[int] = None,
+    ) -> ConnectionType:
+        return self.create_connection(data, connection_args, data_length=data_length)
