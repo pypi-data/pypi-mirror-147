b910fb4aa7dd23bb3ef999bb3413a23f6d54e8ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:55 +0000

    Add package pyVinted/0.4.6.2/pyVinted-0.4.6.2-py3-none-any.whl

---
+++ packages/pyVinted/pyVinted-0.4.6.2-py3-none-any.whl/pyVinted/vinted.py
@@ -0,0 +1,36 @@
+from urllib.parse import urlparse, parse_qsl
+import requests
+from .items.item import Item
+from pyVinted.items import Items
+from pyVinted.requester import requester
+
+
+class Vinted:
+    """
+    This class is built to connect with the pyVinted API.
+
+    It's main goal is to be able to retrieve items from a given url search.\n
+    The items are then be returned in a json format
+    """
+
+    def __init__(self, domain="fr", proxy=None, gateway=None):
+        """
+        Args:
+            domain (str): Domain to be used, example: "fr" for France, "de" for Germany...
+
+        """
+        requester.setCookies(domain)
+
+
+        if proxy is not None:
+            requester.session.proxies.update(proxy)
+
+        if gateway is not None:
+            requester.session.mount("https://www.vinted.fr", gateway)
+
+
+        self.items = Items()
+
+    # def login(self,username,password):
+    #     requester.login(username=username, password=password)
+    #     requester.message()
