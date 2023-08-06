53dee2c7aa7949f6c53cc28b01bf057825fd9b31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:21 +0000

    Add package NiceEncryption/0.1/NiceEncryption-0.1.tar.gz

---
+++ packages/NiceEncryption/NiceEncryption-0.1.tar.gz/NiceEncryption-0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'NiceEncryption',
+    packages = ['NiceEncryption'],
+    version = '0.1',  # Ideally should be same as your GitHub release tag varsion
+    description = 'A Python package that encrypts and decrypts strings.',
+    author = 'SpiderDerp',
+    author_email = 'sfsreeramrave@gmail.com',
+    url = 'https://github.com/SpiderDerp/NiceEncryption',
+    download_url = 'https://github.com/SpiderDerp/NiceEncryption/archive/refs/tags/v0.1.tar.gz',
+    keywords = ['python', 'encryption'],
+    classifiers = [],
+)
