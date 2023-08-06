7716bd528dc909842254c840fc656dc972b91e41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:48:23 +0000

    Add package bestFit/0.3/bestFit-0.3.tar.gz

---
+++ packages/bestFit/bestFit-0.3.tar.gz/bestFit-0.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+setup(
+  name = "bestFit",
+  version='0.3',
+  license ="MIT",
+  author = "SebassNoob",
+  description="A package for plotting best fit lines.",
+  long_description=open("README.md","r").read(),
+  long_description_content_type="text/markdown",
+  author_email="sebastian.ong@hotmail.com",
+  packages=find_packages('src'),
+  install_requires=[
+    'numpy',
+    'matplotlib'
+  ],
+  
+  
+)
