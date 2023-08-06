45dae6e6f795d010ef1bea679c81e9114e535889
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:40 +0000

    Add package ortools-puzzle-solvers/0.0.5/ortools-puzzle-solvers-0.0.5.tar.gz

---
+++ packages/ortools-puzzle-solvers/ortools-puzzle-solvers-0.0.5.tar.gz/ortools-puzzle-solvers-0.0.5/solvers/killer_sudoku/cell.py
@@ -0,0 +1,11 @@
+class Cell:
+    def __init__(self, row: int, column: int, value: int = None) -> None:
+        self.row = row
+        self.column = column
+        self.value = value
+
+    def set_value(self, new_value: int) -> None:
+        self.value = new_value
+
+    def get_value(self) -> int:
+        return self.value
