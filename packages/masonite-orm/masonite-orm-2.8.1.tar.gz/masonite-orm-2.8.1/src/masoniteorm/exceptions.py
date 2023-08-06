8fb8db1442c06311772170c16612ee87c9819502
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:23 +0000

    Add package masonite-orm/2.8.1/masonite-orm-2.8.1.tar.gz

---
+++ packages/masonite-orm/masonite-orm-2.8.1.tar.gz/masonite-orm-2.8.1/src/masoniteorm/exceptions.py
@@ -0,0 +1,30 @@
+class DriverNotFound(Exception):
+    pass
+
+
+class ModelNotFound(Exception):
+    pass
+
+
+class HTTP404(Exception):
+    pass
+
+
+class ConnectionNotRegistered(Exception):
+    pass
+
+
+class QueryException(Exception):
+    pass
+
+
+class MigrationNotFound(Exception):
+    pass
+
+
+class ConfigurationNotFound(Exception):
+    pass
+
+
+class InvalidUrlConfiguration(Exception):
+    pass
