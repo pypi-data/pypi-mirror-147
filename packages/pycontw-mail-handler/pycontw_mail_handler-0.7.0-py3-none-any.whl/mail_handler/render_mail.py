ec3826ea3b303725026036e645c112fc81a554ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:48 +0000

    Add package pycontw-mail-handler/0.7.0/pycontw_mail_handler-0.7.0-py3-none-any.whl

---
+++ packages/pycontw-mail-handler/pycontw_mail_handler-0.7.0-py3-none-any.whl/mail_handler/render_mail.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+import csv
+import json
+import logging
+import os
+from collections import defaultdict
+from html.parser import HTMLParser
+from pathlib import Path
+from typing import DefaultDict, Dict, List, Optional, Sequence, Tuple
+
+import click
+from jinja2 import Template
+
+
+class HTMLCounter(HTMLParser):
+    def __init__(self) -> None:
+        super().__init__()
+        self.count = 0
+
+    def handle_starttag(self, tag: str, attrs: List[Tuple[str, Optional[str]]]) -> None:
+        if tag == "html":
+            self.count += 1
+
+
+def load_template(tmpl_path: str) -> Template:
+    with open(tmpl_path, "r", encoding="utf-8") as input_tmpl:
+        return Template(input_tmpl.read())
+
+
+def render_all_content(
+    template: Template,
+    common_data: Dict[str, str],
+    unique_data: Sequence[Dict[str, str]],
+    separator: str,
+) -> Dict[str, str]:
+    addr_to_content: Dict[str, str] = dict()
+    mail_defdict: DefaultDict[str, int] = defaultdict(int)
+    for data in unique_data:
+        data.update(common_data)
+        if separator:
+            subject = separator.join([data["receiver_email"], data["receiver_name"]])
+        else:
+            subject = data["receiver_email"]
+        mail_defdict[subject] += 1
+        # multi-mail
+        if mail_defdict[subject] > 1:
+            subject = "{}__{:03n}".format(subject, mail_defdict[subject])
+
+        addr_to_content[subject] = template.render(**data)
+    return addr_to_content
+
+
+def export_mails(recv_to_mail: Dict[str, str], output_path: str) -> None:
+    for receiver_mail, mail_content in recv_to_mail.items():
+        parser = HTMLCounter()
+        parser.feed(mail_content)
+        if parser.count > 0:
+            with open(
+                output_path / Path(receiver_mail + ".html"), "w", encoding="utf-8"
+            ) as output_html:
+                output_html.write(mail_content)
+        else:
+            with open(
+                output_path / Path(receiver_mail + ".txt"), "w", encoding="utf-8"
+            ) as output_file:
+                output_file.write(mail_content)
+
+
+@click.command()
+@click.argument("template_path", type=click.Path(exists=True))
+@click.argument("receiver_data", type=click.Path(exists=True))
+@click.option(
+    "--separator",
+    default="",
+    show_default=False,
+    help="Separator used for subject suffix. It is disabled with empty string by default.",
+)
+@click.option(
+    "--output_path",
+    type=click.Path(exists=False),
+    default="mails_to_sent",
+    show_default=True,
+    help="Output path of rendered mails",
+)
+@click.option(
+    "--unique_csv",
+    type=click.Path(exists=True),
+    help="Use CSV file to import unique data",
+)
+def main(
+    template_path: str,
+    receiver_data: str,
+    separator: str,
+    output_path: str,
+    unique_csv: str,
+) -> None:
+    """
+    Application entry point
+    """
+    if not os.path.isdir(output_path):
+        logging.info('Create directory "%s"', output_path)
+        Path(output_path).mkdir(parents=True)
+
+    if unique_csv:
+        with open(receiver_data, "r", encoding="utf-8") as input_file:
+            data = json.load(input_file)
+            common_data = data["common_data"]
+
+        with open(unique_csv, "r", encoding="utf-8-sig") as input_file:
+            unique_data = [row for row in csv.DictReader(input_file)]
+    else:
+        with open(receiver_data, "r", encoding="utf-8") as input_file:
+            data = json.load(input_file)
+            common_data = data["common_data"]
+            unique_data = data["unique_data"]
+
+    template = load_template(template_path)
+    recv_to_mail = render_all_content(template, common_data, unique_data, separator)
+    export_mails(recv_to_mail, output_path)
+
+
+# pylint: disable=no-value-for-parameter
+if __name__ == "__main__":
+    main()
+# pylint: enable=no-value-for-parameter
---
+++ packages/pycontw-mail-handler/pycontw_mail_handler-0.7.0-py3-none-any.whl/mail_handler/send_mail.py
@@ -0,0 +1,197 @@
+#!/usr/bin/env python
+import json
+import logging
+import os
+import pickle
+import smtplib
+from collections import defaultdict
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.utils import formataddr
+from pathlib import Path
+from typing import DefaultDict, Dict, List, Optional
+
+import click
+from typing_extensions import TypedDict
+
+
+class MailConfig(TypedDict):
+    host: str
+    port: int
+
+
+MAIL_SERVER_CONFIG: Dict[str, MailConfig] = {
+    "gmail": {"host": "smtp.gmail.com", "port": 465}
+}
+
+
+logging.basicConfig(level=logging.INFO)
+
+
+def load_mails(input_dir: str) -> DefaultDict[str, List[str]]:
+    addr_to_content = defaultdict(list)
+    for filename in os.listdir(input_dir):
+        with open(f"{input_dir}/{filename}", "r", encoding="utf-8") as input_file:
+            if "@" not in filename:
+                continue
+
+            # remove "__" at multiple mails with same address
+            filenames = filename.split("__")
+            if len(filenames) > 1:
+                filename = "".join(filenames[:-1])
+            else:
+                filename = filenames[0]
+
+            addr_to_content[filename].append(input_file.read())
+    return addr_to_content
+
+
+def build_mail(
+    receiver_addr: str,
+    mail_content: str,
+    config: Dict[str, str],
+    separator: str,
+    attachment_file: Optional[str] = None,
+    suffix: str = None,
+    html: bool = False,
+) -> MIMEMultipart:
+    mail = MIMEMultipart()
+    if html:
+        mail.attach(MIMEText(mail_content, "html", "utf-8"))
+    else:
+        mail.attach(MIMEText(mail_content))
+
+    if suffix:
+        mail["Subject"] = "".join([config.get("Subject", ""), separator, suffix])
+    else:
+        mail["Subject"] = config.get("Subject", "")
+    mail["From"] = formataddr((config.get("SenderName", ""), config.get("From", "")))
+    mail["To"] = receiver_addr
+    mail["CC"] = config.get("CC", "")
+
+    if attachment_file:
+        with open(attachment_file, "rb") as f:
+            attach = MIMEApplication(f.read())
+        attach.add_header(
+            "Content-Disposition",
+            "attachment",
+            filename=str(os.path.basename(attachment_file)),
+        )
+        attach.add_header("Content-ID", f"<{str(os.path.basename(attachment_file))}>")
+        mail.attach(attach)
+
+    return mail
+
+
+def send_mail(
+    mail: MIMEMultipart,
+    user: str,
+    password: str,
+    server_config: MailConfig = None,
+) -> None:
+    if not server_config:
+        server_config = MAIL_SERVER_CONFIG["gmail"]
+
+    server = smtplib.SMTP_SSL(server_config["host"], int(server_config["port"]))
+    server.ehlo()
+    server.login(user, password)
+    server.send_message(mail)
+    logging.info("Email sent to %s!", mail["To"])
+    server.quit()
+
+
+def dump_mail(
+    mail: MIMEMultipart,
+    suffix: Optional[str],
+    debug_dump_path: str = "/tmp/mail_handler",
+) -> None:
+    if suffix:
+        dump_path = "/".join((debug_dump_path, "with-separator"))
+    else:
+        dump_path = "/".join((debug_dump_path, "no-separator"))
+    Path(dump_path).mkdir(parents=True, exist_ok=True)
+    with open(f"{dump_path}/{mail['To']}", "wb") as dumpf:
+        pickle.dump(mail, dumpf)
+    print(f"Debug mode is on. Dump mails to {dump_path} instead of sending them.")
+
+
+@click.command()
+@click.argument("config_path", type=click.Path(exists=True))
+@click.option(
+    "--mails_path",
+    type=click.Path(exists=False),
+    default="mails_to_sent",
+    show_default=True,
+)
+@click.option("--debug", is_flag=True)
+@click.option(
+    "--separator",
+    default=" - ",
+    show_default=True,
+    help="Separator used for subject suffix",
+)
+@click.option("--attachment_file", type=click.Path(exists=False))
+def main(
+    mails_path: str,
+    config_path: str,
+    debug: bool,
+    separator: str,
+    attachment_file: Optional[str] = None,
+) -> None:
+    if click.confirm(
+        f'You are about to send the mails under "{mails_path}". Do you want to continue?',
+        abort=True,
+    ):
+
+        with open(config_path, "r", encoding="utf-8") as config_file:
+            config = json.load(config_file)
+
+        smtp: MailConfig = MAIL_SERVER_CONFIG["gmail"]
+        if not config.get("SMTP"):
+            print(f'{"Using default Gmail SMTP server..."}')
+        else:
+            print(
+                f'Using configured SMTP server "{config.get("SMTP").get("Host")}:{config.get("SMTP").get("Port")}"...'
+            )
+            smtp = {
+                "host": config.get("SMTP").get("Host"),
+                "port": config.get("SMTP").get("Port"),
+            }
+
+        user = click.prompt("Please enter your mail account", type=str)
+        password = click.prompt(
+            "Please enter you mail password", type=str, hide_input=True
+        )
+
+        # now address_suffix id defaultdict with values of list
+        address_suffix_to_content = load_mails(mails_path)
+
+        for mail_addr_suffix_ext, mails_content in address_suffix_to_content.items():
+            mail_addr_suffix, html_ext = os.path.splitext(mail_addr_suffix_ext)
+            ishtml = True if html_ext == ".html" else False
+            for mail_content in mails_content:
+                mail_addr, *mail_suffix_and_more = mail_addr_suffix.split(
+                    separator, maxsplit=1
+                )
+                mail_suffix = mail_suffix_and_more[0] if mail_suffix_and_more else None
+                mail = build_mail(
+                    mail_addr,
+                    mail_content,
+                    config,
+                    separator,
+                    attachment_file=attachment_file,
+                    suffix=mail_suffix,
+                    html=ishtml,
+                )
+
+                if debug:
+                    dump_mail(mail, mail_suffix)
+                else:
+                    send_mail(mail, user, password, smtp)
+
+
+# pylint: disable=no-value-for-parameter
+if __name__ == "__main__":
+    main()
+# pylint: enable=no-value-for-parameter
