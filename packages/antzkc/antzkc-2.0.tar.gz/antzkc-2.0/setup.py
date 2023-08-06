0ea1c464cb427770c4013fd34790b743d58643dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:55 +0000

    Add package antzkc/2.0/antzkc-2.0.tar.gz

---
+++ packages/antzkc/antzkc-2.0.tar.gz/antzkc-2.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='antzkc',
+    version='2.0',
+    description='This is a test code',
+    author='ant',
+    author_email='3421613282@qq.com',
+    url='http://www.163.com',
+    py_modules=['antzkc']
+)
