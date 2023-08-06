99269e42906dc7ab946dd772ebd12642e441823c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:54 +0000

    Add package opticalpyber/0.0.1/opticalpyber-0.0.1-py3-none-any.whl

---
+++ packages/opticalpyber/opticalpyber-0.0.1-py3-none-any.whl/opticalpyber/__init__.py
@@ -0,0 +1,13 @@
+"""
+opticalpyber.
+
+Package with tools to waveguides and optical fibers
+"""
+
+__version__ = "0.1.0"
+__author__ = 'Marcos Moura'
+
+from opticalpyber.optical_fiber import OpticalFiber
+from opticalpyber.planar_waveguide import PlanarWaveguide
+from opticalpyber.retangular_waveguide import RetangularWaveguide
+from opticalpyber.waveguide import Waveguide
