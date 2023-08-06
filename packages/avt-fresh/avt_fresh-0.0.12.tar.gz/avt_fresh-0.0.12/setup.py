12be742e67cee966abf8f1d970f2b3c0fa55370a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:18 +0000

    Add package avt-fresh/0.0.12/avt_fresh-0.0.12.tar.gz

---
+++ packages/avt-fresh/avt_fresh-0.0.12.tar.gz/avt_fresh-0.0.12/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name="avt_fresh",
+    author="Zev Averbach",
+    author_email="zev@averba.ch",
+    version="0.0.12",
+    license="MIT",
+    python_requires=">3.10.0",
+    keywords="freshbooks API",
+    url="https://github.com/zevaverbach/avt-fresh",
+    install_requires=[
+        "requests",
+    ],
+    packages=[
+        "avt_fresh",
+    ],
+)
