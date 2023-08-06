fca44f647c0afb993cfaf4305092f2185357b49f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:48:53 +0000

    Add package zero-width-lib/1.0.0/zero_width_lib-1.0.0-py3-none-any.whl

---
+++ packages/zero-width-lib/zero_width_lib-1.0.0-py3-none-any.whl/zero_width_lib/__init__.py
@@ -0,0 +1,10 @@
+from zero_width_lib.util import zeroWidthDict, encode, decode, extract, split, z2t, t2z
+
+__name__ = 'zero_width_lib'
+
+__version__ = '1.0.0'
+VERSION = __version__
+
+__all__ = [
+    'util'
+]
