63521df313b4f1988a5228cef4a948fc781e567d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:49:36 +0000

    Add package todo-cli-tddschn/0.1.6/todo-cli-tddschn-0.1.6.tar.gz

---
+++ packages/todo-cli-tddschn/todo-cli-tddschn-0.1.6.tar.gz/todo-cli-tddschn-0.1.6/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['todo_cli_tddschn']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['colorama==0.4.4',
+ 'logging-utils-tddschn>=0.1.5,<0.2.0',
+ 'shellingham==1.4.0',
+ 'sqlmodel>=0.0.6,<0.0.7',
+ 'tabulate>=0.8.9,<0.9.0',
+ 'typer>=0.4.1,<0.5.0']
+
+entry_points = \
+{'console_scripts': ['todo = todo_cli_tddschn.cli:app']}
+
+setup_kwargs = {
+    'name': 'todo-cli-tddschn',
+    'version': '0.1.6',
+    'description': 'CLI Todo app with typer and sqlite',
+    'long_description': None,
+    'author': 'Xinyuan Chen',
+    'author_email': '45612704+tddschn@users.noreply.github.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
---
+++ packages/todo-cli-tddschn/todo-cli-tddschn-0.1.6.tar.gz/todo-cli-tddschn-0.1.6/todo_cli_tddschn/models.py
@@ -0,0 +1,43 @@
+from datetime import datetime
+from typing import Optional
+from sqlmodel import Field, Relationship, SQLModel
+
+
+class TodoBase(SQLModel):
+    description: str
+    priority: str = Field(default='medium')
+    status: str = Field(default='todo')
+    tags: str | None
+    due_date: datetime | None
+    project_id: int | None
+    project_id: Optional[int] = Field(default=None, foreign_key="project.id")
+
+
+class Todo(TodoBase, table=True):
+    id: int | None = Field(primary_key=True)
+    project: Optional['Project'] = Relationship(back_populates='todos')
+
+
+class TodoCreate(TodoBase):
+    pass
+
+
+class TodoRead(TodoBase):
+    id: int
+
+
+class ProjectBase(SQLModel):
+    name: str = Field(index=True)
+
+
+class Project(ProjectBase, table=True):
+    id: int | None = Field(primary_key=True)
+    todos: Todo | None = Relationship(back_populates='project')
+
+
+class ProjectCreate(ProjectBase):
+    pass
+
+
+class ProjectRead(ProjectBase):
+    id: int
