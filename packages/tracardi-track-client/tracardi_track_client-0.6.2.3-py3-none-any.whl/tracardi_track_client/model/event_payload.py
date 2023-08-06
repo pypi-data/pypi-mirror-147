63a396037845c360ba77a230d86954944142da27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:42 +0000

    Add package tracardi-track-client/0.6.2.3/tracardi_track_client-0.6.2.3-py3-none-any.whl

---
+++ packages/tracardi-track-client/tracardi_track_client-0.6.2.3-py3-none-any.whl/tracardi_track_client/model/event_payload.py
@@ -0,0 +1,9 @@
+from typing import Optional
+from pydantic import BaseModel
+
+
+class EventPayload(BaseModel):
+    type: str
+    properties: Optional[dict] = {}
+    options: Optional[dict] = {}
+
