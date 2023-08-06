c260f576ab55d7ff93d954d86038e6bad469f3b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:49 +0000

    Add package airontools/0.1.17/airontools-0.1.17-py3-none-any.whl

---
+++ packages/airontools/airontools-0.1.17-py3-none-any.whl/airontools/constructors/models/supervised/classification.py
@@ -0,0 +1,102 @@
+import json
+
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.layers import *
+from tensorflow.keras.metrics import Mean
+from tensorflow.keras.losses import categorical_crossentropy
+from tensorflow.keras.models import Model
+from airontools.on_the_fly import HyperDesignDropoutRate
+
+from airontools.constructors.layers import layer_constructor
+
+
+class ImageClassifierNN(Model):
+    def __init__(
+        self,
+        input_shape,
+        dropout_rate,
+        kernel_regularizer_l1,
+        kernel_regularizer_l2,
+        bias_regularizer_l1,
+        bias_regularizer_l2,
+        bn,
+        **kwargs
+    ):
+        super(ImageClassifierNN, self).__init__(**kwargs)
+
+        reg_kwargs_ = dict(
+            dropout_rate=dropout_rate,
+            kernel_regularizer_l1=kernel_regularizer_l1,
+            kernel_regularizer_l2=kernel_regularizer_l2,
+            bias_regularizer_l1=bias_regularizer_l1,
+            bias_regularizer_l2=bias_regularizer_l2,
+            bn=bn,
+        )
+
+        self.loss_tracker = Mean(name="loss")
+
+        # Encoder
+        encoder_inputs = Input(shape=input_shape)
+        encoder = layer_constructor(
+            encoder_inputs,
+            input_shape=input_shape,
+            name="encoder_conv",
+            filters=32,  # Number of filters used for the convolutional layer
+            kernel_size=15,  # Kernel size used for the convolutional layer
+            strides=2,  # Strides used for the convolutional layer
+            sequential_axis=-1,  # It's the channel axis, used to define the sequence
+            # for the self-attention layer
+            num_heads=3,  # Self-attention heads applied after the convolutional layer
+            units=10,  # Dense units applied after the self-attention layer
+            activation="softmax",  # Output activation function
+            **reg_kwargs_  # Regularization arguments
+        )
+        self.encoder = Model(encoder_inputs, encoder, name="encoder")
+
+        # Hyper design on the fly
+        self.hyper_design_dropout_rate = HyperDesignDropoutRate(model=self.encoder)
+
+    @tf.function
+    def train_step(self, data, **kwargs):
+        if len(data) == 3:
+            x, y, sample_weight = data
+        else:
+            x, y = data
+            sample_weight = 1
+        with tf.GradientTape() as tape:
+            loss = self._loss_evaluation(y, self.encoder(x), sample_weight)
+        grads = tape.gradient(loss, self.trainable_weights)
+        self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
+        self.loss_tracker.update_state(loss)
+        return {"loss": self.loss_tracker.result()}
+
+    @tf.function
+    def evaluate(self, x, y, **kwargs):
+        if "sample_weight_val" in kwargs.keys():
+            sample_weight = kwargs["sample_weight_val"]
+        else:
+            sample_weight = 1
+        return {"loss": self._loss_evaluation(y, self.encoder(x), sample_weight)}
+
+    def _loss_evaluation(self, y, y_pred, sample_weight):
+        loss = categorical_crossentropy(y, y_pred) * sample_weight
+        loss = tf.reduce_mean(tf.reduce_sum(loss))
+        return loss
+
+    @tf.function
+    def call(self, inputs):
+        return self.encoder(inputs)
+
+    def save_weights(self, path):
+        with open(path + "_weights", "w") as f:
+            json.dump([w.tolist() for w in self.encoder.get_weights()], f)
+
+    def load_weights(self, path):
+        with open(path + "_weights", "r") as f:
+            encoder_weights = [np.array(w) for w in json.load(f)]
+        self.encoder.set_weights(encoder_weights)
+
+    @tf.function
+    def summary(self):
+        self.encoder.summary()
---
+++ packages/airontools/airontools-0.1.17-py3-none-any.whl/airontools/constructors/utils.py
@@ -0,0 +1,77 @@
+import warnings
+
+import numpy as np
+import tensorflow as tf
+import tensorflow.keras.backend as k_bcknd
+from tensorflow.keras import regularizers
+from tensorflow.keras.mixed_precision import experimental as mixed_precision
+from tensorflow.keras.models import Model
+
+
+def get_latent_model(model: Model, layer_name: str):
+    try:
+        return Model(
+            inputs=model.inputs,
+            outputs=model.get_layer(layer_name).output,
+            name=layer_name + "_model"
+        )
+    except ValueError:
+        outputs = model.inputs
+        layer_found = False
+        for layer in model.layers:
+            outputs = layer(outputs)
+            if layer.name == layer_name:
+                layer_found = True
+                break
+        if layer_found:
+            return Model(inputs=model.inputs, outputs=outputs, name=layer_name + "_model")
+        else:
+            warnings.warn("could not find the layer")
+
+
+def set_precision(precision: float):
+    if "float16" in precision:
+        if precision == "mixed_float16":
+            policy = mixed_precision.Policy("mixed_float16")
+            mixed_precision.set_policy(policy)
+        else:
+            tf.keras.backend.set_floatx("float16")
+
+
+def to_time_series(tensor: tf.Tensor) -> tf.Tensor:
+    return k_bcknd.expand_dims(tensor, axis=2)
+
+
+def get_layer_units(
+    input_dim: int, output_dim: int, n_layers: int, min_hidden_units=2
+) -> list:
+    units = [
+        max(int(units), min_hidden_units)
+        for units in np.linspace(input_dim, output_dim, n_layers + 1)
+    ]
+    units[0], units[-1] = input_dim, output_dim
+    return units
+
+
+def rm_redundant(values: list, value: int) -> list:
+    taken = False
+    values_ = []
+    for n in values:
+        if n != value:
+            values_ += [n]
+        elif not taken:
+            values_ += [n]
+            taken = True
+    return values_
+
+
+def regularizer(l1=None, l2=None) -> regularizers.Regularizer:
+    if l1 and l2:
+        regularizer = regularizers.l1_l2(l1=l1, l2=l2)
+    elif l1:
+        regularizer = regularizers.l1(l1)
+    elif l2:
+        regularizer = regularizers.l2(l2)
+    else:
+        regularizer = None
+    return regularizer
