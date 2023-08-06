d27bd39dc87973ce5043854f30bb78085b5559e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:37 +0000

    Add package build-django/0.0.10/build-django-0.0.10.tar.gz

---
+++ packages/build-django/build-django-0.0.10.tar.gz/build-django-0.0.10/setup.py
@@ -0,0 +1,43 @@
+import os
+import codecs
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.10'
+
+DESCRIPTION = 'Command line utility for building Django projects'
+
+# Setting up
+setup(
+    name="build-django",
+    version=VERSION,
+    author="Kapustlo",
+    description=DESCRIPTION,
+    url='https://notabug.org/kapustlo/build-django',
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    keywords=['python', 'django', 'build', 'cli', 'generate', 'code'],
+    entry_points={
+        'console_scripts': [
+            'build-django=build_django.__main__:main'
+        ]
+    },
+    classifiers=[
+        "Framework :: Django",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Topic :: Software Development :: Code Generators",
+        "Environment :: Console",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3 :: Only",
+        "Operating System :: POSIX :: Linux",
+    ],
+    python_requires=">=3.8",
+)
+
+
