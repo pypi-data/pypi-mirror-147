763b80f5f9af4976c8fa59afe6ea71b595e00e63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:16 +0000

    Add package strangeworks-rigetti/0.2.4/strangeworks-rigetti-0.2.4.tar.gz

---
+++ packages/strangeworks-rigetti/strangeworks-rigetti-0.2.4.tar.gz/strangeworks-rigetti-0.2.4/strangeworks/rigetti/compiler.py
@@ -0,0 +1,62 @@
+import pyquil
+from pyquil.api import AbstractCompiler, QuantumExecutable
+from pyquil.quil import Program
+from typing import Optional
+import strangeworks
+
+from strangeworks.rigetti.program import CompiledProgram, executable_from_json
+
+
+class StrangeworksCompiler(AbstractCompiler):
+    def __init__(self, target: str, as_qvm: str):
+        self.target = target
+        self.as_qvm = as_qvm
+
+    def quil_to_native_quil(
+        self, prg: Program, *, protoquil: Optional[bool] = None
+    ) -> CompiledProgram:
+        payload = self.__serialize_program(prg)
+        payload["target"] = self.target
+        if protoquil:
+            payload["protoquil"] = str(protoquil)
+        shots = prg.num_shots
+        if shots is None:
+            shots = 1
+        payload["shots"] = shots
+        res = strangeworks.client.rest_client.post(
+            "/plugins/rigetti/compile?return_executable=False", json=payload
+        )
+        return CompiledProgram.from_json(program=prg, payload=res)
+
+    def native_quil_to_executable(self, nq_program: Program) -> QuantumExecutable:
+        payload = self.__serialize_program(nq_program)
+        payload["target"] = self.target
+        shots = nq_program.num_shots
+        if shots is None:
+            shots = 1
+        payload["shots"] = shots
+        res = strangeworks.client.rest_client.post(
+            "/plugins/rigetti/compile?return_executable=True", json=payload
+        )
+        return executable_from_json(res)
+
+    def get_calibration_program(self, force_refresh: bool = False) -> Program:
+        payload = {
+            "as_qvm": self.as_qvm,
+            "circuit": "",
+            "circuit_type": "pyquil.Program",
+            "version": pyquil.pyquil_version,
+        }
+        payload["target"] = self.target
+        res = strangeworks.client.rest_client.post(
+            "/plugins/rigetti/compile?get_calibration_program=True", json=payload
+        )
+        return CompiledProgram.from_json(program=None, payload=res)
+
+    def __serialize_program(self, p: Program) -> dict:
+        return {
+            "as_qvm": self.as_qvm,
+            "circuit": p.out(calibrations=False),
+            "circuit_type": "pyquil.Program",
+            "version": pyquil.pyquil_version,
+        }
