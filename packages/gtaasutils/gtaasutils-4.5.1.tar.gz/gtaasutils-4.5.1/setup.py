8d32eea07c3a93a9bd64417fe8624c3d7f615f98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:29 +0000

    Add package gtaasutils/4.5.1/gtaasutils-4.5.1.tar.gz

---
+++ packages/gtaasutils/gtaasutils-4.5.1.tar.gz/gtaasutils-4.5.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='gtaasutils',
+    version='4.5.1',
+    py_modules=['gtaasutils'],
+    author='QinExpire',
+    author_email='qinexpire@gmail.com',
+    url='https://blog.csdn.net/andrew_wf',
+    description='Format number'
+)
