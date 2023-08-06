866438656b560b8663c9039751ea297557037b14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:39 +0000

    Add package testouillos/0.0.2/testouillos-0.0.2.tar.gz

---
+++ packages/testouillos/testouillos-0.0.2.tar.gz/testouillos-0.0.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['testouillos']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests>=2.27.1,<3.0.0']
+
+setup_kwargs = {
+    'name': 'testouillos',
+    'version': '0.0.2',
+    'description': 'A simple test',
+    'long_description': None,
+    'author': 'Yann Rabiller',
+    'author_email': 'yann.rabiller@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
