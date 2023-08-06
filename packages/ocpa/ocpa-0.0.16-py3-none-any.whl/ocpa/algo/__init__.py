841d1e106878fc68cf8f92334e139d90b2a31aba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:25 +0000

    Add package ocpa/0.0.16/ocpa-0.0.16-py3-none-any.whl

---
+++ packages/ocpa/ocpa-0.0.16-py3-none-any.whl/ocpa/algo/__init__.py
@@ -0,0 +1,12 @@
+import ocpa.algo.discovery
+import ocpa.algo.evaluation
+import ocpa.algo.conformance
+import ocpa.algo.enhancement
+import ocpa.algo.util
+import ocpa.algo.filtering
+import ocpa.algo.projection
+import ocpa.algo.reduction
+
+
+
+
