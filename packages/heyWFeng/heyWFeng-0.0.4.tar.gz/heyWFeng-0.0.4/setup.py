12869844b25497d5b5fa2fab5375cd3b91a683e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:36 +0000

    Add package heyWFeng/0.0.4/heyWFeng-0.0.4.tar.gz

---
+++ packages/heyWFeng/heyWFeng-0.0.4.tar.gz/heyWFeng-0.0.4/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+#############################################
+# File Name: setup.py
+# Author: heyWFeng
+# Mail: 1957875073@qq.com
+# Created Time:  2022-1-5 10:17:34
+#############################################
+
+from setuptools import setup, find_packages  # 这个包没有的可以pip一下
+
+setup()
