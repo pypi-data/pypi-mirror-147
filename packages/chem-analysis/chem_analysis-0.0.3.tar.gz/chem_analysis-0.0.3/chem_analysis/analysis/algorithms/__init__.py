9336af1cf1042ec71400cd301008ff4a40578043
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:01 +0000

    Add package chem-analysis/0.0.3/chem_analysis-0.0.3.tar.gz

---
+++ packages/chem-analysis/chem_analysis-0.0.3.tar.gz/chem_analysis-0.0.3/chem_analysis/analysis/algorithms/__init__.py
@@ -0,0 +1,5 @@
+from chem_analysis.analysis.algorithms.despike import despike_methods
+from chem_analysis.analysis.algorithms.baseline import baseline_methods
+from chem_analysis.analysis.algorithms.smoothing import smoothing_methods
+from chem_analysis.analysis.algorithms.peak_picking import peak_picking_methods, scipy_find_peaks
+from chem_analysis.analysis.algorithms.bound_detection import bound_detection_methods, rolling_value
---
+++ packages/chem-analysis/chem_analysis-0.0.3.tar.gz/chem_analysis-0.0.3/chem_analysis/analysis/utils/__init__.py
@@ -0,0 +1,5 @@
+from chem_analysis.analysis.utils.obj_list import ObjList
+from chem_analysis.analysis.utils.pipeline import Pipeline
+
+
+fig_count = 0
