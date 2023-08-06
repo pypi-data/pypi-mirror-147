65f02206dd1579e1122c03c6af7a4875995acedc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:16:17 +0000

    Add package profileqc/0.0.2/profileqc-0.0.2-py2.py3-none-any.whl

---
+++ packages/profileqc/profileqc-0.0.2-py2.py3-none-any.whl/profileqc/routines/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# Copyright (c) 2022 SMHI, Swedish Meteorological and Hydrological Institute.
+# License: MIT License (see LICENSE.txt or http://opensource.org/licenses/mit).
+"""
+Created on 2020-02-20 13:26
+
+@author: a002028
+"""
+from profileqc.routines.continuous import Decreasing, Increasing  # noqa: F401
+from profileqc.routines.dependencies import Dependencies  # noqa: F401
+from profileqc.routines.diff import DataDiff  # noqa: F401
+from profileqc.routines.range import Range  # noqa: F401
+from profileqc.routines.spike import Spike  # noqa: F401
