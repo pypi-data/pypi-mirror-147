fb11541d59033fdeec506086c8bc3489f8a24b47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:29 +0000

    Add package antiberty/0.0.3/antiberty-0.0.3.tar.gz

---
+++ packages/antiberty/antiberty-0.0.3.tar.gz/antiberty-0.0.3/antiberty/utils/get_weights.py
@@ -0,0 +1,12 @@
+import os
+import urllib.request
+
+import antiberty
+from antiberty.utils.general import exists
+
+
+def get_weights():
+    project_path = os.path.dirname(os.path.realpath(antiberty.__file__))
+    dest = os.path.join(project_path, "trained_models/AntiBERTy_md_smooth/")
+
+    return dest
