a8f6f6f61d5deee36bf7ab7644ae529316606e63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:21 +0000

    Add package pyzr/0.0.4/pyzr-0.0.4.tar.gz

---
+++ packages/pyzr/pyzr-0.0.4.tar.gz/pyzr-0.0.4/src/pyzr/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: guzhi
+@file: __init__.py
+@time: 2022/04/24
+@projectExplain: 
+"""
+
+from __future__ import absolute_import
+from .alert import Alert
+from .phone import Phone
+
+name = "pyzr"
