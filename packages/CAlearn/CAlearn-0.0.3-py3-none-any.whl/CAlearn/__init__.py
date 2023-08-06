813ecce2594944b17d887841f1b2ab45b74b8beb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:26 +0000

    Add package CAlearn/0.0.3/CAlearn-0.0.3-py3-none-any.whl

---
+++ packages/CAlearn/CAlearn-0.0.3-py3-none-any.whl/CAlearn/__init__.py
@@ -0,0 +1,6 @@
+"""Genetic Programming in Python, with a scikit-learn inspired API
+``CAlearn`` is a set of algorithms for learning genetic programming models of Cellular Automaton.
+"""
+__version__ = '0.0.3'
+
+__all__ = ['genetic', 'functions', 'fitness']
