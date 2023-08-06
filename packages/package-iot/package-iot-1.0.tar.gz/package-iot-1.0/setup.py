f34f7d31e4453c9532a73ebc619fe751bcc2a404
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:08 +0000

    Add package package-iot/1.0/package-iot-1.0.tar.gz

---
+++ packages/package-iot/package-iot-1.0.tar.gz/package-iot-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='package-iot',
+      version='1.0',
+      description='Calculate and visuallize, Gaussian, Binomial and Normal distributions',
+      packages=['package-iot'],
+      author_email='sjahnaviprasad@gmail.com',
+      zip_safe=False)
