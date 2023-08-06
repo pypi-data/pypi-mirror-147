83c22206b09575c443288b938ab831174707d589
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:48:45 +0000

    Add package serializers-set/1.0/serializers-set-1.0.tar.gz

---
+++ packages/serializers-set/serializers-set-1.0.tar.gz/serializers-set-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name="serializers-set",
+    version="1.0",
+    author="sakovnv",
+    author_email="sakovnv@gmail.com",
+    description="json, toml, yaml serializers",
+    keywords="json toml yaml serializer",
+    packages=['src'],
+)
