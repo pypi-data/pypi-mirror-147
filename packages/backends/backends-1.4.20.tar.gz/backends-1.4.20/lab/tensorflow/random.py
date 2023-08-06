d563d5599018e3da6b07d59cdda3f043fc425327
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:13 +0000

    Add package backends/1.4.20/backends-1.4.20.tar.gz

---
+++ packages/backends/backends-1.4.20.tar.gz/backends-1.4.20/lab/tensorflow/random.py
@@ -0,0 +1,75 @@
+import logging
+
+import tensorflow as tf
+
+from . import dispatch, B
+from ..types import TFDType, TFNumeric, Int, TFRandomState
+
+__all__ = []
+
+log = logging.getLogger(__name__)
+
+
+@dispatch
+def create_random_state(_: TFDType, seed: Int = 0):
+    return tf.random.Generator.from_seed(seed)
+
+
+@dispatch
+def global_random_state(_: TFDType):
+    return tf.random.get_global_generator()
+
+
+@dispatch
+def set_global_random_state(state: TFRandomState):
+    tf.random.set_global_generator(state)
+
+
+@dispatch
+def rand(state: TFRandomState, dtype: TFDType, *shape: Int):
+    return state, state.uniform(shape, dtype=dtype)
+
+
+@dispatch
+def rand(dtype: TFDType, *shape: Int):
+    return rand(global_random_state(dtype), dtype, *shape)[1]
+
+
+@dispatch
+def randn(state: TFRandomState, dtype: TFDType, *shape: Int):
+    return state, state.normal(shape, dtype=dtype)
+
+
+@dispatch
+def randn(dtype: TFDType, *shape: Int):
+    return randn(global_random_state(dtype), dtype, *shape)[1]
+
+
+@dispatch
+def choice(state: TFRandomState, a: TFNumeric, n: Int):
+    inds = state.uniform([n], minval=0, maxval=a.shape[0], dtype=tf.int64)
+    choices = tf.gather(a, inds)
+    return state, choices[0] if n == 1 else choices
+
+
+@dispatch
+def choice(a: TFNumeric, n: Int):
+    return choice(global_random_state(a), a, n)[1]
+
+
+@dispatch
+def randint(
+    state: TFRandomState,
+    dtype: TFDType,
+    *shape: Int,
+    lower: Int = 0,
+    upper: Int,
+):
+    dtype = B.dtype_int(dtype)
+    return state, state.uniform(shape, lower, upper, dtype=dtype)
+
+
+@dispatch
+def randint(dtype: TFDType, *shape: Int, lower: Int = 0, upper: Int):
+    state = global_random_state(dtype)
+    return randint(state, dtype, *shape, lower=lower, upper=upper)[1]
