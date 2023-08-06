6d6334daa60817b8a316fe63af4b9a3992a81782
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:37 +0000

    Add package gdsfactory/5.0.8/gdsfactory-5.0.8.tar.gz

---
+++ packages/gdsfactory/gdsfactory-5.0.8.tar.gz/gdsfactory-5.0.8/gdsfactory/components/resistance_sheet.py
@@ -0,0 +1,64 @@
+from functools import partial
+
+from gdsfactory.cell import cell
+from gdsfactory.component import Component
+from gdsfactory.components.compass import compass
+from gdsfactory.components.contact import contact_slab_npp_m3
+from gdsfactory.tech import LAYER
+from gdsfactory.types import ComponentFactory, Floats, Layers, Optional
+
+pad_contact_slab_npp = partial(contact_slab_npp_m3, size=(80, 80))
+
+
+@cell
+def resistance_sheet(
+    width: float = 10,
+    layers: Layers = (LAYER.SLAB90, LAYER.NPP),
+    layer_offsets: Floats = (0, 0.2),
+    pad: ComponentFactory = pad_contact_slab_npp,
+    pad_pitch: float = 100.0,
+    ohms_per_square: Optional[float] = None,
+) -> Component:
+    """Sheet resistance.
+    keeps connectivity for pads and first layer in layers
+
+    Args:
+        width:
+        layers: for the middle part
+        layer_offsets: from edge, positive: over, negative: inclusion
+        pad: function to create a pad
+        pad_pitch:
+        ohms_per_square: optional sheet resistance to compute info.resistance
+    """
+    c = Component()
+
+    pad = pad()
+    length = pad_pitch - pad.get_setting("size")[0]
+
+    pad1 = c << pad
+    pad2 = c << pad
+    r0 = c << compass(
+        size=(length + layer_offsets[0], width + layer_offsets[0]), layer=layers[0]
+    )
+
+    for layer, offset in zip(layers[1:], layer_offsets[1:]):
+        c << compass(size=(length + 2 * offset, width + 2 * offset), layer=layer)
+
+    pad1.connect("e3", r0.ports["e1"])
+    pad2.connect("e1", r0.ports["e3"])
+
+    c.info["resistance"] = ohms_per_square * width * length if ohms_per_square else None
+
+    c.add_port("pad1", port_type="vertical_dc", midpoint=pad1.center)
+    c.add_port("pad2", port_type="vertical_dc", midpoint=pad2.center)
+    return c
+
+
+if __name__ == "__main__":
+    c = resistance_sheet(width=40)
+    c.show()
+
+    # import gdsfactory as gf
+    # sweep_resistance = list(map(resistance_sheet, (5, 10, 80)))
+    # c = gf.grid(sweep_resistance)
+    # c.show()
---
+++ packages/gdsfactory/gdsfactory-5.0.8.tar.gz/gdsfactory-5.0.8/gdsfactory/components/straight_pin_slot.py
@@ -0,0 +1,119 @@
+"""Straight Doped PIN waveguide."""
+from typing import Optional
+
+import gdsfactory as gf
+from gdsfactory.component import Component
+from gdsfactory.components.contact import contact_m1_m3
+from gdsfactory.components.contact_slot import contact_slot_slab_m1
+from gdsfactory.components.taper import taper_strip_to_ridge
+from gdsfactory.cross_section import pin, pn
+from gdsfactory.types import ComponentFactory, CrossSectionSpec
+
+
+@gf.cell
+def straight_pin_slot(
+    length: float = 500.0,
+    cross_section: CrossSectionSpec = pin,
+    contact: Optional[ComponentFactory] = contact_m1_m3,
+    contact_width: float = 10.0,
+    contact_slab: Optional[ComponentFactory] = contact_slot_slab_m1,
+    contact_slab_top: Optional[ComponentFactory] = None,
+    contact_slab_bot: Optional[ComponentFactory] = None,
+    contact_slab_width: Optional[float] = None,
+    contact_spacing: float = 3.0,
+    contact_slab_spacing: float = 2.0,
+    taper: Optional[ComponentFactory] = taper_strip_to_ridge,
+    **kwargs,
+) -> Component:
+    """Returns a PIN straight waveguide with slotted via
+
+    https://doi.org/10.1364/OE.26.029983
+
+    500um length for PI phase shift
+    https://ieeexplore.ieee.org/document/8268112
+
+    to go beyond 2PI, you will need at least 1mm
+    https://ieeexplore.ieee.org/document/8853396/
+
+    Args:
+        length: of the waveguide
+        cross_section: for the waveguide
+        contact: for contacting the metal
+        contact_width:
+        contact_slab: function for the component contacting the slab
+        contact_slab_top: Optional, defaults to contact_slab
+        contact_slab_bot: Optional, defaults to contact_slab
+        contact_slab_width: defaults to contact_width
+        contact_spacing: spacing between contacts
+        taper: optional taper
+        kwargs: cross_section settings
+
+    """
+    c = Component()
+    if taper:
+        taper = taper() if callable(taper) else taper
+        length -= 2 * taper.get_ports_xsize()
+
+    wg = c << gf.components.straight(
+        cross_section=cross_section,
+        length=length,
+        **kwargs,
+    )
+
+    contact_slab_width = contact_slab_width or contact_width
+    contact_slab_spacing = contact_slab_spacing or contact_spacing
+
+    if taper:
+        t1 = c << taper
+        t2 = c << taper
+        t1.connect("o2", wg.ports["o1"])
+        t2.connect("o2", wg.ports["o2"])
+        c.add_port("o1", port=t1.ports["o1"])
+        c.add_port("o2", port=t2.ports["o1"])
+
+    else:
+        c.add_ports(wg.get_ports_list())
+
+    contact_length = length
+
+    if contact:
+        contact_top = c << contact(
+            size=(contact_length, contact_width),
+        )
+        contact_bot = c << contact(
+            size=(contact_length, contact_width),
+        )
+
+        contact_bot.x = wg.x
+        contact_top.x = wg.x
+
+        contact_top.ymin = +contact_spacing / 2
+        contact_bot.ymax = -contact_spacing / 2
+        c.add_ports(contact_bot.ports, prefix="bot_")
+        c.add_ports(contact_top.ports, prefix="top_")
+
+    contact_slab_top = contact_slab_top or contact_slab
+    contact_slab_bot = contact_slab_bot or contact_slab
+
+    if contact_slab_top:
+        slot_top = c << contact_slab_top(
+            size=(contact_length, contact_slab_width),
+        )
+        slot_top.x = wg.x
+        slot_top.ymin = +contact_slab_spacing / 2
+
+    if contact_slab_bot:
+        slot_bot = c << contact_slab_bot(
+            size=(contact_length, contact_slab_width),
+        )
+        slot_bot.x = wg.x
+        slot_bot.ymax = -contact_slab_spacing / 2
+
+    return c
+
+
+straight_pn_slot = gf.partial(straight_pin_slot, cross_section=pn)
+
+if __name__ == "__main__":
+    c = straight_pin_slot(contact_width=4, contact_slab_width=3, length=50)
+    c.show()
