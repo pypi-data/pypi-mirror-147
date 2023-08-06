94d8e402e458c2e02d97a0697db845211862bf8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:56 +0000

    Add package password-generator-imbev/0.1.0/password-generator-imbev-0.1.0.tar.gz

---
+++ packages/password-generator-imbev/password-generator-imbev-0.1.0.tar.gz/password-generator-imbev-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['password_generator']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'password-generator-imbev',
+    'version': '0.1.0',
+    'description': 'A password generator written in Python.',
+    'long_description': None,
+    'author': 'Isaac Beverly',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
