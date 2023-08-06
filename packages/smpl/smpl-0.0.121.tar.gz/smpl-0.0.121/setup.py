b88557d43b3057f923efad5ffce2c2c87fef815f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:18 +0000

    Add package smpl/0.0.121/smpl-0.0.121.tar.gz

---
+++ packages/smpl/smpl-0.0.121.tar.gz/smpl-0.0.121/setup.py
@@ -0,0 +1,61 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+requirements = [
+    "uncertainties",
+    "numpy",
+    "matplotlib",
+    "scipy",
+    "sympy",
+    "tqdm",
+    "pandas",
+    # "requests",
+]
+dev_requirements = [
+    "build",
+    "pytest",
+    "sphinx",
+    "sphinx-math-dollar",
+    "jupyterlab",
+    "numpydoc",
+    "sphinx",
+    "nbsphinx",
+    "sphinx-rtd-theme",
+    "pandas",
+    "ipython",
+    "pandoc",
+    "jupyter-sphinx"
+]
+
+setuptools.setup(
+    name="smpl",
+    setup_requires=['setuptools-git-versioning'],
+    author="APN",
+    author_email="APN-Pucky@no-reply.github.com",
+    description="simple plotting and fitting",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/APN-Pucky/smpl",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=requirements,
+    extra_require={
+        'dev': dev_requirements
+    },
+    version_config={
+        "template": "{tag}",
+        "dev_template": "{tag}.{ccount}",
+        "dirty_template": "{tag}.{ccount}+dirty",
+        "starting_version": "0.0.0",
+        "version_callback": None,
+        "version_file": None,
+        "count_commits_from_version_file": False
+    },
+    python_requires='>=3.6',
+)
