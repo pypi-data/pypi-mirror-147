6476ae8fe91da65e547142003c7077b9db46ec71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:26 +0000

    Add package sqlcrud/1.0.0/sqlcrud-1.0.0.tar.gz

---
+++ packages/sqlcrud/sqlcrud-1.0.0.tar.gz/sqlcrud-1.0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name="sqlcrud",
+    version="1.0.0",
+    packages=["sqlcrud"],
+    install_requires=["sqlmodel==0.0.6"],
+)
---
+++ packages/sqlcrud/sqlcrud-1.0.0.tar.gz/sqlcrud-1.0.0/sqlcrud/__init__.py
@@ -0,0 +1,1 @@
+from sqlcrud.crud import CrudBase
