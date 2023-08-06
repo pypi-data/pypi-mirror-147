99632f264c97441b76b55621b945dfdf28e66e6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:48 +0000

    Add package django-form-surveys/1.5.4/django-form-surveys-1.5.4.tar.gz

---
+++ packages/django-form-surveys/django-form-surveys-1.5.4.tar.gz/django-form-surveys-1.5.4/djf_surveys/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from djf_surveys import views
+
+
+app_name = 'djf_surveys'
+urlpatterns = [
+    path('', views.SurveyListView.as_view(), name='index'),
+    path('detail/<str:slug>', views.DetailSurveyView.as_view(), name='detail'),
+    path('edit/<int:pk>', views.EditSurveyFormView.as_view(), name='edit'),
+    path('create/<str:slug>', views.CreateSurveyFormView.as_view(), name='create'),
+    path('delete/<int:pk>', views.DeleteSurveyAnswerView.as_view(), name='delete'),
+    path('dashboard/', include('djf_surveys.admins.urls')),
+]
