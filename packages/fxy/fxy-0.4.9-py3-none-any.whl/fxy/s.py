b0971b2c053b5c78ecf61830fe23e71a57bf529d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:48:57 +0000

    Add package fxy/0.4.9/fxy-0.4.9-py3-none-any.whl

---
+++ packages/fxy/fxy-0.4.9-py3-none-any.whl/fxy/s.py
@@ -0,0 +1,13 @@
+# Symbolics and Calculus
+
+try:
+    import sympy; exec(sympy.interactive.session.preexec_source)
+except:
+    pass
+
+try:
+    from sympy.plotting import plot3d
+except:
+    pass
+
+# Plotting is included
