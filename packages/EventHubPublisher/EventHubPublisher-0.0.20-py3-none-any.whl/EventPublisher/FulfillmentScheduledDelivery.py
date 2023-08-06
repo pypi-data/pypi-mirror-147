c0abc685853625b3f38d7c79b70bbe72099f01f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:54 +0000

    Add package EventHubPublisher/0.0.20/EventHubPublisher-0.0.20-py3-none-any.whl

---
+++ packages/EventHubPublisher/EventHubPublisher-0.0.20-py3-none-any.whl/EventPublisher/FulfillmentScheduledDelivery.py
@@ -0,0 +1,15 @@
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__)))
+
+import EventHub.SendEvents as SendEvents
+
+def run(env_name, client_code, account_name, events_json):
+    return SendEvents.send_events('FulfillmentScheduledDelivery', env_name, client_code, account_name, events_json)
+
+#if len(sys.argv) > 0:
+#    print(SendEvents.run('FulfillmentScheduledDelivery'))
+#    print('order.create events were sent.')
+
+
