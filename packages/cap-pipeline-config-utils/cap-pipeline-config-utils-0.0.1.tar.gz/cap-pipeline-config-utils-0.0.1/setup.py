e088829fd37e4726a477d2f3d66f277cdc7cafd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:11 +0000

    Add package cap-pipeline-config-utils/0.0.1/cap-pipeline-config-utils-0.0.1.tar.gz

---
+++ packages/cap-pipeline-config-utils/cap-pipeline-config-utils-0.0.1.tar.gz/cap-pipeline-config-utils-0.0.1/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cap-pipeline-config-utils",
+    version="0.0.1",
+    author="Ugur Bayindir",
+    author_email="ugur@ebi.ac.uk",
+    description="Utility package to autogenerate of semantic tags in ontology pipeline",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ubyndr/config_utils",
+    project_urls={
+        "Bug Tracker": "https://github.com/ubyndr/config_utils/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    install_requires=[
+        'jsonschema==4.4.0',
+        'SPARQLWrapper==1.8.5'
+      ],
+    python_requires=">=3.6",
+)
