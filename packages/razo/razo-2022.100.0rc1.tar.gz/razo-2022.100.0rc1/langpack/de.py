73cddfb6b909221c1c563472e659d6fbab29bcbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:31 +0000

    Add package razo/2022.100.0rc1/razo-2022.100.0rc1.tar.gz

---
+++ packages/razo/razo-2022.100.0rc1.tar.gz/razo-2022.100.0rc1/langpack/de.py
@@ -0,0 +1,36 @@
+pack=['''
+Urheberrecht (c) 2022 Razo Gemeinschaft
+
+Hiermit wird jeder Person, die eine Kopie erhält, kostenlos die Erlaubnis erteilt, eine Kopie zu erhalten.
+dieser Software und der zugehörigen Dokumentationsdateien (die "Software"), um
+in der Software ohne Einschränkung, einschließlich, aber nicht beschränkt auf die Rechte
+zu verwenden, zu kopieren, zu modifizieren, zusammenzuführen, zu veröffentlichen, zu verteilen, unterzulizenzieren und/oder zu verkaufen
+Kopien der Software und um Personen, denen die Software zur Verfügung steht, zu erlauben
+zu diesem Zweck unter folgenden Bedingungen ausgestattet sind:
+
+Der obige Urheberrechtshinweis und dieser Erlaubnishinweis sind in allen
+Kopien oder wesentliche Teile der Software.
+
+DIE SOFTWARE WIRD "WIE BESEHEN" ZUR VERFÜGUNG GESTELLT, OHNE JEGLICHE GEWÄHRLEISTUNG, AUSDRÜCKLICH ODER
+STILLSCHWEIGEND, EINSCHLIESSLICH, ABER NICHT BESCHRÄNKT AUF DIE GEWÄHRLEISTUNG DER MARKTGÄNGIGKEIT,
+EIGNUNG FÜR EINEN BESTIMMTEN ZWECK UND NICHTVERLETZUNG. IN KEINEM FALL DARF DIE
+AUTOREN ODER URHEBERRECHTSINHABER HAFTEN FÜR JEGLICHE ANSPRÜCHE, SCHÄDEN ODER ANDERE
+HAFTUNG, SEI ES AUS EINER VERTRAGS-, UNERLAUBTEN HANDLUNG ODER AUS ANDEREN GRÜNDEN, DIE SICH ERGIBT AUS,
+AUS ODER IM ZUSAMMENHANG MIT DER SOFTWARE ODER DER NUTZUNG ODER SONSTIGEN GESCHÄFTEN IM
+SOFTWARE.
+''','Razo first use setting.','Stimmen Sie der obigen Lizenz zu?[ y/n][y in default]','Weil Sie mit der Lizenz nicht einverstanden sind, wird sie heruntergefahren.',
+'Bitte setzen Sie das root-Passwort (denken Sie daran, andere nicht wissen zu lassen):','Wie lautet Ihr Benutzername (auf Englisch)?',
+'Geben Sie Hilfe für Hilfe ein.','Noch testen,bitte wissen.',
+'''
+help: Hilfe anzeigen.
+su:Fragen Sie nach Superuser-Lizenz.
+shutdown:Shutdown razo.
+info: Info anzeigen.
+setting: Ausführungseinstellung.
+time:Holen Sie sich Zeit.
+sudo(add before commands):Lassen Sie den Befehl nach sudo temp root erhalten.
+[Ein Modulname]:Import(run)dieses Modul.
+''', 'Bitte geben Sie das root-Passwort ein:' ,'su:Sorry', "Möchten Sie WIRKLICH herunterfahren?[y/n]",
+"Herunterfahren. ",{6:'So.',0:'Mo.',1:'Di.',2:'Mi.',3:'Do.',4:'Fr',5:'Sa.'},
+'%Y-%m-%d %H:%M:%S','Nicht möglich bis root.','SYS1001:Es gibt keinen Befehl {}.',
+'Hallo,{}.','Entschuldigung.','Sie müssen neu starten, um die Einrichtung abzuschließen.']
---
+++ packages/razo/razo-2022.100.0rc1.tar.gz/razo-2022.100.0rc1/langpack/fr.py
@@ -0,0 +1,36 @@
+pack=['''
+Droits d’auteur (c) 2022 Razo communauté
+
+L’autorisation est accordée, gratuitement, à toute personne obtenant une copie
+de ce logiciel et des fichiers de documentation associés (le « Logiciel »), à traiter
+dans le Logiciel sans restriction, y compris, mais sans s’y limiter, les droits
+pour utiliser, copier, modifier, fusionner, publier, distribuer, concéder en sous-licence et/ou vendre
+des copies du Logiciel, et pour permettre aux personnes auxquelles le Logiciel est destiné
+fournis à cet effet, sous réserve des conditions suivantes:
+
+L’avis de droit d’auteur ci-dessus et cet avis d’autorisation doivent être inclus dans tous les
+des copies ou des parties substantielles du Logiciel.
+
+LE LOGICIEL EST FOURNI « EN L’ÉTAT », SANS GARANTIE D’AUCUNE SORTE, EXPRESSE OU
+IMPLICITE, Y COMPRIS, MAIS SANS S’Y LIMITER, LES GARANTIES DE QUALITÉ MARCHANDE,
+APTITUDE À UN USAGE PARTICULIER ET NON-CONTREFAÇON. EN AUCUN CAS, LE
+LES AUTEURS OU LES DÉTENTEURS DE DROITS D’AUTEUR DOIVENT ÊTRE TENUS RESPONSABLES DE TOUTE RÉCLAMATION, DOMMAGE OU AUTRE
+RESPONSABILITÉ, QUE CE SOIT DANS LE CADRE D’UNE ACTION CONTRACTUELLE, DÉLICTUELLE OU AUTRE, DÉCOULANT DE,
+HORS OU EN RELATION AVEC LE LOGICIEL OU L’UTILISATION OU D’AUTRES TRANSACTIONS DANS LE
+LOGICIEL.
+''','Paramètre d’utilisation de la première utilisation de Razo.','Êtes-vous d’accord avec la licence ci-dessus?[ y/n][y par défaut]','Parce que vous n’êtes pas d’accord avec la licence,elle s’arrête.',
+'Veuillez définir le mot de passe root (n’oubliez pas de laisser les autres ne peuvent pas savoir):','Quel est votre nom d’utilisateur (en anglais)?',
+'Tapez help for help.','Still testing,please know.',
+'''
+help:Afficher l’aide.
+su:Demandez une licence de superutilisateur.
+shutdown:Shutdown razo.
+info:Afficher les infos.
+setting:Exécuter le paramètre.
+time:Obtenez l’heure.
+sudo(ajouter avant les commandes):Laissez la commande après sudo obtenir temp root.
+[Un nom de module]:Import(run)ce module.
+''', 'Veuillez entrer le mot de passe root:' ,'su:Désolé', 'Voulez-vous VRAIMENT arrêter?[ y/n]',
+' Fermeture. ',{6:'Dim.',0:'Lun.',1:'Mar.',2:'Mer.',3:'Jeu.',4:'Ven',5:'Sam.'},
+'%Y-%m-%d %H:%M:%S','Impossible jusqu’à root.','SYS1001:There is not command {}.',
+'Bonjour,{}.','Désolé.','Vous devez redémarrer pour terminer l’installation.']
