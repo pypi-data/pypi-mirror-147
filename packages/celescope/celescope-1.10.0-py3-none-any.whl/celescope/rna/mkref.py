67d55ca792df5c712a6cf30efa70cd8eb01aa1db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:48 +0000

    Add package celescope/1.10.0/celescope-1.10.0-py3-none-any.whl

---
+++ packages/celescope/celescope-1.10.0-py3-none-any.whl/celescope/rna/mkref.py
@@ -0,0 +1,98 @@
+import subprocess
+
+from celescope.tools import utils
+from celescope.tools.mkref import Mkref, super_opts
+
+
+class Mkref_rna(Mkref):
+    """
+    ## Features
+    - Create a genome reference directory.
+
+    ## Output
+
+    - STAR genome index files
+
+    - Genome refFlat file
+
+    - Genome config file
+    ```
+    $ cat celescope_genome.config
+    [genome]
+    genome_name = Homo_sapiens_ensembl_99
+    genome_type = rna
+    fasta = Homo_sapiens.GRCh38.dna.primary_assembly.fa
+    gtf = Homo_sapiens.GRCh38.99.gtf
+    refflat = Homo_sapiens_ensembl_99.refFlat
+    ```
+    """
+
+    def __init__(self, genome_type, args, files=(), non_files=()):
+        super().__init__(genome_type, args, files, non_files)
+        self.refflat = f'{self.genome_name}.refFlat'
+
+    @utils.add_log
+    def build_star_index(self):
+        cmd = (
+            f'STAR \\\n'
+            f'--runMode genomeGenerate \\\n'
+            f'--runThreadN {self.thread} \\\n'
+            f'--genomeDir ./ \\\n'
+            f'--genomeFastaFiles {self.fasta} \\\n'
+            f'--sjdbGTFfile {self.gtf} \\\n'
+            f'--sjdbOverhang 100 \\\n'
+        )
+        self.build_star_index.logger.info(cmd)
+        subprocess.check_call(cmd, shell=True)
+
+
+    @utils.add_log
+    def build_refflat(self):
+        cmd = (
+            'gtfToGenePred -genePredExt -geneNameAsName2 \\\n'
+            f'{self.gtf} /dev/stdout | \\\n'
+            'awk \'{print $12"\\t"$1"\\t"$2"\\t"$3"\\t"$4"\\t"$5"\\t"$6"\\t"$7"\\t"$8"\\t"$9"\\t"$10}\' \\\n'
+            f'> {self.refflat} \\\n'
+        )
+        self.build_refflat.logger.info(cmd)
+        subprocess.check_call(cmd, shell=True)
+
+    def set_config_dict(self):
+        super().set_config_dict()
+        self.config_dict['refflat'] = self.refflat
+
+    @staticmethod
+    def parse_genomeDir(genomeDir):
+        return Mkref.parse_genomeDir(genomeDir, files=('gtf', 'refflat', 'mt_gene_list'))
+
+
+    @utils.add_log
+    def run(self):
+        super().run()
+        self.build_star_index()
+        self.build_refflat()
+
+
+def mkref(args):
+    genome_type = 'rna'
+    # files do not contain refflat because refflat is not input argument
+    with Mkref_rna(genome_type, args, files=('gtf', 'mt_gene_list'), non_files=('genomeSAindexNbases',)) as runner:
+        runner.run()
+
+
+def get_opts_mkref(parser, sub_program):
+    super_opts(parser, sub_program)
+    if sub_program:
+        parser.add_argument(
+            "--gtf",
+            help="Required. Genome gtf file. Use absolute path or relative path to `genomeDir`.",
+            required=True
+        )
+        parser.add_argument(
+            "--mt_gene_list",
+            help="""Mitochondria gene list file. Use absolute path or relative path to `genomeDir`.
+It is a plain text file with one gene per line. 
+If not provided, will use `MT-` and `mt-` to determine mitochondria genes.""",
+            default="None"
+        )
+        parser.add_argument("--genomeSAindexNbases", help="STAR genomeSAindexNbases", default=14)
---
+++ packages/celescope/celescope-1.10.0-py3-none-any.whl/celescope/tools/__init__.py
@@ -0,0 +1,25 @@
+# barcode
+__PATTERN_DICT__ = {
+    'scopeV2.0.0': 'C8L16C8L16C8U8T18',
+    'scopeV2.0.1': 'C8L16C8L16C8L1U8T18',
+    'scopeV2.1.0': 'C8L16C8L16C8U12T18',
+    'scopeV2.1.1': 'C8L16C8L16C8L1U12T18',
+    'scopeV2.2.1': 'C8L16C8L16C8L1U12T18',
+    'scopeV3.0.1': 'C9L16C9L16C9L1U12T18',
+    'customized': None,
+    'auto': None,
+    'scopeV1': 'C12U8T18',
+    'flv_rna': 'C8L16C8L16C8U9',
+    'flv' : 'U9C8L16C8L16C8',
+}
+
+
+# count
+RAW_MATRIX_DIR_SUFFIX = ['raw_feature_bc_matrix', 'all_matrix']
+FILTERED_MATRIX_DIR_SUFFIX = ['filtered_feature_bc_matrix', 'matrix_10X']
+MATRIX_FILE_NAME = ['matrix.mtx']
+FEATURE_FILE_NAME = ['genes.tsv']
+BARCODE_FILE_NAME = ['barcodes.tsv']
+
+# mkref
+GENOME_CONFIG = 'celescope_genome.config'
