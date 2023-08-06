200fe47e46ec62fa17a9d65491a222411f2acc94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:25:03 +0000

    Add package double-cartpole-custom-gym-env/1.1.1/double_cartpole_custom_gym_env-1.1.1-py3-none-any.whl

---
+++ packages/double-cartpole-custom-gym-env/double_cartpole_custom_gym_env-1.1.1-py3-none-any.whl/double_cartpole_custom_gym_env/event_handler.py
@@ -0,0 +1,8 @@
+import pygame
+from pygame.locals import (QUIT, KEYDOWN, K_ESCAPE)
+
+def pygame_events():
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
