560afaba00f2213c71c0bb843384171b604607c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:42 +0000

    Add package http-message-signatures/0.4.1/http-message-signatures-0.4.1.tar.gz

---
+++ packages/http-message-signatures/http-message-signatures-0.4.1.tar.gz/http-message-signatures-0.4.1/http_message_signatures/algorithms.py
@@ -0,0 +1,2 @@
+from ._algorithms import (HTTPSignatureAlgorithm, RSA_PSS_SHA512, RSA_V1_5_SHA256, HMAC_SHA256,  # noqa:F401
+                          ECDSA_P256_SHA256, ED25519, signature_algorithms)
