50496e616f4f16e411b714ccba9ca855ec0792a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:54 +0000

    Add package heaserver-buckets/1.0.0a6/heaserver-buckets-1.0.0a6.tar.gz

---
+++ packages/heaserver-buckets/heaserver-buckets-1.0.0a6.tar.gz/heaserver-buckets-1.0.0a6/tests/heaserver/buckettest/testcase.py
@@ -0,0 +1,117 @@
+"""
+Creates a test case class for use with the unittest library that is built into Python.
+"""
+from heaserver.service.testcase.mockmongotestcase import get_test_case_cls_default
+from heaserver.bucket import service
+from heaobject.user import NONE_USER
+from heaserver.service.testcase.expectedvalues import ActionSpec
+
+db_store = {
+    service.MONGODB_BUCKET_COLLECTION: [{
+        'id': '666f6f2d6261722d71757578',
+        'created': None,
+        'derived_by': None,
+        'derived_from': [],
+        'description': None,
+        'object_count': None,
+        'size': 8.00,
+        'display_name': 'hci-foundation',
+        'invited': [],
+        'modified': None,
+        'name': 'hci-foundation',
+        'owner': NONE_USER,
+        'shared_with': [],
+        'source': None,
+        'type': 'heaobject.bucket.AWSBucket',
+        'version': None,
+        'arn': None,
+        'is_versioned': False,
+        'is_encrypted': False,
+        'region': None,
+        'permission_policy': None,
+        'tags': [],
+        's3_uri': None
+    },
+        {
+            'id': '0123456789ab0123456789ab',
+            'created': None,
+            'derived_by': None,
+            'derived_from': [],
+            'description': None,
+            'object_count': None,
+            'size': 8.00,
+            'display_name': 'hci-caris',
+            'invited': [],
+            'modified': None,
+            'name': 'hci-foundation',
+            'owner': NONE_USER,
+            'shared_with': [],
+            'source': None,
+            'type': 'heaobject.bucket.AWSBucket',
+            'version': None,
+            'arn': None,
+            'is_versioned': False,
+            'is_encrypted': False,
+            'region': None,
+            'permission_policy': None,
+            'tags': [],
+            's3_uri': None
+        }
+
+    ],
+    'filesystems': [{
+        'id': '666f6f2d6261722d71757578',
+        'created': None,
+        'derived_by': None,
+        'derived_from': [],
+        'description': None,
+        'display_name': 'Amazon Web Services',
+        'invited': [],
+        'modified': None,
+        'name': 'amazon_web_services',
+        'owner': NONE_USER,
+        'shared_with': [],
+        'source': None,
+        'type': 'heaobject.volume.AWSFileSystem',
+        'version': None
+    }],
+    'volumes': [{
+        'id': '666f6f2d6261722d71757578',
+        'created': None,
+        'derived_by': None,
+        'derived_from': [],
+        'description': None,
+        'display_name': 'My Amazon Web Services',
+        'invited': [],
+        'modified': None,
+        'name': 'amazon_web_services',
+        'owner': NONE_USER,
+        'shared_with': [],
+        'source': None,
+        'type': 'heaobject.volume.Volume',
+        'version': None,
+        'file_system_name': 'amazon_web_services',
+        'credential_id': None  # Let boto3 try to find the user's credentials.
+    }]}
+TestCase = get_test_case_cls_default(coll=service.MONGODB_BUCKET_COLLECTION,
+                                     wstl_package=service.__package__,
+                                     href='http://localhost:8080/buckets/',
+                                     fixtures=db_store,
+                                     get_actions=[ActionSpec(name='heaserver-buckets-bucket-get-properties',
+                                                             rel=['properties']),
+                                                  ActionSpec(name='heaserver-buckets-bucket-open',
+                                                             url='/buckets/{id}/opener',
+                                                             rel=['opener']),
+                                                  ActionSpec(name='heaserver-buckets-bucket-duplicate',
+                                                             url='/buckets/{id}/duplicator',
+                                                             rel=['duplicator'])
+                                                  ],
+                                     get_all_actions=[ActionSpec(name='heaserver-buckets-bucket-get-properties',
+                                                                 rel=['properties']),
+                                                      ActionSpec(name='heaserver-buckets-bucket-open',
+                                                                 url='/buckets/{id}/opener',
+                                                                 rel=['opener']),
+                                                      ActionSpec(name='heaserver-buckets-bucket-duplicate',
+                                                                 url='/buckets/{id}/duplicator',
+                                                                 rel=['duplicator'])],
+                                     duplicate_action_name='heaserver-buckets-bucket-duplicate-form')
