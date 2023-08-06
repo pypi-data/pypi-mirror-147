2c4343dfde5fed3a98f98c8b79d827238a5035cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:38 +0000

    Add package expressmoney/9.1.0/expressmoney-9.1.0.tar.gz

---
+++ packages/expressmoney/expressmoney-9.1.0.tar.gz/expressmoney-9.1.0/setup.py
@@ -0,0 +1,16 @@
+"""
+py setup.py sdist
+twine upload dist/expressmoney-9.1.0.tar.gz
+"""
+import setuptools
+
+setuptools.setup(
+    name='expressmoney',
+    packages=setuptools.find_packages(),
+    version='9.1.0',
+    description='SDK ExpressMoney',
+    author='Development team',
+    author_email='dev@expressmoney.com',
+    install_requires=('requests', 'google-cloud-error-reporting', 'google-cloud-tasks'),
+    python_requires='>=3.7',
+)
