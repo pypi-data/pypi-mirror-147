037f7c6a4dd25922e24e52f60cd32bbde71e8e8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:30 +0000

    Add package aioze/0.0.3/aioze-0.0.3.tar.gz

---
+++ packages/aioze/aioze-0.0.3.tar.gz/aioze-0.0.3/aioze/api/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+
+__all__ = []
+
+from aioze.api.event import Event
+from aioze.api.punishment import Punishment
+from aioze.api.notification import Notification
+from aioze.api.information import Information
+from aioze.api.homework import Homework
+from aioze.api.userinfo import UserInfo
+from aioze.api.email import Email
