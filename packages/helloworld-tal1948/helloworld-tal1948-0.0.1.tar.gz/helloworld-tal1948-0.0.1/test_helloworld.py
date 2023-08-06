85167a9d6191d3d348ef77a7299813bda7eac536
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:19 +0000

    Add package helloworld-tal1948/0.0.1/helloworld-tal1948-0.0.1.tar.gz

---
+++ packages/helloworld-tal1948/helloworld-tal1948-0.0.1.tar.gz/helloworld-tal1948-0.0.1/test_helloworld.py
@@ -0,0 +1,9 @@
+from helloworldtal1948 import say_hello
+
+
+def test_helloworld_no_params():
+    assert say_hello() == "Hello, World!"
+
+
+def test_helloworld_with_params():
+    assert say_hello("Tal") == "Hello, Tal!"
