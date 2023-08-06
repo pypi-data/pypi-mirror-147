416b1dffe1cd121e4586db2887d6d5f6a47612c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:40 +0000

    Add package cpflow/0.0.3/cpflow-0.0.3.tar.gz

---
+++ packages/cpflow/cpflow-0.0.3.tar.gz/cpflow-0.0.3/cpflow/__init__.py
@@ -0,0 +1,10 @@
+"""Variational synthesis of quantum circuits with coherent multi-start optimization."""
+
+__version__ = '0.0.3'
+
+from cpflow.main import Results
+from cpflow.main import Decomposition
+from cpflow.main import Synthesize
+from cpflow.main import StaticOptions
+from cpflow.main import AdaptiveOptions
+from cpflow.main import Ansatz
