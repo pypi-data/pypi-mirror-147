62064d8e6d78a2753708980febf1c3329754cccf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:31 +0000

    Add package freezerbox/0.24.0/freezerbox-0.24.0.tar.gz

---
+++ packages/freezerbox/freezerbox-0.24.0.tar.gz/freezerbox-0.24.0/freezerbox/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""\
+Database of DNA sequences.
+"""
+
+__version__ = '0.24.0'
+
+from .config import *
+from .errors import *
+from .model import *
+from .appcli import *
+from .fields import *
+from .group_by import *
+from .utils import *
