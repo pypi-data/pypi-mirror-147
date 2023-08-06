c23b74be9c3241e65f5973192159a958f0a5b7ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:37 +0000

    Add package hahomematic/1.1.4/hahomematic-1.1.4-py3-none-any.whl

---
+++ packages/hahomematic/hahomematic-1.1.4-py3-none-any.whl/hahomematic/platforms/switch.py
@@ -0,0 +1,55 @@
+"""
+Module for entities implemented using the
+switch platform (https://www.home-assistant.io/integrations/switch/).
+"""
+from __future__ import annotations
+
+import logging
+from typing import Any
+
+from hahomematic.const import TYPE_ACTION, HmPlatform
+import hahomematic.device as hm_device
+from hahomematic.entity import GenericEntity
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class HmSwitch(GenericEntity[bool]):
+    """
+    Implementation of a switch.
+    This is a default platform that gets automatically generated.
+    """
+
+    def __init__(
+        self,
+        device: hm_device.HmDevice,
+        unique_id: str,
+        channel_address: str,
+        paramset_key: str,
+        parameter: str,
+        parameter_data: dict[str, Any],
+    ):
+        super().__init__(
+            device=device,
+            unique_id=unique_id,
+            channel_address=channel_address,
+            paramset_key=paramset_key,
+            parameter=parameter,
+            parameter_data=parameter_data,
+            platform=HmPlatform.SWITCH,
+        )
+
+    @property
+    def value(self) -> bool | None:
+        """Get the value of the entity."""
+        if self._type == TYPE_ACTION:
+            return False
+        return self._value
+
+    async def turn_on(self) -> None:
+        """Turn the switch on."""
+        await self.send_value(True)
+
+    async def turn_off(self) -> None:
+        """Turn the switch off."""
+        await self.send_value(False)
