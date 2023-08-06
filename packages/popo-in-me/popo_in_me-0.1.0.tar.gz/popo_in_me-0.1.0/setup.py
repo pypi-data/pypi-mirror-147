d3dbe6fbc5c52338214e43623b874e1bbfab80d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:44 +0000

    Add package popo-in-me/0.1.0/popo_in_me-0.1.0.tar.gz

---
+++ packages/popo-in-me/popo_in_me-0.1.0.tar.gz/popo_in_me-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['popo_in_me', 'popo_in_me.popo_in_me', 'popo_in_me.tests']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'popo-in-me',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Your Name',
+    'author_email': 'you@example.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
