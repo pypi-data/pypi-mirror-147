a0e49d1833b6d970bb6209068cb521af97c58d49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:43 +0000

    Add package LexTestAuto/0.0.34/LexTestAuto-0.0.34.tar.gz

---
+++ packages/LexTestAuto/LexTestAuto-0.0.34.tar.gz/LexTestAuto-0.0.34/LexTestAuto/customVar.py
@@ -0,0 +1,27 @@
+# !/usr/bin/env python
+# -*-coding:utf-8 -*-
+
+"""
+# File       : customVar.py
+# Time       ：22/3/10 15:26
+# Author     ：Lex
+# email      : 2983997560@qq.com
+# Description：自定义数据类型
+"""
+from .dmE import *
+
+
+class CustomE:
+    def __init__(self):
+        self.dm = tmp
+        self.thread = None
+        self.hwnd = None
+        self.title = None
+        self.account = None
+        self.password = None
+        self.service = None
+        self.task_list = None
+        self.progress = None
+
+    def clear(self):
+        pass
