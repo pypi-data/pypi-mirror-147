f08b4fc0ec6a18e5527e3bd5ef1d8969f87c804e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:39 +0000

    Add package pyarubaimc/0.1.2/pyarubaimc-0.1.2.tar.gz

---
+++ packages/pyarubaimc/pyarubaimc-0.1.2.tar.gz/pyarubaimc-0.1.2/pyarubaimc/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Information
+==================
+
+pyarubaimc is a python binding for working with Aruba IMC 7.3 or greater.
+
+Copyright (c) 2022 Aruba, a Hewlett Packard Enterprise company
+
+version 0.1.2 - beta.
+"""
+from pyarubaimc.plat import (auth, objects)
