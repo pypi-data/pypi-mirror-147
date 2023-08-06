7969ebdc1ea262264a29e601f98876dd73a230fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:23 +0000

    Add package cic-helper/0.1.0/cic_helper-0.1.0-py3-none-any.whl

---
+++ packages/cic-helper/cic_helper-0.1.0-py3-none-any.whl/cic_helper/constants.py
@@ -0,0 +1,19 @@
+import csv
+import shutil
+from enum import IntEnum
+from tempfile import NamedTemporaryFile
+
+CSV_HEADER = ['phone_number', 'user_address', 'contract_address',
+              'current_balance', 'send_amount', 'timestamp']
+
+RPC_PROVIDER = 'http://127.0.0.1:8545'
+CHAIN_SPEC = 'evm:kitabu:6060:sarafu'
+
+
+class CSV_Column(IntEnum):
+    Phone = 0,
+    Address = 1,
+    ContractAddress = 2
+    CurrentBalance = 3,
+    SendAmount = 4,
+    Timestamp = 5
