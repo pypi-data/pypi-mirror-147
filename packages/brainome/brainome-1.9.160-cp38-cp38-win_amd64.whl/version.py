a50eaaa08992771bf3e95a3318b1948b1f517b0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:34 +0000

    Add package brainome/1.9.160/brainome-1.9.160-cp38-cp38-win_amd64.whl

---
+++ packages/brainome/brainome-1.9.160-cp38-cp38-win_amd64.whl/version.py
@@ -0,0 +1,6 @@
+BTC_FULL_VERSION = "v1.9-160-prod"
+BTC_SHORT_VERSION = "latest"
+BTC_STAGE = "prod"
+BTC_PEP = "1.9.160"
+BTC_SRV_FULL_TAG = "brainome/btc_srv:v1.9-160-prod"
+BTC_SRV_SHORT_TAG = "brainome/btc_srv:latest"
