d1869561df188d1bf5406b04f12cdb058cf10c70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:39 +0000

    Add package many-abis/0.1.3/many_abis-0.1.3.tar.gz

---
+++ packages/many-abis/many_abis-0.1.3.tar.gz/many_abis-0.1.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='many_abis',
+    packages=find_packages(),
+    version='0.1.3',
+    license='MIT',
+    description='A simple way to get different DEXs abis for block chains.',
+    author='Yong',
+    author_email='ackness8@gmail.com',
+    url='https://github.com/ackness/many_abis',
+    keywords=['abi', 'dex', 'block chain', 'bsc', 'eth', 'matic', 'heco', 'kcc', 'pancakeswap', 'mdex', 'quickswap',
+              'uniswap', 'koffeeswap'],
+    install_requires=[
+        # 'requests',
+        # 'web3',
+    ]
+)
