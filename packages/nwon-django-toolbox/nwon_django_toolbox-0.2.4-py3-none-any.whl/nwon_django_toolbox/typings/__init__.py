e2cffe18c6c5cd911c74305979b38ec93dbdcb6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:17 +0000

    Add package nwon-django-toolbox/0.2.4/nwon_django_toolbox-0.2.4-py3-none-any.whl

---
+++ packages/nwon-django-toolbox/nwon_django_toolbox-0.2.4-py3-none-any.whl/nwon_django_toolbox/typings/__init__.py
@@ -0,0 +1,5 @@
+from nwon_django_toolbox.typings.celery import CeleryFolder, CeleryReturn
+from nwon_django_toolbox.typings.model_type import ModelType
+from nwon_django_toolbox.typings.test_fixture import TestFixture
+
+__all__ = ["ModelType", "CeleryFolder", "CeleryReturn", "TestFixture"]
