d72fabdaeb81b90bf53b4d3bb5502802f5a9fec3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:09 +0000

    Add package pgserials/0.0.1/pgserials-0.0.1.tar.gz

---
+++ packages/pgserials/pgserials-0.0.1.tar.gz/pgserials-0.0.1/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pgserials',
+      version='0.0.1',
+      description='use postgresql to process time serials data',
+      author='Alexander.Li',
+      author_email='superpowerlee@gmaial.com',
+      url='https://github.com/ipconfiger/pgserials',
+      packages=['pgserials'],
+      install_requires=[
+          'sqlalchemy>=1.4',
+          'numpy'
+      ]
+      )
