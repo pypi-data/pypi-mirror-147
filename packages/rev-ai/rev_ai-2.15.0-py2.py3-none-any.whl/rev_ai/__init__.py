4012233ef70f8fcded9847af4453aef4aac78d35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:50 +0000

    Add package rev-ai/2.15.0/rev_ai-2.15.0-py2.py3-none-any.whl

---
+++ packages/rev-ai/rev_ai-2.15.0-py2.py3-none-any.whl/rev_ai/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""Top-level package for rev_ai"""
+
+__version__ = '2.15.0'
+
+from .models import Job, JobStatus, Account, Transcript, Monologue, Element, MediaConfig, \
+    CaptionType, CustomVocabulary, TopicExtractionJob, TopicExtractionResult, Topic, Informant, \
+    SpeakerName
