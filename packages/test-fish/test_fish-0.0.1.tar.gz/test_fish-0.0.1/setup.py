5a28c8d0e3b8e3bf170b80555a0ec2aa9a617dd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:15:30 +0000

    Add package test-fish/0.0.1/test_fish-0.0.1.tar.gz

---
+++ packages/test-fish/test_fish-0.0.1.tar.gz/test_fish-0.0.1/setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import setup, find_packages
+setup(name='test_fish',
+      version='0.0.1',
+      description='just test',
+      author='fmc123653',
+      author_email='fmc0315@126.com', #这里务必填写正确的email格式
+      packages=find_packages(),  # 表示你要封装的包，find_packages用于系统自动从当前目录开始找包
+      license="apache 3.0"
+      )
