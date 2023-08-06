0af9c12cc16cca16d877b1f52a708fdd797eba21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:48:29 +0000

    Add package gtocclient/0.7.12/gtocclient-0.7.12.tar.gz

---
+++ packages/gtocclient/gtocclient-0.7.12.tar.gz/gtocclient-0.7.12/owncloud/pkg_info.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+import io
+
+package = "gtocclient"
+version = "0.7.12"
+url = ""
+license = "LICENSE.txt"
+short_description = "Getui Custom-Developed Python client library for ownCloud"
+
+# vim:et:ts=4:sts=4:ai
