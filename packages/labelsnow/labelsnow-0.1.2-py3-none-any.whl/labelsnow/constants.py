fe8065e72ac18eafa343a105f18d9f8015946637
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:53 +0000

    Add package labelsnow/0.1.2/labelsnow-0.1.2-py3-none-any.whl

---
+++ packages/labelsnow/labelsnow-0.1.2-py3-none-any.whl/labelsnow/constants.py
@@ -0,0 +1,17 @@
+LABELBOX_DEFAULT_TYPE_DICTIONARY = {
+    'ID': 'string',
+    'DataRow ID': 'string',
+    'Labeled Data': 'string',
+    'Created By': 'string',
+    'Project Name': 'string',
+    'Seconds to Label': 'float64',
+    'External ID': 'string',
+    'Agreement': 'int64',
+    'Benchmark Agreement': 'int64',
+    'Benchmark ID': 'string',
+    'Dataset Name': 'string',
+    'Reviews': 'object',
+    'View Label': 'string',
+    'Has Open Issues': 'int64',
+    'Skipped': 'bool'
+}
