a03f307e85aef42127e41e70750c004acf33243f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:04 +0000

    Add package frumpy/1.0.2/frumpy-1.0.2.tar.gz

---
+++ packages/frumpy/frumpy-1.0.2.tar.gz/frumpy-1.0.2/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="frumpy",
+    version="1.0.2",
+    author="Frumpy",
+    author_email="frumpyfrumpster@gmail.com",
+    description="It's Frumpy time.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.6",
+    packages=setuptools.find_packages(where="."),
+    install_requires="numpy",
+)
