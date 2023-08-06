bc1b02da74e270f84b677d062ac1be49fa993cb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:22:48 +0000

    Add package apsd/0.2/apsd-0.2.tar.gz

---
+++ packages/apsd/apsd-0.2.tar.gz/apsd-0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name='apsd',                    # package name
+    version='0.2',                          # version
+    description='Package Description',      # short description
+    author_email='inspiredbycatdreams@gmail.com',
+    install_requires=["random"],                    # list of packages this package depends
+                                            # on.
+    packages=['apsd'],              # List of module names that installing
+
+) 
