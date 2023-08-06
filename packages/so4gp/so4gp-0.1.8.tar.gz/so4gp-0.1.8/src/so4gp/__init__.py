0c040b7ae0c1254518a8bd762eb1fac05df259ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:27 +0000

    Add package so4gp/0.1.8/so4gp-0.1.8.tar.gz

---
+++ packages/so4gp/so4gp-0.1.8.tar.gz/so4gp-0.1.8/src/so4gp/__init__.py
@@ -0,0 +1,24 @@
+
+from .so4gp import DataGP
+from .so4gp import GI
+from .so4gp import GP
+from .so4gp import TimeLag
+from .so4gp import gibitmap
+from .so4gp import inv
+
+from .so4gp import gend
+from .so4gp import genapri
+from .so4gp import genaco
+
+from .so4gp import validategp
+from .so4gp import isduplicate
+from .so4gp import amcheck
+from .so4gp import decodegp
+from .so4gp import costfxn
+
+from .so4gp import graank
+from .so4gp import acogps
+from .so4gp import gagps
+from .so4gp import psogps
+from .so4gp import hcgps
+from .so4gp import rsgps
