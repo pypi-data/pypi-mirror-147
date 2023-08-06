3287607571d1997146846ae826300e4fabda4f72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:03 +0000

    Add package euler-math/0.4.4/euler-math-0.4.4.tar.gz

---
+++ packages/euler-math/euler-math-0.4.4.tar.gz/euler-math-0.4.4/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['euler_math']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['gmpy2>=2.1.2,<3.0.0', 'numpy>=1.22.3,<2.0.0']
+
+setup_kwargs = {
+    'name': 'euler-math',
+    'version': '0.4.4',
+    'description': 'Utilities for Project Euler',
+    'long_description': 'euler-math\n==========\n\nUtility methods for Project Euler\n\nInstallation\n------------\n\npip install euler-math',
+    'author': 'David Carlson',
+    'author_email': 'mndrake2222@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/mndrake/euler-math',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
