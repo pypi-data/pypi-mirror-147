9f2f400223ac56fbe374a4670998a7d8614487c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:02 +0000

    Add package pomerium-http-adapter/0.6.1/pomerium_http_adapter-0.6.1.tar.gz

---
+++ packages/pomerium-http-adapter/pomerium_http_adapter-0.6.1.tar.gz/pomerium_http_adapter-0.6.1/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import os
+
+try:
+    from setuptools import setup
+
+except ImportError:
+    from distutils.core import setup
+
+version = '0.6.1'
+source_directory = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(source_directory, 'README.md'), 'r') as file_handle:
+    long_description = file_handle.read()
+
+setup(
+    name='pomerium_http_adapter',
+    description='Transport adapter for requests to handle Pomerium authentication',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords=['pomerium', 'requests', 'adapter'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Environment :: Plugins',
+        'Topic :: Internet :: WWW/HTTP'],
+    license='Apache License 2.0',
+    author='Joel Rangsmo',
+    author_email='joel.rangsmo@elastx.se',
+    version=version,
+    url='https://github.com/elastx/python-pomerium_http_adapter',
+    download_url=(
+        'https://github.com/elastx/python-pomerium_http_adapter/archive/v%s.tar.gz' % version),
+    packages=['pomerium_http_adapter'],
+    package_dir={'pomerium_http_adapter': 'pomerium_http_adapter'},
+    install_requires=['requests>=2.23.0'])
