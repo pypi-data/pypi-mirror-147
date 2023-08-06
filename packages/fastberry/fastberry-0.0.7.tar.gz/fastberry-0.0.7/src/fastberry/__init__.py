c7a32c77d6a2c45c9ba11ab9d935962340d4b969
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:41 +0000

    Add package fastberry/0.0.7/fastberry-0.0.7.tar.gz

---
+++ packages/fastberry/fastberry-0.0.7.tar.gz/fastberry-0.0.7/src/fastberry/__init__.py
@@ -0,0 +1,14 @@
+"""
+    Fastberry Main (Core-Methods)
+"""
+
+from starlette.middleware.base import BaseHTTPMiddleware as BaseMiddleware
+from strawberry.extensions import Extension as BaseExtension
+from strawberry.permission import BasePermission
+
+from . import config
+# from .config import Settings
+from .config import Settings as Fastberry
+from .config.extras.crud import CRUD
+from .graphql import BaseType
+from .schema import Schema
