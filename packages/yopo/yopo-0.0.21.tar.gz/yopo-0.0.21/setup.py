b6763cac3406b40608a3f9f587df73a8d3115b85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:26 +0000

    Add package yopo/0.0.21/yopo-0.0.21.tar.gz

---
+++ packages/yopo/yopo-0.0.21.tar.gz/yopo-0.0.21/setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+    
+setuptools.setup(
+    name="yopo",
+    version="0.0.21",
+    author="Adarsh Chekodu",
+    author_email="chekodu.adarsh@gmail.com",
+    description="You Plot Only Once",
+    license='Apache License 2.0',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/chekoduadarsh/YOPO-You-Only-Plot-Once",
+    packages=setuptools.find_packages(exclude=("tests",)),
+    install_requires=[
+        "MarkupSafe>=2.0",
+        "jupyter_dash",
+        "click>=7.1.2",
+        "ipython",
+        "jupyter",
+        "wtforms",
+        "dash==2.0.0",
+        "dash_daq",
+        "pandas",
+        "numpy",
+        "dash_bootstrap_components",
+        "pyngrok"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
---
+++ packages/yopo/yopo-0.0.21.tar.gz/yopo-0.0.21/yopo/__init__.py
@@ -0,0 +1,58 @@
+import json
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask import request
+import dash
+from dash import dcc
+from dash import html
+import plotly.express as px
+import pandas as pd
+import string
+import os
+import random
+from jupyter_dash import JupyterDash
+from yopo.dashboard import dashboardApp
+import requests
+from pyngrok import ngrok
+import dash_bootstrap_components as dbc
+
+
+def dashboard(input=pd.DataFrame(),mode="inline",port=8050, tunnel = "", plotly_config= {}):
+    
+    if tunnel.lower() == "ngrok":
+        tunnel = ngrok.connect(port)
+        dash_app = JupyterDash(
+        external_stylesheets=[
+            'https://fonts.googleapis.com/css?family=Lato',
+            'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css',
+            dbc.themes.BOOTSTRAP
+        ],
+        name='dash-app-1',
+        server_url=tunnel.public_url,
+        )
+
+        dash_app.layout = html.Div()
+        dash_app = dashboardApp(input,dash_app,plotly_config)
+        dash_app.run_server(mode=mode,port=port)
+    else:
+        dash_app = JupyterDash(
+        external_stylesheets=[
+            'https://fonts.googleapis.com/css?family=Lato',
+            'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css',
+            dbc.themes.BOOTSTRAP
+        ],
+        name='dash-app-1',
+        )
+
+        dash_app.layout = html.Div()
+        dash_app = dashboardApp(input,dash_app,plotly_config)
+        dash_app.run_server(mode=mode,port=port)
+
+def kill_dashboard(host="localhost", port=8050):
+    shutdown_url = "http://{host}:{port}/_shutdown_{token}".format(
+        host=host, port=port, token=JupyterDash._token
+    )
+    try:
+        response = requests.get(shutdown_url)
+    except Exception as e:
+        print(e)
