24ac1761e2c8288cadc83840c35c6b93ebad213b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:21 +0000

    Add package max-char-match/0.1.0/max-char-match-0.1.0.tar.gz

---
+++ packages/max-char-match/max-char-match-0.1.0.tar.gz/max-char-match-0.1.0/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['max_char_match']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['diff-match-patch>=20200713,<20200714',
+ 'more-itertools>=8.12.0,<9.0.0',
+ 'numpy>=1.22.3,<2.0.0',
+ 'tqdm>=4.64.0,<5.0.0']
+
+setup_kwargs = {
+    'name': 'max-char-match',
+    'version': '0.1.0',
+    'description': 'MaxCharMatch metric',
+    'long_description': None,
+    'author': 'melisa-writer',
+    'author_email': 'melisa@writer.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
