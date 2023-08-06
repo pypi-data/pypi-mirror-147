ac6158ae80db64f401cb377e1a3f10e3aa5fa1d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:35 +0000

    Add package vegafusion-python-embed/0.3.0/vegafusion_python_embed-0.3.0.tar.gz

---
+++ packages/vegafusion-python-embed/vegafusion_python_embed-0.3.0.tar.gz/vegafusion_python_embed-0.3.0/local_dependencies/vegafusion-core/src/planning/plan.rs
@@ -0,0 +1,40 @@
+/*
+ * VegaFusion
+ * Copyright (C) 2022 VegaFusion Technologies LLC
+ *
+ * This program is distributed under multiple licenses.
+ * Please consult the license documentation provided alongside
+ * this program the details of the active license.
+ */
+use crate::error::Result;
+use crate::planning::extract::extract_server_data;
+use crate::planning::optimize_server::split_data_url_nodes;
+use crate::planning::split_domain_data::split_domain_data;
+use crate::planning::stitch::{stitch_specs, CommPlan};
+use crate::spec::chart::ChartSpec;
+
+pub struct SpecPlan {
+    pub server_spec: ChartSpec,
+    pub client_spec: ChartSpec,
+    pub comm_plan: CommPlan,
+}
+
+impl SpecPlan {
+    pub fn try_new(full_spec: &ChartSpec) -> Result<Self> {
+        let mut client_spec = full_spec.clone();
+        split_domain_data(&mut client_spec)?;
+
+        let mut task_scope = client_spec.to_task_scope()?;
+
+        let mut server_spec = extract_server_data(&mut client_spec, &mut task_scope)?;
+        let comm_plan = stitch_specs(&task_scope, &mut server_spec, &mut client_spec)?;
+
+        split_data_url_nodes(&mut server_spec)?;
+
+        Ok(Self {
+            server_spec,
+            client_spec,
+            comm_plan,
+        })
+    }
+}
---
+++ packages/vegafusion-python-embed/vegafusion_python_embed-0.3.0.tar.gz/vegafusion_python_embed-0.3.0/local_dependencies/vegafusion-rt-datafusion/src/expression/compiler/builtin_functions/date_time/mod.rs
@@ -0,0 +1,19 @@
+/*
+ * VegaFusion
+ * Copyright (C) 2022 VegaFusion Technologies LLC
+ *
+ * This program is distributed under multiple licenses.
+ * Please consult the license documentation provided alongside
+ * this program the details of the active license.
+ */
+/*!
+## Date-Time Functions
+Functions for working with date-time values.
+
+See: https://vega.github.io/vega/docs/expressions/#datetime-functions
+*/
+pub mod date_parsing;
+pub mod date_parts;
+pub mod datetime;
+pub mod local_to_utc;
+pub mod time;
