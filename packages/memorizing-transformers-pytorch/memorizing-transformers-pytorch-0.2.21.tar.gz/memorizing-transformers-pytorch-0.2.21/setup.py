74273cef032091d824d9adcb3703670451b59183
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:58 +0000

    Add package memorizing-transformers-pytorch/0.2.21/memorizing-transformers-pytorch-0.2.21.tar.gz

---
+++ packages/memorizing-transformers-pytorch/memorizing-transformers-pytorch-0.2.21.tar.gz/memorizing-transformers-pytorch-0.2.21/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'memorizing-transformers-pytorch',
+  packages = find_packages(exclude=[]),
+  version = '0.2.21',
+  license='MIT',
+  description = 'Memorizing Transformer - Pytorch',
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  url = 'https://github.com/lucidrains/memorizing-transformers-pytorch',
+  keywords = [
+    'artificial intelligence',
+    'deep learning',
+    'transformers',
+    'memory',
+    'retrieval'
+  ],
+  install_requires=[
+    'einops>=0.4',
+    'einops-exts',
+    'joblib',
+    'faiss-gpu',
+    'numpy',
+    'torch>=1.6',
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
