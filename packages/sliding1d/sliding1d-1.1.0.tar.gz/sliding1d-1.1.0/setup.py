9a4228f888763a62ee77e64ead0414fbdfcd046c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:20:47 +0000

    Add package sliding1d/1.1.0/sliding1d-1.1.0.tar.gz

---
+++ packages/sliding1d/sliding1d-1.1.0.tar.gz/sliding1d-1.1.0/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+setuptools.setup(
+    name='sliding1d',
+    version="1.1.0",
+    description='1D sliding window calculation tools',
+    url='https://github.com/gwappa/python-sliding1d',
+    author='Keisuke Sehara',
+    author_email='keisuke.sehara@gmail.com',
+    license='MIT',
+    install_requires=[
+        'numpy>=1.0',
+        'bottleneck>=0.6'
+        ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        ],
+    packages=['sliding1d',],
+    entry_points={
+        # nothing for the time being
+    }
+)
