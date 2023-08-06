6eb3a329694b304355678bbdbda77c5ca7ed92d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:10 +0000

    Add package deez69420/0.1.1/deez69420-0.1.1.tar.gz

---
+++ packages/deez69420/deez69420-0.1.1.tar.gz/deez69420-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+setup(
+    name='deez69420',
+    packages=find_packages(),
+    version='0.1.1',
+    description='funny:)',
+    author='Lawrence LOng',
+    license='MIT',
+)
+
