577816be1b58f825895dee152a8af054f77e4e2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:20 +0000

    Add package Jam-Sesh-Server/1.0.12/Jam-Sesh-Server-1.0.12.tar.gz

---
+++ packages/Jam-Sesh-Server/Jam-Sesh-Server-1.0.12.tar.gz/Jam-Sesh-Server-1.0.12/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Jam-Sesh-Server',
+    version='1.0.12',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'flask', 'amqp', 'celery', 'psycopg2', 'SQLAlchemy', 'requests', 'python-dotenv'
+    ],
+)
