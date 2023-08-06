9ce6b1acd8cd1a62ac6b9db427bcd5d3b4821862
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:43 +0000

    Add package mocha-time/0.0.1/mocha-time-0.0.1.tar.gz

---
+++ packages/mocha-time/mocha-time-0.0.1.tar.gz/mocha-time-0.0.1/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="mocha-time",
+    version="0.0.1",
+    author="Jared Zhao",
+    description="Python Performance Profiling for Production",
+    license="Apache 2.0",
+    python_requires=">=3.6",
+    packages=["mocha"],
+)
