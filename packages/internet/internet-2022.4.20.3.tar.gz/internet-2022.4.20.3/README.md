2128e77a3180a0fb366155d8acf5f5a357184aa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:56 +0000

    Add package internet/2022.4.20.3/internet-2022.4.20.3.tar.gz

---
+++ packages/internet/internet-2022.4.20.3.tar.gz/internet-2022.4.20.3/README.md
@@ -0,0 +1,10 @@
+# internet
+Python library for working with IP addresses and other internet related functionalities
+
+# Installation
+`pip install internet`
+
+# Usage
+```python
+from internet.ipv4 import I
+```
---
+++ packages/internet/internet-2022.4.20.3.tar.gz/internet-2022.4.20.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+	with open('./README.md') as f:
+		return f.read()
+
+
+setup(
+	name='internet',
+	version='2022.4.20.3',
+	description='Python library for working with IP addresses and other internet related functionalities',
+	long_description=readme(),
+	long_description_content_type='text/markdown',
+	url='https://github.com/idin/internet',
+	author='Idin',
+	author_email='py@idin.ca',
+	license='MIT',
+	packages=find_packages(exclude=("jupyter_tests", ".idea", ".git")),
+	install_requires=[
+		'base32hex', 'numpy', 'pandas', 'pyspark', 'disk', 'abstract>=2022.4.20', 'pensieve>=2022.4.20',
+		'chronometry', 'requests', 'soupspoon>=2022.4.20'
+	],
+	python_requires='~=3.6',
+	zip_safe=False
+)
+
+# todo add ipv6 to the library
