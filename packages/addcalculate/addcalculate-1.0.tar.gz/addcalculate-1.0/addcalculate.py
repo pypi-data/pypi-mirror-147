507045a0294391b3dae1e09b10d9e4860ea57505
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:27 +0000

    Add package addcalculate/1.0/addcalculate-1.0.tar.gz

---
+++ packages/addcalculate/addcalculate-1.0.tar.gz/addcalculate-1.0/addcalculate.py
@@ -0,0 +1,3 @@
+
+def add(first,second):
+    print(first+second)
---
+++ packages/addcalculate/addcalculate-1.0.tar.gz/addcalculate-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='addcalculate',
+    version='1.0',
+    description='This is a calculate',
+    author='Mengxin',
+    py_modules=['addcalculate']
+)
