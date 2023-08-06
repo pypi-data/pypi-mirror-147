89cb855a6087033d02a4400daadc19849d92561b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:36 +0000

    Add package nisanyan-cli/0.2/nisanyan_cli-0.2-py3-none-any.whl

---
+++ packages/nisanyan-cli/nisanyan_cli-0.2-py3-none-any.whl/nisanyan_cli/__main__.py
@@ -0,0 +1,37 @@
+import argparse
+from .cli import Niscli
+from .tree import Nistree
+
+__version__ = "0.2"
+
+# parse arguments
+argparser = argparse.ArgumentParser(prog="nisanyan_cli")
+argparser.add_argument(
+    "word",
+    type=str,
+    nargs="*",
+    help="<word>",
+)
+argparser.add_argument(
+    "--tree",
+    "-t",
+    action="store_true",
+    default=False,
+    help="show result as etymology tree",
+)
+argparser.add_argument(
+    "-v", "--version", action="version", version="%(prog)s v" + __version__
+)
+args = argparser.parse_args()
+
+
+def cli():
+    word = " ".join(args.word)
+    if args.tree:
+        Nistree(word)
+    else:
+        Niscli(word)
+
+
+if __name__ == "__main__":
+    cli()
