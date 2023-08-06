83fddadc4d91bd2b42605a4437d1cf580c9da7b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:06 +0000

    Add package odoo12-addon-vh-invoicing/12.0.0.1.3/odoo12_addon_vh_invoicing-12.0.0.1.3-py3-none-any.whl

---
+++ packages/odoo12-addon-vh-invoicing/odoo12_addon_vh_invoicing-12.0.0.1.3-py3-none-any.whl/odoo/addons/vh_invoicing/models/vh_account_journal.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+from odoo.tools.translate import _
+
+
+class VhAccountJournal(models.Model):
+  _inherit = 'account.journal'
+
+  is_vh_journal = fields.Boolean(string=_("VH Journal"))
+  belongs_to_receipt_doc = fields.Boolean(string=_("Belongs to receipt doc"))
+
+  order_mids = fields.Many2many('vh.invoicing.order', 
+    'vh_invoicing_order_account_journal', 'account_journal_id',
+    'invoicing_order_id',string=_("Account journals"))
+
+  # vh_invoicing_config_settings_company_ids = fields.Many2many('res.company', 
+  #   'vh_invoicing_company_account_journal', 'account_journal_id', 'company_id', string=_("Vh journals"))
