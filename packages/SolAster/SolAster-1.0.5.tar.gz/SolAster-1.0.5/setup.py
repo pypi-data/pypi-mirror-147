ed5ca99ade7281f8e1a9b74f8297eb9a4fdf51cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:29 +0000

    Add package SolAster/1.0.5/SolAster-1.0.5.tar.gz

---
+++ packages/SolAster/SolAster-1.0.5.tar.gz/SolAster-1.0.5/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='SolAster',
+    version='1.0.5',
+    packages=['SolAster', 'SolAster.tools', 'SolAster.source', 'SolAster.examples'],
+    install_requires=['astropy', 'sunpy', 'scikit-image'],
+    url='https://github.com/tamarervin/SolAster',
+    license='Apache Software License',
+    author='Tamar Ervin',
+    author_email='tamarervin@gmail.com',
+    description='Python package to calculate \'Sun-as-a-star\' RVs.'
+)
