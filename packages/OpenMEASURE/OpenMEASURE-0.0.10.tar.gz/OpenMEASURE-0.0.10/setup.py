56f3b6e10edfa27440c402835311afc325355474
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:49 +0000

    Add package OpenMEASURE/0.0.10/OpenMEASURE-0.0.10.tar.gz

---
+++ packages/OpenMEASURE/OpenMEASURE-0.0.10.tar.gz/OpenMEASURE-0.0.10/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+      name='OpenMEASURE',
+      version='0.0.10',
+      description='Python package for soft sensing applications',
+      py_modules=['sparse_sensing'],
+      package_dir={'':'src'},
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      install_requires=['numpy>=1.19', 'scipy>=1.5'],
+      url='https://github.com/albertoprocacci/OpenMEASURE',
+      author='Alberto Procacci',
+      author_email='alberto.procacci@gmail.com',
+      )
