33b7b764a494896fd77c45cf7a92243e68b824a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:30 +0000

    Add package expression-parse-eval/0.13.0/expression-parse-eval-0.13.0.tar.gz

---
+++ packages/expression-parse-eval/expression-parse-eval-0.13.0.tar.gz/expression-parse-eval-0.13.0/eval/__main__.py
@@ -0,0 +1,51 @@
+import argparse
+import signal
+import sys
+
+from eval.evaluator import evaluate
+
+
+def main():
+    arg_parser = argparse.ArgumentParser(
+        prog="eval", usage="%(prog)s [expression] [-f FILE] [-h]"
+    )
+    arg_parser.add_argument(
+        "-f",
+        action="store",
+        dest="file",
+        type=str,
+        help="the file that stores expressions",
+    )
+    all_args, others = arg_parser.parse_known_args()
+    if all_args.file:
+        with open(all_args.file) as f:
+            for line in f:
+                result = evaluate(line)
+                print(result)
+    elif len(sys.argv) > 1:
+        try:
+            user_input = "".join(others)
+            value = evaluate(user_input)
+            print(value)
+        except Exception as e:
+            print(e)
+            exit(-1)
+    else:
+
+        def int_handler(sig, frame):
+            print("Bye bye!")
+            sys.exit(0)
+
+        signal.signal(signal.SIGINT, int_handler)
+
+        while True:
+            try:
+                user_input = input("eval > ")
+                value = evaluate(user_input)
+                print(value)
+            except Exception as e:
+                print(e)
+
+
+if __name__ == "__main__":
+    main()
