05a9c535b59855321330baeddc1f3a415b8caef5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:31 +0000

    Add package osgithub/0.3.2/osgithub-0.3.2.tar.gz

---
+++ packages/osgithub/osgithub-0.3.2.tar.gz/osgithub-0.3.2/osgithub/__init__.py
@@ -0,0 +1,16 @@
+from .github import (
+    GithubAPIException,
+    GithubAPIFileTooLarge,
+    GithubClient,
+    GithubContentFile,
+    GithubRepo,
+)
+
+
+__all__ = [
+    "GithubAPIFileTooLarge",
+    "GithubAPIException",
+    "GithubClient",
+    "GithubContentFile",
+    "GithubRepo",
+]
