8ee84ad17465ad5044075f0cfe82916adcc4cb48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:15 +0000

    Add package pypredict/1.6.2/pypredict-1.6.2.tar.gz

---
+++ packages/pypredict/pypredict-1.6.2.tar.gz/pypredict-1.6.2/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup, Extension
+
+setup(
+    name="pypredict",
+    version="1.6.2",
+    author="Jesse Trutna",
+    author_email="jesse@spire.com",
+    maintainer="Spire Global Inc",
+    maintainer_email="opensource@spire.com",
+    url="https://github.com/nsat/pypredict",
+    py_modules=["predict"],
+    ext_modules=[Extension("cpredict", ["predict.c"])],
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
