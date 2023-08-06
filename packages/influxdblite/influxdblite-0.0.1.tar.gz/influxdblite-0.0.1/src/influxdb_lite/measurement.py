bf1f7a500a530a4dff1cd7c35dc91d92ac7d0674
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:15 +0000

    Add package influxdblite/0.0.1/influxdblite-0.0.1.tar.gz

---
+++ packages/influxdblite/influxdblite-0.0.1.tar.gz/influxdblite-0.0.1/src/influxdb_lite/measurement.py
@@ -0,0 +1,15 @@
+from influxdb_lite.attributes import Tag, Field
+
+
+class MetaMeasurement(type):
+    def __init__(cls, name, *args, **kwargs):
+        super(MetaMeasurement, cls).__init__(name)
+        cls.tags = [attr_name for attr_name in cls.__dict__ if isinstance(cls.__dict__[attr_name], Tag)]
+        cls.fields = [attr_name for attr_name in cls.__dict__ if isinstance(cls.__dict__[attr_name], Field)]
+        [cls.__dict__[elem].set_name(elem) for elem in cls.tags + cls.fields]
+
+
+class Measurement(metaclass=MetaMeasurement):
+    name = ''
+    bucket = ''
+    timestamp_label = ''
