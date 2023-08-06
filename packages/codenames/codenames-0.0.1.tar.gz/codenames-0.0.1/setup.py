e09ebc82d712ea653d8107c36857510f3696b0c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:48:19 +0000

    Add package codenames/0.0.1/codenames-0.0.1.tar.gz

---
+++ packages/codenames/codenames-0.0.1.tar.gz/codenames-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="codenames",
+    version="0.0.1",
+    description="Codenames game logic and solvers implementation in python.",
+    author="Asaf Kali",
+    author_email="akali93@gmail.com",
+    url="https://github.com/asaf-kali/codenames",
+    include_package_data=True,
+)
