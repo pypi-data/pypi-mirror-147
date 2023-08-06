a38826d13b485b74a843f92b26061e07cbfa5812
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:42 +0000

    Add package ready-logger/0.1.3/ready_logger-0.1.3.tar.gz

---
+++ packages/ready-logger/ready_logger-0.1.3.tar.gz/ready_logger-0.1.3/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['ready_logger']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'ready-logger',
+    'version': '0.1.3',
+    'description': 'Easily configure loggers.',
+    'long_description': None,
+    'author': 'Dan Kelleher',
+    'author_email': 'dan@danklabs.org',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
