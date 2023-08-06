b8be76a9abbd02868412999a3ab1ed1415e78347
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:03 +0000

    Add package managedtenants-cli/1.22.0/managedtenants_cli-1.22.0-py3-none-any.whl

---
+++ packages/managedtenants-cli/managedtenants_cli-1.22.0-py3-none-any.whl/managedtenants/bundles/docker_api.py
@@ -0,0 +1,150 @@
+import logging
+import os
+
+import docker
+import docker.api.build
+from requests.exceptions import HTTPError
+from sretoolbox.utils.logger import get_text_logger
+
+from managedtenants.bundles.exceptions import DockerError, QuayAPIError
+from managedtenants.bundles.quay_api import QuayAPI
+
+# Hack to allow dockerfile as string
+# https://github.com/docker/docker-py/issues/2105#issuecomment-613685891
+docker.api.build.process_dockerfile = lambda dockerfile, path: (
+    "Dockerfile",
+    dockerfile,
+)
+
+
+class DockerAPI:
+    """
+    Class to build and push docker images.
+
+    :param quay_api: (optional) QuayApi object used for pushing images. Default
+                     to osd-addons, reading token from QUAY_APIKEY env var.
+    :param registry: docker registry to use. Default: quay.io/{quay_api.org}.
+    :param dockercfg_path: (optional) Custom path for the Docker config file.
+                           If not present, the client does not login.
+    :param force_push: overwrite an existing remote image.
+    :param debug: Enable debug logging.
+    :raise ValueError: If an invalid empty username is provided.
+    """
+
+    # pylint: disable=too-many-arguments
+    def __init__(
+        self,
+        registry,
+        dockercfg_path,
+        quay_org,
+        debug=False,
+        force_push=False,
+    ):
+        self.registry = registry
+        self.dockercfg_path = dockercfg_path
+        self.client = docker.from_env()
+        self.force_push = force_push
+        self.log = get_text_logger(
+            "managedtenants-docker",
+            level=logging.DEBUG if debug else logging.INFO,
+        )
+        if self._is_quay_registry():
+            self.quay_api = QuayAPI(org=quay_org, debug=debug)
+
+    def build_bundle(self, bundle):
+        dockerfile = """
+        FROM scratch
+        COPY manifests /manifests/
+        COPY metadata /metadata/
+        """
+        return self._build(
+            path=bundle.path,
+            dockerfile=dockerfile,
+            tag=bundle.image.url_tag,
+            labels=bundle.annotations,
+        )
+
+    def _build(self, path, dockerfile, tag, labels=None):
+        """
+        Build a docker image using in-memory dockerfile.
+
+        :param path: Path to the context directory.
+        :param dockerfile: A file object to use as the Dockerfile.
+        :param tag: Tag for the built image.
+        :param labels: (Optional) image labels.
+
+        :raise DockerError: on a build error or if the built image has Size 0.
+        """
+        try:
+            out_image, log_generator = self.client.images.build(
+                path=str(path),  # Path(..) obj are not allowed
+                dockerfile=dockerfile,
+                labels=labels if labels is not None else {},
+                tag=tag,
+            )
+            for log in log_generator:
+                self.log.debug(log)
+
+            self.validate_image(tag)
+            return out_image
+
+        except docker.errors.BuildError as e:
+            raise DockerError(
+                f"Failed to build image for path {path}, got {e}."
+            )
+
+    def push(self, image, ensure_repo=True):
+        """
+        Push an image to a remote repository.
+
+        :param image: Sretoolbox Image(..) to be pushed.
+
+        :raise DockerError: failed to push image.
+        """
+        try:
+            # docker-py add auth headers from cred store
+            # https://github.com/docker/docker-py/blob/a48a5a9647761406d66e8271f19fab7fa0c5f582/docker/utils/config.py#L33-L38
+            os.environ["DOCKER_CONFIG"] = str(self.dockercfg_path)
+
+            if self._is_quay_registry() and ensure_repo:
+                self.log.info(f"Ensuring quay repo: {image.image}.")
+                self.quay_api.ensure_repo(image.image)
+
+            if not self._image_exists(image) or self.force_push:
+                response = self.client.api.push(
+                    image.url_tag, stream=True, decode=True
+                )
+                for log in response:
+                    self.log.debug(log)
+
+        except QuayAPIError as e:
+            raise DockerError(
+                f"Failed to ensure quay repo {image.repository} got {e}."
+            )
+
+        except docker.errors.APIError as e:
+            raise DockerError(f"Failed to push {image.url_tag}, got {e}.")
+
+    def _is_quay_registry(self):
+        return self.registry.startswith("quay.io")
+
+    def _image_exists(self, image):
+        # The Image(...) sretoolbox library requires a valid quay registry.
+        if not self._is_quay_registry():
+            return False
+
+        try:
+            self.log.info(
+                f"Skipping pushing {image.url_digest} as it already exists."
+            )
+            return True
+        except HTTPError:
+            # image.url_digest, calls digest which raises HTTPError
+            # https://github.com/app-sre/sretoolbox/blob/master/sretoolbox/container/image.py#L119
+            return False
+
+    def validate_image(self, tag):
+        image = self.client.images.get(tag)
+        self.log.debug(image.attrs)
+        if image.attrs.get("Size", -1) == 0:
+            raise DockerError(f"Built an empty image: {tag}.")
---
+++ packages/managedtenants-cli/managedtenants_cli-1.22.0-py3-none-any.whl/managedtenants/bundles/index_builder.py
@@ -0,0 +1,90 @@
+import logging
+import subprocess
+
+from sretoolbox.container import Image
+from sretoolbox.utils.logger import get_text_logger
+
+from managedtenants.bundles.binary_deps import OPM
+from managedtenants.bundles.exceptions import DockerError, IndexBuilderError
+from managedtenants.utils.git import get_short_hash
+
+
+class IndexBuilder:
+    def __init__(
+        self,
+        docker_api,
+        dry_run=False,
+        debug=False,
+    ):
+        self.dry_run = dry_run
+        self.docker_api = docker_api
+        self.log = get_text_logger(
+            "managedtenants-index-builder",
+            level=logging.DEBUG if debug else logging.INFO,
+        )
+
+    def build_and_push(self, bundles, hash_string=get_short_hash()):
+        """
+        Build and push an index image.
+
+        :params bundles: List of Bundle to be added to the index image.
+        :return: An Index image that has been pushed.
+        """
+
+        index_image = self._build(bundles, hash_string)
+        return self._push(index_image)
+
+    def _build(self, bundles, hash_string):
+        if len(bundles) == 0:
+            raise IndexBuilderError("invalid empty bundles list")
+
+        # all bundles for a given addon produce the same index_repo
+        index_image = Image(
+            f"{self.docker_api.registry}/"
+            f"{bundles[0].index_repo_name()}:{hash_string}"
+        )
+        self.log.info(f'Building index image "{index_image.url_tag}".')
+        self.log.debug(
+            f"Index image contains {len(bundles)} bundles: {bundles}."
+        )
+
+        cmd = [
+            "index",
+            "--container-tool",
+            "docker",
+            "add",
+            "--permissive",
+            "--bundles",
+            ",".join([bundle.image.url_tag for bundle in bundles]),
+            "--tag",
+            index_image.url_tag,
+        ]
+
+        try:
+            OPM.run(cmd)
+            self.docker_api.validate_image(index_image.url_tag)
+            return index_image
+
+        except subprocess.CalledProcessError as e:
+            raise IndexBuilderError(
+                f"Failed to build index image {index_image.url_tag} with opm"
+                f" version {OPM.version}: {e.stdout.decode()}."
+            )
+
+        except DockerError as e:
+            raise IndexBuilderError(f"Built invalid index_image: {e}")
+
+    def _push(self, index_image):
+        # skip pushing on dry_run
+        if self.dry_run:
+            return index_image
+
+        try:
+            self.log.info(f'Pushing index image "{index_image.url_tag}".')
+            self.docker_api.push(index_image)
+            return index_image
+
+        except DockerError as e:
+            err_msg = f"failed to push {index_image}: {e}."
+            self.log.error(err_msg)
+            raise IndexBuilderError(err_msg)
