19831da497ef0ac3e17f00be77c836eee92139a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:22 +0000

    Add package aquantresearch/0.1/aquantresearch-0.1.tar.gz

---
+++ packages/aquantresearch/aquantresearch-0.1.tar.gz/aquantresearch-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='aquantresearch',
+    version='0.1',
+    license='Aquant',
+    author="Amir Dafnai",
+    author_email='amirdafnai@aquant.io',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/AquantIO/AquantReasearch',
+    keywords='example project'
+
+)
