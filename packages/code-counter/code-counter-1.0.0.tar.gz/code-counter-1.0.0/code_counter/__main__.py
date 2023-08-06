1488e4c875528ce30ed1ddd628d5a9261409f8c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:33 +0000

    Add package code-counter/1.0.0/code-counter-1.0.0.tar.gz

---
+++ packages/code-counter/code-counter-1.0.0.tar.gz/code-counter-1.0.0/code_counter/__main__.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# coding:utf8
+
+import time
+from code_counter.core.counter import CodeCounter
+from code_counter.core.args import CodeCounterArgs
+from code_counter.conf.config import Config
+
+
+def main():
+    args = CodeCounterArgs()
+
+    config = Config()
+    if args.has_config_args():
+        config.invoke(args.config())
+        return
+
+    code_counter = CodeCounter(config)
+
+    search_args = args.search()
+    code_counter.setSearchArgs(search_args)
+
+    time_start = time.time()
+    code_counter.search()
+    time_end = time.time()
+
+    print('\n\tTotally cost {} s.'.format(time_end - time_start))
+
+    if search_args.graph:
+        code_counter.visualize()
+
+
+if __name__ == '__main__':
+    main()
