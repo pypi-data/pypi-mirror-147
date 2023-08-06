6010f628c0ba52d3535e7a7383843de7cec03a05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:22 +0000

    Add package agenius/1.0/agenius-1.0-py3-none-any.whl

---
+++ packages/agenius/agenius-1.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,9 @@
+"""
+Copyright (C) 2022 dopebnan
+This file is part of AGenius.py.
+You should have received a copy of the GNU Lesser General Public License along with AGenius.py.
+If not, see <https://www.gnu.org/licenses/>.
+"""
+
+__version__ = "1.0"
+__author__ = "dopebnan"
