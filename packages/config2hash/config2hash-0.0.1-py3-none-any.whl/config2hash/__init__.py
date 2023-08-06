a4d0e0ad938bbcad96bf2f08a78a360e917d7e96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:48:10 +0000

    Add package config2hash/0.0.1/config2hash-0.0.1-py3-none-any.whl

---
+++ packages/config2hash/config2hash-0.0.1-py3-none-any.whl/config2hash/__init__.py
@@ -0,0 +1,12 @@
+from config2hash.hash_generator import HashGenerator
+
+
+__version__ = "0.0.1"
+__copyright__ = "Copyright (C) 2022 Shuhei Watanabe"
+__licence__ = "Apache-2.0 License"
+__author__ = "Shuhei Watanabe"
+__author_email__ = "shuhei.watanabe.utokyo@gmail.com"
+__url__ = "https://github.com/nabenabe0928/config2hash"
+
+
+__all__ = ["HashGenerator"]
