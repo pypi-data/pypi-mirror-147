4278dc7d196bbe7c510adb12e6e0437ec543b8c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:28:07 +0000

    Add package meapi/0.1.1/meapi-0.1.1.tar.gz

---
+++ packages/meapi/meapi-0.1.1.tar.gz/meapi-0.1.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='meapi',
+    packages=find_packages(),
+    version='0.1.1',
+    description="Unofficial api for 'Me - Caller ID & Spam Blocker' app",
+    long_description=(open('README.rst').read()),
+    author_email='davidlev@telegmail.com',
+    url='https://meapi.readthedocs.io',
+    author='David lev',
+    license='MIT',
+    install_requires=['requests'],
+)
