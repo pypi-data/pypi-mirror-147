044e86ff6934a908bfc37f09d011382851bda96c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:52:43 +0000

    Add package let-me-google-that-for-you/0.0.1/let_me_google_that_for_you-0.0.1.tar.gz

---
+++ packages/let-me-google-that-for-you/let_me_google_that_for_you-0.0.1.tar.gz/let_me_google_that_for_you-0.0.1/let_me_google_that_for_you/tests/test_let_me_google_that_for_you.py
@@ -0,0 +1,5 @@
+from let_me_google_that_for_you import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
---
+++ packages/let-me-google-that-for-you/let_me_google_that_for_you-0.0.1.tar.gz/let_me_google_that_for_you-0.0.1/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['let_me_google_that_for_you',
+ 'let_me_google_that_for_you.let_me_google_that_for_you',
+ 'let_me_google_that_for_you.tests']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'let-me-google-that-for-you',
+    'version': '0.0.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Your Name',
+    'author_email': 'you@example.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.5,<4.0',
+}
+
+
+setup(**setup_kwargs)
