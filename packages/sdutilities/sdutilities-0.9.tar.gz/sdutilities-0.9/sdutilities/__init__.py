aff9c4f825073cf4900f00b46eb656d01d649d09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:07 +0000

    Add package sdutilities/0.9/sdutilities-0.9.tar.gz

---
+++ packages/sdutilities/sdutilities-0.9.tar.gz/sdutilities-0.9/sdutilities/__init__.py
@@ -0,0 +1,11 @@
+"""
+
+"""
+
+# Allow general functions to be immediately available upon import
+
+from sdutilities.general import *
+from sdutilities.logger import *
+from sdutilities.datalink import *
+from sdutilities.acsclient import *
+from sdutilities.awsclient import *
