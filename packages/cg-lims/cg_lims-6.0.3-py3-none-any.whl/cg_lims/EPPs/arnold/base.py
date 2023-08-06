2315dfa8bd09fdcc699b8f2d90770ad1bd57ad33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:19:16 +0000

    Add package cg-lims/6.0.3/cg_lims-6.0.3-py3-none-any.whl

---
+++ packages/cg-lims/cg_lims-6.0.3-py3-none-any.whl/cg_lims/EPPs/arnold/base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import click
+
+from cg_lims.EPPs.arnold.prep import prep
+from cg_lims.EPPs.arnold.sample import sample
+from cg_lims.EPPs.arnold.sequencing import sequencing
+
+
+@click.group(invoke_without_command=True)
+@click.pass_context
+def arnold_upload(ctx):
+    """Main load commands."""
+    pass
+
+
+arnold_upload.add_command(prep)
+arnold_upload.add_command(sample)
+arnold_upload.add_command(sequencing)
