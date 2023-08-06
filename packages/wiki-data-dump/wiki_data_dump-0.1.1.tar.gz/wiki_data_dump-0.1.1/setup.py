eed8d476d59f70cd6a460d7f0d890c0ac057b10c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:05 +0000

    Add package wiki-data-dump/0.1.1/wiki_data_dump-0.1.1.tar.gz

---
+++ packages/wiki-data-dump/wiki_data_dump-0.1.1.tar.gz/wiki_data_dump-0.1.1/setup.py
@@ -0,0 +1,28 @@
+"""Setup dist for PyPI distribution."""
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wiki_data_dump",
+    version="0.1.1",
+    author="jon-edward",
+    author_email="arithmatlic@gmail.com",
+    description="A package for traversing and downloading files from Wiki Data Dump mirrors.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jon-edward/wiki_dump",
+    keywords=["wikimedia", "wiki data dumps", "wikipedia"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "requests",
+    ],
+    packages=setuptools.find_packages(include=["wiki_data_dump"]),
+    python_requires=">=3.8",
+)
