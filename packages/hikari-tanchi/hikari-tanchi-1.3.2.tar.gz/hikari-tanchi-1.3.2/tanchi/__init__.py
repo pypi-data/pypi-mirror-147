0391ccee195ce61d0064af5e6c2bf1fdb1e56dcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:11 +0000

    Add package hikari-tanchi/1.3.2/hikari-tanchi-1.3.2.tar.gz

---
+++ packages/hikari-tanchi/hikari-tanchi-1.3.2.tar.gz/hikari-tanchi-1.3.2/tanchi/__init__.py
@@ -0,0 +1,6 @@
+"""A signature parser for hikari's command handler tanjun.
+
+Finally be able to define your commands without those bloody decorator chains!
+"""
+from .commands import *
+from .types import *
