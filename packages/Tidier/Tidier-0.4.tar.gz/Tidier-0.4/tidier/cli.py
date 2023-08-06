ba73491099a510c0aa4c826d6e11bf4ce3a4f36d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:48 +0000

    Add package Tidier/0.4/Tidier-0.4.tar.gz

---
+++ packages/Tidier/Tidier-0.4.tar.gz/Tidier-0.4/tidier/cli.py
@@ -0,0 +1,106 @@
+"""The CLI commands"""
+import locale
+from typing import Tuple
+
+import click
+
+from .core import Path, find_sub_files
+
+
+@click.command()
+@click.argument(
+    "input_path",
+    type=click.Path(exists=True, path_type=Path, file_okay=False),
+)
+@click.option(
+    "-o",
+    "--output",
+    "output_path",
+    help="Output directory",
+    default="./Tidier",
+    type=click.Path(
+        file_okay=False,
+        writable=True,
+        path_type=Path
+    ))
+@click.option(
+    "-r",
+    "--regex",
+    "regex_replace",
+    help="Regex match and replace",
+    nargs=2,
+    type=(str, str),
+    default=(r"(.+)", r"%Y/%B/%d/\1"),
+    show_default=True,
+)
+@click.option(
+    "-e",
+    "--exclude",
+    "exclude_patterns",
+    help="Excluding patterns",
+    default=[],
+    multiple=True,
+)
+@click.option(
+    "-c",
+    "--copy",
+    "should_copy",
+    help="Copy files instead of Move",
+    default=False,
+    show_default=True,
+    is_flag=True,
+)
+@click.option(
+    "-l",
+    "--locale",
+    "locale_code",
+    help="Date and time locale",
+    default="en_US",
+    show_default=True,
+)
+@click.option(
+    "-a",
+    "--all",
+    "all_files",
+    help="Apply on all files including HIDDEN files",
+    default=False,
+    show_default=True,
+    is_flag=True,
+)
+@click.option(
+    "-j",
+    "--jalali",
+    "jalali_date",
+    help="Use Jalali calendar",
+    default=False,
+    show_default=True,
+    is_flag=True,
+)
+def main(
+    input_path: Path,
+    output_path: Path,
+    regex_replace: str,
+    exclude_patterns: Tuple[str],
+    should_copy: bool,
+    locale_code: str,
+    all_files: bool,
+    jalali_date: bool,
+) -> None:
+    """Tidy up the file & folder names"""
+    locale.setlocale(locale.LC_ALL, locale_code)
+
+    exclude_patterns = list(exclude_patterns)
+    if not all_files:
+        exclude_patterns.append("**/.*")
+
+    regex, replacement = regex_replace[0], regex_replace[1]
+    files = find_sub_files(input_path, regex, exclude_patterns)
+
+    for file in files:
+        old_path = file.path
+        new_path = file.rename(regex, file.format(replacement, jalali_date), output_path, should_copy)
+        click.echo(f"[-] {'Copying' if should_copy else 'Moving'} {old_path} to {new_path}")
+
+
+if __name__ == "__main__":
+    main()
