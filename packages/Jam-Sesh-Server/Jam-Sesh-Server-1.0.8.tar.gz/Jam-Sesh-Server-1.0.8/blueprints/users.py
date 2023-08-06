d19485337c4f12d6fe4d3f9163f341c8ab7c5478
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:13 +0000

    Add package Jam-Sesh-Server/1.0.8/Jam-Sesh-Server-1.0.8.tar.gz

---
+++ packages/Jam-Sesh-Server/Jam-Sesh-Server-1.0.8.tar.gz/Jam-Sesh-Server-1.0.8/blueprints/users.py
@@ -0,0 +1,19 @@
+import os
+import time
+import dotenv
+from flask import Blueprint, render_template
+from .creds import celery_link
+from .crud import show_all_users
+
+dotenv.load_dotenv()
+
+users = Blueprint("users", __name__, static_folder=os.getenv("CURR_PATH")+"\static", template_folder=os.getenv("CURR_PATH")+"\templates")
+
+
+
+@users.route('/show_users')
+def show_users():
+    title = "All Users"
+
+    return render_template('user_list.html', title=title, users=show_all_users())
+
